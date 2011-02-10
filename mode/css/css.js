26c9bd8a358073a61609d51c2122ef302f208512
Marijn Haverbeke <marijnh@gmail.com>
2011-02-10 14:20:13 +0100

    less chaotic indentation for CSS mode

--- mode/css/css.js
+++ mode/css/css.js
@@ -4,7 +4,7 @@ CodeMirror.defineMode("css", function(config) {
 
   function tokenBase(stream, state) {
     var ch = stream.next();
-    if (ch == "@") return ret("css-at", ch + stream.eatWhile(/\w/));
+    if (ch == "@") {stream.eatWhile(/\w/); return ret("css-at", stream.current());}
     else if (ch == "/" && stream.eat("*")) {
       state.tokenize = tokenCComment;
       return tokenCComment(stream, state);
@@ -21,7 +21,7 @@ CodeMirror.defineMode("css", function(config) {
     }
     else if (ch == "#") {
       stream.eatWhile(/\w/);
-      return ret("css-hash", "hash");
+      return ret("css-selector", "hash");
     }
     else if (ch == "!") {
       stream.match(/^\s*\w*/);
@@ -84,40 +84,37 @@ CodeMirror.defineMode("css", function(config) {
     startState: function(base) {
       return {tokenize: tokenBase,
               baseIndent: base || 0,
-              inBraces: false, inRule: false, inDecl: false};
+              stack: []};
     },
 
     token: function(stream, state) {
       if (stream.eatSpace()) return null;
       var style = state.tokenize(stream, state);
 
-      if (type == "hash")
-        style = state.inRule ? "css-colorcode" : "css-identifier";
-      if (style == "css-identifier") {
-        if (state.inRule) style = "css-value";
-        else if (!state.inBraces && !state.inDecl) style = "css-selector";
+      var context = state.stack[state.stack.length-1];
+      if (type == "hash" && context == "rule") style = "css-colorcode";
+      else if (style == "css-identifier") {
+        if (context == "rule") style = "css-value";
+        else if (!context || context == "@media{") style = "css-selector";
       }
 
-      if (type == "{" && state.inDecl == "@media")
-        state.inDecl = false;
-      else if (type == "{")
-        state.inBraces = true;
-      else if (type == "}")
-        state.inBraces = state.inRule = state.inDecl = false;
-      else if (type == ";")
-        state.inRule = state.inDecl = false;
-      else if (state.inBraces && type != "comment")
-        state.inRule = true;
-      else if (!state.inBraces && style == "css-at")
-        state.inDecl = type;
-
+      if (context == "rule" && /^[\{\};]$/.test(type))
+        state.stack.pop();
+      if (type == "{") {
+        if (context == "@media") state.stack[state.stack.length-1] = "@media{";
+        else state.stack.push("{");
+      }
+      else if (type == "}") state.stack.pop();
+      else if (type == "@media") state.stack.push("@media");
+      else if (context != "rule" && context != "@media" && type != "comment") state.stack.push("rule");
       return style;
     },
 
     indent: function(state, textAfter) {
-      if (!state.inBraces || /^\}/.test(textAfter)) return state.baseIndent;
-      else if (state.inRule) return state.baseIndent + indentUnit * 2;
-      else return state.baseIndent + indentUnit;
+      var n = state.stack.length;
+      if (/^\}/.test(textAfter))
+        n -= state.stack[state.stack.length-1] == "rule" ? 2 : 1;
+      return state.baseIndent + n * indentUnit;
     }
   };
 });
