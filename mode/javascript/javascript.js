5134bcb8cd871e2169893307c3cc33cbce813c8b
alexey-k <alexey.kolosoff@gmail.com>
2011-05-24 04:23:11 -0700

    Edited mode/javascript/javascript.js via GitHub

--- mode/javascript/javascript.js
+++ mode/javascript/javascript.js
@@ -1,15 +1,13 @@
-CodeMirror.defineMode("javascript", function (config, parserConfig)
-{
+CodeMirror.defineMode("javascript", function(config, parserConfig) {
   var indentUnit = config.indentUnit;
   var jsonMode = parserConfig.json;
 
   // Tokenizer
 
-  var keywords = function ()
-  {
-    function kw(type) { return { type: type, style: "js-keyword" }; }
+  var keywords = function(){
+    function kw(type) {return {type: type, style: "js-keyword"};}
     var A = kw("keyword a"), B = kw("keyword b"), C = kw("keyword c");
-    var operator = kw("operator"), atom = { type: "atom", style: "js-atom" };
+    var operator = kw("operator"), atom = {type: "atom", style: "js-atom"};
     return {
       "if": A, "while": A, "with": A, "else": B, "do": B, "try": B, "finally": B,
       "return": C, "break": C, "continue": C, "new": C, "delete": C, "throw": C,
@@ -18,21 +16,18 @@ CodeMirror.defineMode("javascript", function (config, parserConfig)
       "in": operator, "typeof": operator, "instanceof": operator,
       "true": atom, "false": atom, "null": atom, "undefined": atom, "NaN": atom, "Infinity": atom
     };
-  } ();
+  }();
 
   var isOperatorChar = /[+\-*&%=<>!?|]/;
 
-  function chain(stream, state, f)
-  {
+  function chain(stream, state, f) {
     state.tokenize = f;
     return f(stream, state);
   }
 
-  function nextUntilUnescaped(stream, end)
-  {
+  function nextUntilUnescaped(stream, end) {
     var escaped = false, next;
-    while ((next = stream.next()) != null)
-    {
+    while ((next = stream.next()) != null) {
       if (next == end && !escaped)
         return false;
       escaped = !escaped && next == "\\";
@@ -43,59 +38,48 @@ CodeMirror.defineMode("javascript", function (config, parserConfig)
   // Used as scratch variables to communicate multiple values without
   // consing up tons of objects.
   var type, content;
-  function ret(tp, style, cont)
-  {
+  function ret(tp, style, cont) {
     type = tp; content = cont;
     return style;
   }
 
-  function jsTokenBase(stream, state)
-  {
+  function jsTokenBase(stream, state) {
     var ch = stream.next();
     if (ch == '"' || ch == "'")
       return chain(stream, state, jsTokenString(ch));
     else if (/[\[\]{}\(\),;\:\.]/.test(ch))
       return ret(ch);
-    else if (ch == "0" && stream.eat(/x/i))
-    {
+    else if (ch == "0" && stream.eat(/x/i)) {
       stream.eatWhile(/[\da-f]/i);
       return ret("number", "js-atom");
-    }
-    else if (/\d/.test(ch))
-    {
+    }      
+    else if (/\d/.test(ch)) {
       stream.match(/^\d*(?:\.\d*)?(?:e[+\-]?\d+)?/);
       return ret("number", "js-atom");
     }
-    else if (ch == "/")
-    {
-      if (stream.eat("*"))
-      {
+    else if (ch == "/") {
+      if (stream.eat("*")) {
         return chain(stream, state, jsTokenComment);
       }
-      else if (stream.eat("/"))
-      {
+      else if (stream.eat("/")) {
         stream.skipToEnd();
         return ret("comment", "js-comment");
       }
-      else if (state.reAllowed)
-      {
+      else if (state.reAllowed) {
         nextUntilUnescaped(stream, "/");
         stream.eatWhile(/[gimy]/); // 'y' is "sticky" option in Mozilla
         return ret("regexp", "js-string");
       }
-      else
-      {
+      else {
         stream.eatWhile(isOperatorChar);
         return ret("operator", null, stream.current());
       }
     }
-    else if (isOperatorChar.test(ch))
-    {
+    else if (isOperatorChar.test(ch)) {
       stream.eatWhile(isOperatorChar);
       return ret("operator", null, stream.current());
     }
-    else
-    {
+    else {
       stream.eatWhile(/[\w\$_]/);
       var word = stream.current(), known = keywords.propertyIsEnumerable(word) && keywords[word];
       return known ? ret(known.type, known.style, word) :
@@ -103,23 +87,18 @@ CodeMirror.defineMode("javascript", function (config, parserConfig)
     }
   }
 
-  function jsTokenString(quote)
-  {
-    return function (stream, state)
-    {
+  function jsTokenString(quote) {
+    return function(stream, state) {
       if (!nextUntilUnescaped(stream, quote))
         state.tokenize = jsTokenBase;
       return ret("string", "js-string");
     };
   }
 
-  function jsTokenComment(stream, state)
-  {
+  function jsTokenComment(stream, state) {
     var maybeEnd = false, ch;
-    while (ch = stream.next())
-    {
-      if (ch == "/" && maybeEnd)
-      {
+    while (ch = stream.next()) {
+      if (ch == "/" && maybeEnd) {
         state.tokenize = jsTokenBase;
         break;
       }
@@ -130,10 +109,9 @@ CodeMirror.defineMode("javascript", function (config, parserConfig)
 
   // Parser
 
-  var atomicTypes = { "atom": true, "number": true, "variable": true, "string": true, "regexp": true };
+  var atomicTypes = {"atom": true, "number": true, "variable": true, "string": true, "regexp": true};
 
-  function JSLexical(indented, column, type, align, prev, info)
-  {
+  function JSLexical(indented, column, type, align, prev, info) {
     this.indented = indented;
     this.column = column;
     this.type = type;
@@ -142,28 +120,24 @@ CodeMirror.defineMode("javascript", function (config, parserConfig)
     if (align != null) this.align = align;
   }
 
-  function inScope(state, varname)
-  {
+  function inScope(state, varname) {
     for (var v = state.localVars; v; v = v.next)
       if (v.name == varname) return true;
   }
 
-  function parseJS(state, style, type, content, stream)
-  {
+  function parseJS(state, style, type, content, stream) {
     var cc = state.cc;
     // Communicate our context to the combinators.
     // (Less wasteful than consing up a hundred closures on every call.)
     cx.state = state; cx.stream = stream; cx.marked = null, cx.cc = cc;
-
+  
     if (!state.lexical.hasOwnProperty("align"))
       state.lexical.align = true;
 
-    while (true)
-    {
+    while(true) {
       var combinator = cc.length ? cc.pop() : jsonMode ? expression : statement;
-      if (combinator(type, content))
-      {
-        while (cc.length && cc[cc.length - 1].lex)
+      if (combinator(type, content)) {
+        while(cc.length && cc[cc.length - 1].lex)
           cc.pop()();
         if (cx.marked) return cx.marked;
         if (type == "variable" && inScope(state, content)) return "js-localvariable";
@@ -174,56 +148,46 @@ CodeMirror.defineMode("javascript", function (config, parserConfig)
 
   // Combinator utils
 
-  var cx = { state: null, column: null, marked: null, cc: null };
-  function pass()
-  {
+  var cx = {state: null, column: null, marked: null, cc: null};
+  function pass() {
     for (var i = arguments.length - 1; i >= 0; i--) cx.cc.push(arguments[i]);
   }
-  function cont()
-  {
+  function cont() {
     pass.apply(null, arguments);
     return true;
   }
-  function register(varname)
-  {
+  function register(varname) {
     var state = cx.state;
-    if (state.context)
-    {
+    if (state.context) {
       cx.marked = "js-variabledef";
       for (var v = state.localVars; v; v = v.next)
         if (v.name == varname) return;
-      state.localVars = { name: varname, next: state.localVars };
+      state.localVars = {name: varname, next: state.localVars};
     }
   }
 
   // Combinators
 
-  var defaultVars = { name: "this", next: { name: "arguments"} };
-  function pushcontext()
-  {
+  var defaultVars = {name: "this", next: {name: "arguments"}};
+  function pushcontext() {
     if (!cx.state.context) cx.state.localVars = defaultVars;
-    cx.state.context = { prev: cx.state.context, vars: cx.state.localVars };
+    cx.state.context = {prev: cx.state.context, vars: cx.state.localVars};
   }
-  function popcontext()
-  {
+  function popcontext() {
     cx.state.localVars = cx.state.context.vars;
     cx.state.context = cx.state.context.prev;
   }
-  function pushlex(type, info)
-  {
-    var result = function ()
-    {
+  function pushlex(type, info) {
+    var result = function() {
       var state = cx.state;
       state.lexical = new JSLexical(state.indented, cx.stream.column(), type, null, state.lexical, info)
     };
     result.lex = true;
     return result;
   }
-  function poplex()
-  {
+  function poplex() {
     var state = cx.state;
-    if (state.lexical.prev)
-    {
+    if (state.lexical.prev) {
       if (state.lexical.type == ")")
         state.indented = state.lexical.indented;
       state.lexical = state.lexical.prev;
@@ -231,18 +195,15 @@ CodeMirror.defineMode("javascript", function (config, parserConfig)
   }
   poplex.lex = true;
 
-  function expect(wanted)
-  {
-    return function expecting(type)
-    {
+  function expect(wanted) {
+    return function expecting(type) {
       if (type == wanted) return cont();
       else if (wanted == ";") return pass();
       else return cont(arguments.callee);
     };
   }
 
-  function statement(type)
-  {
+  function statement(type) {
     if (type == "var") return cont(pushlex("vardef"), vardef1, expect(";"), poplex);
     if (type == "keyword a") return cont(pushlex("form"), expression, statement, poplex);
     if (type == "keyword b") return cont(pushlex("form"), statement, poplex);
@@ -260,8 +221,7 @@ CodeMirror.defineMode("javascript", function (config, parserConfig)
                                         statement, poplex, popcontext);
     return pass(pushlex("stat"), expression, expect(";"), poplex);
   }
-  function expression(type)
-  {
+  function expression(type) {
     if (atomicTypes.hasOwnProperty(type)) return cont(maybeoperator);
     if (type == "function") return cont(functiondef);
     if (type == "keyword c") return cont(expression);
@@ -271,8 +231,7 @@ CodeMirror.defineMode("javascript", function (config, parserConfig)
     if (type == "{") return cont(pushlex("}"), commasep(objprop, "}"), poplex, maybeoperator);
     return cont();
   }
-  function maybeoperator(type, value)
-  {
+  function maybeoperator(type, value) {
     if (type == "operator" && /\+\+|--/.test(value)) return cont(maybeoperator);
     if (type == "operator") return cont(expression);
     if (type == ";") return;
@@ -280,167 +239,70 @@ CodeMirror.defineMode("javascript", function (config, parserConfig)
     if (type == ".") return cont(property, maybeoperator);
     if (type == "[") return cont(pushlex("]"), expression, expect("]"), poplex, maybeoperator);
   }
-  function maybelabel(type)
-  {
+  function maybelabel(type) {
     if (type == ":") return cont(poplex, statement);
     return pass(maybeoperator, expect(";"), poplex);
   }
-  function property(type)
-  {
-    if (type == "variable") { cx.marked = "js-property"; return cont(); }
+  function property(type) {
+    if (type == "variable") {cx.marked = "js-property"; return cont();}
   }
-  function objprop(type)
-  {
+  function objprop(type) {
     if (type == "variable") cx.marked = "js-property";
     if (atomicTypes.hasOwnProperty(type)) return cont(expect(":"), expression);
   }
-  function commasep(what, end)
-  {
-    function proceed(type)
-    {
+  function commasep(what, end) {
+    function proceed(type) {
       if (type == ",") return cont(what, proceed);
       if (type == end) return cont();
       return cont(expect(end));
     }
-    return function commaSeparated(type)
-    {
+    return function commaSeparated(type) {
       if (type == end) return cont();
       else return pass(what, proceed);
     };
   }
-  function block(type)
-  {
+  function block(type) {
     if (type == "}") return cont();
     return pass(statement, block);
   }
-  function vardef1(type, value)
-  {
-    if (type == "variable") { register(value); return cont(vardef2); }
+  function vardef1(type, value) {
+    if (type == "variable"){register(value); return cont(vardef2);}
     return cont();
   }
-  function vardef2(type, value)
-  {
+  function vardef2(type, value) {
     if (value == "=") return cont(expression, vardef2);
     if (type == ",") return cont(vardef1);
   }
-  function forspec1(type)
-  {
+  function forspec1(type) {
     if (type == "var") return cont(vardef1, forspec2);
     if (type == ";") return pass(forspec2);
     if (type == "variable") return cont(formaybein);
     return pass(forspec2);
   }
-  function formaybein(type, value)
-  {
+  function formaybein(type, value) {
     if (value == "in") return cont(expression);
     return cont(maybeoperator, forspec2);
   }
-  function forspec2(type, value)
-  {
+  function forspec2(type, value) {
     if (type == ";") return cont(forspec3);
     if (value == "in") return cont(expression);
     return cont(expression, expect(";"), forspec3);
   }
-  function forspec3(type)
-  {
+  function forspec3(type) {
     if (type != ")") cont(expression);
   }
-  function functiondef(type, value)
-  {
-    if (type == "variable") { register(value); return cont(functiondef); }
+  function functiondef(type, value) {
+    if (type == "variable") {register(value); return cont(functiondef);}
     if (type == "(") return cont(pushlex(")"), pushcontext, commasep(funarg, ")"), poplex, statement, popcontext);
   }
-  function funarg(type, value)
-  {
-    if (type == "variable") { register(value); return cont(); }
+  function funarg(type, value) {
+    if (type == "variable") {register(value); return cont();}
   }
-
-  // =========== Extended by A.K. =============
-  function getNonBreakableBlocks(text)
-  {
-    var nonBreakableRegexes = new Array();
-    nonBreakableRegexes[0] = new RegExp("for\\s*?\\(([\\s\\S]*?)\\)");
-    nonBreakableRegexes[1] = new RegExp("'([\\s\\S]*?)('|$)");
-    nonBreakableRegexes[2] = new RegExp("\"([\\s\\S]*?)(\"|$)");
-    var nonBreakableBlocks = new Array();    
-    for (var i = 0; i < nonBreakableRegexes.length; i++)
-    {
-      var curPos = 0;
-      while (curPos < text.length)
-      {
-        var m = text.substr(curPos).match(nonBreakableRegexes[i]);
-        if (m != null)
-        {
-          nonBreakableBlocks.push({
-            start: curPos + m.index,
-            end: curPos + m.index + m[0].length
-          });
-          curPos += m.index + Math.max(1, m[0].length);
-        }
-        else
-        { // No more matches
-          break;
-        }
-      }
-    }
-    nonBreakableBlocks.sort(function (a, b)
-    {
-      return a.start - b.start;
-    });
-
-    return nonBreakableBlocks;
-  }
-  // ==========================================
 
   // Interface
 
   return {
-    // =========== Extended by A.K. =============
-    commentStart: "/*",
-    commentEnd: "*/",
-    wordWrapChars: [";", "\\{", "\\}"],
-    getModeAtPos: function (text, absPos)
-    {
-      return this;
-    },
-    autoFormatLineBreaks: function (text)
-    {
-      var curPos = 0;
-      var reLinesSplitter = new RegExp("(;|\\{|\\})([^\r\n])", "g");
-      var nonBreakableBlocks = getNonBreakableBlocks(text);
-      if (nonBreakableBlocks != null)
-      {
-        var res = "";
-        for (var i = 0; i < nonBreakableBlocks.length; i++)
-        {
-          if (nonBreakableBlocks[i].start > curPos)
-          { // Break lines till the block
-            res += text.substring(curPos, nonBreakableBlocks[i].start).replace(reLinesSplitter, "$1\n$2");
-            curPos = nonBreakableBlocks[i].start;
-          }
-          if (nonBreakableBlocks[i].start <= curPos
-            && nonBreakableBlocks[i].end >= curPos)
-          { // Skip non-breakable block
-            res += text.substring(curPos, nonBreakableBlocks[i].end);
-            curPos = nonBreakableBlocks[i].end;
-          }
-        }
-        if (curPos < text.length - 1)
-        {
-          res += text.substr(curPos).replace(reLinesSplitter, "$1\n$2");
-        }
-        return res;
-      }
-      else
-      {
-        //return text.replace(new RegExp("(;|{|})([^\r\n])", "g"), "$1\n$2");
-        return text.replace(reLinesSplitter, "$1\n$2");
-      }
-    },
-    // ==========================================
-
-    startState: function (basecolumn)
-    {
+    startState: function(basecolumn) {
       return {
         tokenize: jsTokenBase,
         reAllowed: true,
@@ -452,10 +314,8 @@ CodeMirror.defineMode("javascript", function (config, parserConfig)
       };
     },
 
-    token: function (stream, state)
-    {
-      if (stream.sol())
-      {
+    token: function(stream, state) {
+      if (stream.sol()) {
         if (!state.lexical.hasOwnProperty("align"))
           state.lexical.align = false;
         state.indented = stream.indentation();
@@ -467,8 +327,7 @@ CodeMirror.defineMode("javascript", function (config, parserConfig)
       return parseJS(state, style, type, content, stream);
     },
 
-    indent: function (state, textAfter)
-    {
+    indent: function(state, textAfter) {
       if (state.tokenize != jsTokenBase) return 0;
       var firstChar = textAfter && textAfter.charAt(0), lexical = state.lexical,
           type = lexical.type, closing = firstChar == type;
@@ -485,5 +344,5 @@ CodeMirror.defineMode("javascript", function (config, parserConfig)
   };
 });
 
-CodeMirror.defineMIME("text/javascript", "javscript");
+CodeMirror.defineMIME("text/javascript", "javascript");
 CodeMirror.defineMIME("application/json", {name: "javascript", json: true});
