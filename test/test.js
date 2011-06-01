02c569fdf4540365218b81e8cae96f6bf1fcfeb3
Marijn Haverbeke <marijnh@gmail.com>
2011-06-01 10:27:49 +0200

    Introduce an extra DIV for scrolling
    This might break some client code.
    
    There is now a CodeMirror-scroll class that is used to provide the
    scrolling, rather than using the outer wrapper for this. This allows
    adding fat borders to the wrapper without causing glitches in
    cursor and mouse-click positioning.

--- test/test.js
+++ test/test.js
@@ -116,7 +116,8 @@ testCM("lineInfo", function(cm) {
 }, {value: "111111\n222222\n333333"});
 
 testCM("coords", function(cm) {
-  cm.getWrapperElement().style.height = "100px";
+  var scroller = cm.getWrapperElement().getElementsByClassName("CodeMirror-scroll")[0];
+  scroller.style.height = "100px";
   var content = [];
   for (var i = 0; i < 200; ++i) content.push("------------------------------" + i);
   cm.setValue(content.join("\n"));
@@ -125,7 +126,7 @@ testCM("coords", function(cm) {
   is(top.x < bot.x);
   is(top.y < bot.y);
   is(top.y < top.yBot);
-  cm.getWrapperElement().scrollTop = 100;
+  scroller.scrollTop = 100;
   cm.refresh();
   var top2 = cm.charCoords({line: 0, ch: 0});
   is(top.y > top2.y);
