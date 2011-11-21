iee/CodeMirror2

Make TextMarker.clear only update the lines that were touched
This can be done efficiently now

lib/codemirror.js
@@ -1162,16 +1162,18 @@ var CodeMirror = (function() {
 
     function TextMarker(set) { this.set = set; }
     TextMarker.prototype.clear = operation(function() {
+      var min = Infinity, max = -Infinity;
       for (var i = 0, e = this.set.length; i < e; ++i) {
-        var mk = this.set[i].marked;
-        if (!mk) continue;
+        var line = this.set[i], mk = line.marked;
+        if (!mk || !line.parent) continue;
+        var lineNo = lineNo(line);
+        min = Math.min(min, lineNo); max = Math.max(max, lineNo);
         for (var j = 0; j < mk.length; ++j) {
           if (mk[j].set == this.set) mk.splice(j--, 1);
         }
       }
-      // We don't know the exact lines that changed. Refreshing is
-      // cheaper than finding them.
-      changes.push({from: 0, to: doc.size});
+      if (min != Infinity)
+        changes.push({from: min, to: max + 1});
     });
     TextMarker.prototype.find = function() {
       var from, to;

codemirror/codemirror5

lib/codemirror.js
@@ -1157,15 +1157,17 @@ var CodeMirror = (function() {
 
     function TextMarker() { this.set = []; }
     TextMarker.prototype.clear = operation(function() {
+      var min = Infinity, max = -Infinity;
       for (var i = 0, e = this.set.length; i < e; ++i) {
-        var mk = this.set[i].marked;
-        if (!mk) continue;
+        var line = this.set[i], mk = line.marked;
+        if (!mk || !line.parent) continue;
+        var lineN = lineNo(line);
+        min = Math.min(min, lineN); max = Math.max(max, lineN);
         for (var j = 0; j < mk.length; ++j)
           if (mk[j].set == this.set) mk.splice(j--, 1);
       }
-      // We don't know the exact lines that changed. Refreshing is
-      // cheaper than finding them.
-      changes.push({from: 0, to: doc.size});
+      if (min != Infinity)
+        changes.push({from: min, to: max + 1});
     });
     TextMarker.prototype.find = function() {
       var from, to;