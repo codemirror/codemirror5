7b3f6cdb9f12e93a1db5d9b4dd01b05fea7d16ea
Marijn Haverbeke <marijnh@gmail.com>
2011-11-21 22:10:51 +0100

    Add the setBookmark method

--- test/test.js
+++ test/test.js
@@ -266,6 +266,22 @@ testCM("markTextMultiLine", function(cm) {
   });
 });
 
+testCM("bookmark", function(cm) {
+  function p(v) { return v && {line: v[0], ch: v[1]}; }
+  forEach([{a: [1, 0], b: [1, 1], c: "", d: [1, 4]},
+           {a: [1, 1], b: [1, 1], c: "xx", d: [1, 7]},
+           {a: [1, 4], b: [1, 5], c: "ab", d: [1, 6]},
+           {a: [1, 4], b: [1, 6], c: "", d: null},
+           {a: [1, 5], b: [1, 6], c: "abc", d: [1, 5]},
+           {a: [1, 6], b: [1, 8], c: "", d: [1, 5]},
+           {a: [1, 4], b: [1, 4], c: "\n\n", d: [3, 1]}], function(test) {
+    cm.setValue("1234567890\n1234567890\n1234567890");
+    var b = cm.setBookmark({line: 1, ch: 5});
+    cm.replaceRange(test.c, p(test.a), p(test.b));
+    eqPos(b.find(), p(test.d));
+  });
+});
+
 // Scaffolding
 
 function htmlEscape(str) {
