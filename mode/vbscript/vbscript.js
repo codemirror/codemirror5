c8b1512f64aee00ae6ee6a3f68692c702956e169
Marijn Haverbeke <marijnh@gmail.com>
2012-03-14 15:23:25 +0100

    Clean up and integrate VBScript mode

--- mode/vbscript/vbscript.js
+++ mode/vbscript/vbscript.js
@@ -1,24 +1,26 @@
 CodeMirror.defineMode("vbscript", function() {
-  var regexVBScriptKeyword = / Call | Case | CDate | Clear | CInt | CLng | Const | CStr | Description | Dim | Do | Each | Else | ElseIf | End | Err | Error | Exit | False | For | Function | If | LCase | Loop | LTrim | Next | Nothing | Now | Number | On | Preserve | Quit | ReDim | Resume | RTrim | Select | Set | Sub | Then | To | Trim | True | UBound | UCase | Until | VbCr | VbCrLf | VbLf | VbTab /im;
-  var VBScriptElectricChars = /[(). =+\-<>*'"]/;
+  var regexVBScriptKeyword = /Call|Case|CDate|Clear|CInt|CLng|Const|CStr|Description|Dim|Do|Each|Else|ElseIf|End|Err|Error|Exit|False|For|Function|If|LCase|Loop|LTrim|Next|Nothing|Now|Number|On|Preserve|Quit|ReDim|Resume|RTrim|Select|Set|Sub|Then|To|Trim|True|UBound|UCase|Until|VbCr|VbCrLf|VbLf|VbTab/im;
+
   return {
     token: function(stream) {
-      stream.eatSpace();
-	  var ch=stream.next();
-      if(ch=="'"){
-      	ch = stream.next();
+      if (stream.eatSpace()) return null;
+      var ch = stream.next();
+      if (ch == "'") {
       	stream.skipToEnd();
-      	return "comment";}
-      else if (ch == '"'){
+      	return "comment";
+      }
+      if (ch == '"') {
       	stream.skipTo('"');
-      	return "string";}
-      else if (ch == ' '){
-      	return null;}
-      var strToken = '';
-      while(ch && ch != stream.eol() && !VBScriptElectricChars.test(ch) ){
-      	strToken=strToken+ch; 
-      	ch=stream.next();} 
-      if(ch&&ch!=' ') return null;
-	  if(regexVBScriptKeyword.test(' '+strToken+' ')) return "keyword";}};
+      	return "string";
+      }
+
+      if (/\w/.test(ch)) {
+        stream.eatWhile(/\w/);
+        if (regexVBScriptKeyword.test(stream.current())) return "keyword";
+      }
+      return null;
+    }
+  };
 });
+
 CodeMirror.defineMIME("text/vbscript", "vbscript");
