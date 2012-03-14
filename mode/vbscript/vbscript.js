dd9bb710a770e4d1d8ad5bc8edefbd56d1f7600d
Marijn Haverbeke <marijnh@gmail.com>
2012-03-14 16:08:02 +0100

    [vbscript mode] Add start/end anchors to keyword regexp
    Issue #445

--- mode/vbscript/vbscript.js
+++ mode/vbscript/vbscript.js
@@ -1,5 +1,5 @@
 CodeMirror.defineMode("vbscript", function() {
-  var regexVBScriptKeyword = /Call|Case|CDate|Clear|CInt|CLng|Const|CStr|Description|Dim|Do|Each|Else|ElseIf|End|Err|Error|Exit|False|For|Function|If|LCase|Loop|LTrim|Next|Nothing|Now|Number|On|Preserve|Quit|ReDim|Resume|RTrim|Select|Set|Sub|Then|To|Trim|True|UBound|UCase|Until|VbCr|VbCrLf|VbLf|VbTab/im;
+  var regexVBScriptKeyword = /^(?:Call|Case|CDate|Clear|CInt|CLng|Const|CStr|Description|Dim|Do|Each|Else|ElseIf|End|Err|Error|Exit|False|For|Function|If|LCase|Loop|LTrim|Next|Nothing|Now|Number|On|Preserve|Quit|ReDim|Resume|RTrim|Select|Set|Sub|Then|To|Trim|True|UBound|UCase|Until|VbCr|VbCrLf|VbLf|VbTab)$/im;
 
   return {
     token: function(stream) {
