b400e02dfa088659bac0d089c4affcd8f8684460
Marijn Haverbeke <marijnh@gmail.com>
2012-04-02 14:52:40 +0200

    Allow compareStates to return CodeMirror.Pass to pass
    This to make it possible to mix modes with and without compareStates
    implementations.

--- mode/htmlmixed/htmlmixed.js
+++ mode/htmlmixed/htmlmixed.js
@@ -73,6 +73,8 @@ CodeMirror.defineMode("htmlmixed", function(config, parserConfig) {
     },
 
     compareStates: function(a, b) {
+      if (a.mode != b.mode) return false;
+      if (a.localState) return CodeMirror.Pass;
       return htmlMode.compareStates(a.htmlState, b.htmlState);
     },
 
