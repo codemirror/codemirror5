7a7f5bd7a630c2278ee48406b14e187a6a3103dd
Peter Kroon <plakroon@gmail.com>
2012-04-22 00:43:57 +0300

    Added missing semicolon.

--- mode/clike/clike.js
+++ mode/clike/clike.js
@@ -21,7 +21,7 @@ CodeMirror.defineMode("clike", function(config, parserConfig) {
     }
     if (/[\[\]{}\(\),;\:\.]/.test(ch)) {
       curPunc = ch;
-      return null
+      return null;
     }
     if (/\d/.test(ch)) {
       stream.eatWhile(/[\w\.]/);
