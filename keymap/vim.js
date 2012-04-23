178e41f9fbf7a1cbc3b5b62d295b8aaa8a5ca6c2
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2012-04-23 16:42:38 +0400

    do not crash when mark is not set

--- keymap/vim.js
+++ keymap/vim.js
@@ -64,7 +64,12 @@
     }
   }
   function delTillMark(cm, cHar) { 
-    var i = mark[cHar], l = cm.getCursor().line, start = i > l ? l : i, end = i > l ? i : l;
+    var i = mark[cHar];
+    if (i === undefined) {
+        console.log('Mark not set'); // TODO - show in status bar
+        return;
+    }
+    var l = cm.getCursor().line, start = i > l ? l : i, end = i > l ? i : l;
     cm.setCursor(start);
     for (var c = start; c <= end; c++) {
       pushInBuffer("\n"+cm.getLine(start)); 
@@ -72,7 +77,12 @@
     }
   }
   function yankTillMark(cm, cHar) { 
-    var i = mark[cHar], l = cm.getCursor().line, start = i > l ? l : i, end = i > l ? i : l;
+    var i = mark[cHar];
+    if (i === undefined) {
+        console.log('Mark not set'); // TODO - show in status bar
+        return;
+    }
+    var l = cm.getCursor().line, start = i > l ? l : i, end = i > l ? i : l;
     for (var c = start; c <= end; c++) {
       pushInBuffer("\n"+cm.getLine(c));
     }
