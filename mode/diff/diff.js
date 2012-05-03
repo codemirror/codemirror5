02cd718e4f00f89b18db12efa05167f01f7b28da
Filip Noetzel <filip@j03.de>
2012-05-03 12:56:18 +0200

    Teach "diff" mode to highlight trailing whitespace
    This is probably the first and only commit where
    I deliberately *add* trailing whitespace (to show
    off the feature here).

--- mode/diff/diff.js
+++ mode/diff/diff.js
@@ -1,11 +1,30 @@
 CodeMirror.defineMode("diff", function() {
+
+  var TOKEN_NAMES = {
+    '+': "plus",
+    '-': "minus",
+    '@': "rangeinfo"
+  };
+
   return {
     token: function(stream) {
-      var ch = stream.next();
-      stream.skipToEnd();
-      if (ch == "+") return "plus";
-      if (ch == "-") return "minus";
-      if (ch == "@") return "rangeinfo";
+      var tw_pos = stream.string.search(/[\t ]+?$/);
+
+      if (!stream.sol() || tw_pos === 0) {
+        stream.skipToEnd();
+        return "trailing-whitespace-" + (
+          TOKEN_NAMES[stream.string.charAt(0)] || "normal");
+      }
+
+      var token_name = TOKEN_NAMES[stream.peek()] || stream.skipToEnd();
+
+      if (tw_pos === -1) {
+        stream.skipToEnd();
+      } else {
+        stream.pos = tw_pos;
+      }
+
+      return token_name;
     }
   };
 });
