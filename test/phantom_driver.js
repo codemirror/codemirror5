0ac8a6ebcf5cf50f165011f8b2757e6f580e89c3
Tim Baumann <tim@timbaumann.info>
2012-07-24 00:09:11 +0200

    Use full path to test page because it uses relative URLs

--- test/phantom_driver.js
+++ test/phantom_driver.js
@@ -4,6 +4,6 @@ page.onError = function (msg, trace) {
   console.log(msg);
 };
 
-page.open("http://localhost:3000/test", function (status) {
+page.open("http://localhost:3000/test/index.html", function (status) {
   phantom.exit(status == "success" ? 0 : 1);
 });
