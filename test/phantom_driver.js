8b36b7e5c9370fd38fa63c4f392a3454423da954
Tim Baumann <tim@timbaumann.info>
2012-07-24 00:43:38 +0200

    phantom driver: exit with 1 in case of an error

--- test/phantom_driver.js
+++ test/phantom_driver.js
@@ -1,9 +1,12 @@
 var page = require('webpage').create();
 
+var errors = false;
+
 page.onError = function (msg, trace) {
+  errors = true;
   console.log(msg);
 };
 
 page.open("http://localhost:3000/test/index.html", function (status) {
-  phantom.exit(status == "success" ? 0 : 1);
+  phantom.exit(status == "success" && !errors ? 0 : 1);
 });
