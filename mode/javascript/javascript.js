415776acc178fc4a91fd6c44c6a4d23dff9fba75
Marijn Haverbeke <marijnh@gmail.com>
2012-08-08 11:02:09 +0200

    [javascript mode] Fix poor handling of : punctuation
    Closes #708

--- mode/javascript/javascript.js
+++ mode/javascript/javascript.js
@@ -243,7 +243,7 @@ CodeMirror.defineMode("javascript", function(config, parserConfig) {
     
   function maybeoperator(type, value) {
     if (type == "operator" && /\+\+|--/.test(value)) return cont(maybeoperator);
-    if (type == "operator" || type == ":") return cont(expression);
+    if (type == "operator" && value == "?") return cont(expression, expect(":"), expression);
     if (type == ";") return;
     if (type == "(") return cont(pushlex(")"), commasep(expression, ")"), poplex, maybeoperator);
     if (type == ".") return cont(property, maybeoperator);
