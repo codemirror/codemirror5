47c84585281b284c0bbbd22e09a17611a94de631
Peter Kroon <plakroon@gmail.com>
2012-08-11 19:23:21 +0300

    [less mode] Improve highlighting

--- mode/less/less.js
+++ mode/less/less.js
@@ -7,8 +7,8 @@
 CodeMirror.defineMode("less", function(config) {
   var indentUnit = config.indentUnit, type;
   function ret(style, tp) {type = tp; return style;}
-  //html5 tags
-  var tags = ["a","abbr","acronym","address","applet","area","article","aside","audio","b","base","basefont","bdi","bdo","big","blockquote","body","br","button","canvas","caption","cite","code","col","colgroup","command","datalist","dd","del","details","dfn","dir","div","dl","dt","em","embed","fieldset","figcaption","figure","font","footer","form","frame","frameset","h1","h2","h3","h4","h5","h6","head","header","hgroup","hr","html","i","iframe","img","input","ins","keygen","kbd","label","legend","li","link","map","mark","menu","meta","meter","nav","noframes","noscript","object","ol","optgroup","option","output","p","param","pre","progress","q","rp","rt","ruby","s","samp","script","section","select","small","source","span","strike","strong","style","sub","summary","sup","table","tbody","td","textarea","tfoot","th","thead","time","title","tr","track","tt","u","ul","var","video","wbr"];
+  //html tags
+  var tags = "a abbr acronym address applet area article aside audio b base basefont bdi bdo big blockquote body br button canvas caption cite code col colgroup command datalist dd del details dfn dir div dl dt em embed fieldset figcaption figure font footer form frame frameset h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins keygen kbd label legend li link map mark menu meta meter nav noframes noscript object ol optgroup option output p param pre progress q rp rt ruby s samp script section select small source span strike strong style sub summary sup table tbody td textarea tfoot th thead time title tr track tt u ul var video wbr".split(' ');
   
   function inTagsArray(val){
     for(var i=0; i<tags.length; i++)if(val === tags[i])return true;
@@ -152,6 +152,22 @@ CodeMirror.defineMode("less", function(config) {
         return ret("string", "string");
       }else if(type == "|" || stream.current() == "-" || type == "["){
         return ret(null, ch);
+      }else if(stream.peek() == ":") {
+        stream.next();
+        var t_v = stream.peek() == ":" ? true : false;
+        if(!t_v){
+      	  var old_pos = stream.pos;
+      	  var sc = stream.current().length;
+      	  stream.eatWhile(/[a-z\\\-]/);
+      	  var new_pos = stream.pos;
+      	  if(stream.current().substring(sc-1).match(selectors) != null){
+      	    stream.backUp(new_pos-(old_pos-1));
+      		return ret("tag", "tag");
+      	  } else stream.backUp(new_pos-(old_pos-1));
+      	}else{
+      	  stream.backUp(1);	
+      	}
+      	if(t_v)return ret("tag", "tag"); else return ret("variable", "variable");
       }else{		
         return ret("variable", "variable");		
       }
