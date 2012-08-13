codemirror/codemirror5

commit 76dd7c6475e71fce142ce75c6acce61ce2ad1ea0

Change autofocus rules in fromTextArea
It now focuses the new editor only if the textarea argument
currently has focus.

Closes #714

lib/codemirror.js
@@ -2258,8 +2258,8 @@ var CodeMirror = (function() {
     options.value = textarea.value;
     if (!options.tabindex && textarea.tabindex)
       options.tabindex = textarea.tabindex;
-    if (options.autofocus == null && textarea.getAttribute("autofocus") != null)
-      options.autofocus = true;
+    if (options.autofocus == null)
+      try { if (document.activeElement == textarea) options.autofocus = true; } catch(e) {}
 
     function save() {textarea.value = instance.getValue();}
     if (textarea.form) {

commit 9a1917abd4c94696091a3dd340c55346a0353b92

Fix issues introduced in 76dd7c6475e71fce142ce75c6acce61ce2ad1ea0 and update `autofocus` documentation.

doc/manual.html
@@ -290,7 +290,10 @@
       When <a href="#fromTextArea"><code>fromTextArea</code></a> is
       used, and no explicit value is given for this option, it will
       inherit the setting from the textarea's <code>autofocus</code>
-      attribute.</dd>
+      attribute, but if another element already has focus because of
+      a script, an <code>autofocus</code> attribute, or user
+      interaction, CodeMirror will leave that element focused, rather
+      than focusing itself.</dd>
 
       <dt id="option_dragDrop"><code>dragDrop (boolean)</code></dt>
       <dd>Controls whether drag-and-drop is enabled. On by default.</dd>

lib/codemirror.js
@@ -2259,8 +2259,20 @@ var CodeMirror = (function() {
     options.value = textarea.value;
     if (!options.tabindex && textarea.tabindex)
       options.tabindex = textarea.tabindex;
+    // Focus editor if the textarea is focused
     if (options.autofocus == null)
       try { if (document.activeElement == textarea) options.autofocus = true; } catch(e) {}
+    // Focus editor if autofocus attribute is found
+    if (options.autofocus == null && textarea.getAttribute("autofocus") != null) {
+      options.autofocus = true;
+      // IE throws unspecified error in certain cases, when
+      // trying to access activeElement before onload
+      try {
+        // Remove autofocus if another element is already focused
+        if (document.activeElement != document.body && document.activeElement != textarea)
+          options.autofocus = false;
+      } catch(e) {}
+    }
 
     function save() {textarea.value = instance.getValue();}
     if (textarea.form) {