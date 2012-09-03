codemirror/codemirror5

commit 6ba24813c96687dac2f23db100780bc42e6ed808

Save overhead of operation when polling doesn't find a change

lib/codemirror.js
@@ -911,21 +911,17 @@ window.CodeMirror = (function() {
     function slowPoll() {
       if (pollingFast) return;
       poll.set(options.pollInterval, function() {
-        startOperation();
         readInput();
         if (focused) slowPoll();
-        endOperation();
       });
     }
     function fastPoll() {
       var missed = false;
       pollingFast = true;
       function p() {
-        startOperation();
         var changed = readInput();
         if (!changed && !missed) {missed = true; poll.set(60, p);}
         else {pollingFast = false; slowPoll();}
-        endOperation();
       }
       poll.set(20, p);
     }
@@ -940,6 +936,7 @@ window.CodeMirror = (function() {
       if (!focused || hasSelection(input) || options.readOnly) return false;
       var text = input.value;
       if (text == prevInput) return false;
+      startOperation();
       shiftSelecting = null;
       var same = 0, l = Math.min(prevInput.length, text.length);
       while (same < l && prevInput[same] == text[same]) ++same;
@@ -950,6 +947,7 @@ window.CodeMirror = (function() {
       replaceSelection(text.slice(same), "end");
       if (text.length > 1000) { input.value = prevInput = ""; }
       else prevInput = text;
+      endOperation();
       return true;
     }
     function resetInput(user) {

commit 4ac5b7aec8c998e21612796e18bfeb9a8aa7f393

Fix corruption of operation data introduced by 6ba24813c96687dac2f23db100780bc42e6ed808

lib/codemirror.js
@@ -933,7 +933,7 @@ window.CodeMirror = (function() {
       if (!focused || hasSelection(input) || options.readOnly) return false;
       var text = input.value;
       if (text == prevInput) return false;
-      startOperation();
+      if (!nestedOperation) startOperation();
       shiftSelecting = null;
       var same = 0, l = Math.min(prevInput.length, text.length);
       while (same < l && prevInput[same] == text[same]) ++same;
@@ -944,7 +944,7 @@ window.CodeMirror = (function() {
       replaceSelection(text.slice(same), "end");
       if (text.length > 1000) { input.value = prevInput = ""; }
       else prevInput = text;
-      endOperation();
+      if (!nestedOperation) endOperation();
       return true;
     }
     function resetInput(user) {