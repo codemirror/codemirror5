75ff456150135ea42b1d9602bf35f3c0b12b9dc0
Brandon Frohs <bfrohs@gmail.com>
2012-09-03 05:54:29 -0400

    [markdown mode] Improvements to parsing and tester
    (Squashed commit. Below are the original commit messages:)
    
    - Add highlighting test for Markdown for consecutive backticks.
    
    - Make it possible to wrap inline code in multiple backticks.
    
    - Add highlighting test for Markdown mode to only allow a single space
      to separate brackets.
    
    - Make it possible to separate bracket groups with a space.
    
    - Allow for horizontal rules to use dashes.
    
    - Allow for *any* number of -'s or ='s for setex headers.
    
    - Remove requirement that the text for setext headers have to be
      highlighted.
    
    - Might revisit someday to work on this, but in the meantime, it's
      better to prevent regressions than to be picky about what would be
      highlighted in an ideal world.
    
    - Correctly match single line footnotes with title.
    
    - Match inline links in Markdown mode.
    
    - Don't match unclosed code blocks.
    
    - Get mode test highlighter in line with CodeMirror.highlight().
    
    - It would be nice to use all native functions, to avoid having to update this manually each time.
    
    - Fix tests per fixes made to mode test highlighter.
    
    - Properly handle multiple classes for mode tests.
    
    - More correct handling of EM and STRONG in Markdown mode.
    
      Per Markdown documentation: "You can use whichever style you prefer;
      the lone restriction is that the same character must be used to open
      and close an emphasis span." # This is the 15th commit message:
    
    - Allow escaping of * and _ by surrounding with spaces.
    
      Per Markdown documentation:
    
      "But if you surround an * or _ with spaces, it’ll be treated as a
      literal asterisk or underscore."
    
    - More correct support for inline code blocks.
    
    - Adjust Markdown highlighting tests to fit readability, rather than
      100% conformance with Markdown documentation.
    
      Although unclosed italics, bold, and inline code would not be
      formatted by a Markdown parser, it is extremely helpful to have the
      styles added even when it is not yet closed. It may be a good idea
      to add an 'incomplete' flag for each at some point, in order to
      style it (and mark it) as different. Perhaps piggyback off of
      "error".

--- mode/markdown/markdown.js
+++ mode/markdown/markdown.js
@@ -2,12 +2,18 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
 
   var htmlFound = CodeMirror.mimeModes.hasOwnProperty("text/html");
   var htmlMode = CodeMirror.getMode(cmCfg, htmlFound ? "text/html" : "text/plain");
+  
+  var codeDepth = 0;
+  var prevLineHasContent = false
+  ,   thisLineHasContent = false;
 
   var header   = 'header'
   ,   code     = 'comment'
   ,   quote    = 'quote'
   ,   list     = 'string'
   ,   hr       = 'hr'
+  ,   linkinline = 'link'
+  ,   linkemail = 'link'
   ,   linktext = 'link'
   ,   linkhref = 'string'
   ,   em       = 'em'
@@ -17,8 +23,8 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
   var hrRE = /^([*\-=_])(?:\s*\1){2,}\s*$/
   ,   ulRE = /^[*\-+]\s+/
   ,   olRE = /^[0-9]+\.\s+/
-  ,   headerRE = /^(?:\={3,}|-{3,})$/
-  ,   textRE = /^[^\[*_\\<>`]+/;
+  ,   headerRE = /^(?:\={1,}|-{1,})$/
+  ,   textRE = /^[^\[*_\\<>` ]+/;
 
   function switchInline(stream, state, f) {
     state.f = state.inline = f;
@@ -34,6 +40,8 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
   // Blocks
 
   function blankLine(state) {
+    // Reset CODE state
+    state.code = false;
     // Reset EM state
     state.em = false;
     // Reset STRONG state
@@ -55,7 +63,7 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
       return code;
     } else if (stream.eatSpace()) {
       return null;
-    } else if (stream.peek() === '#' || stream.match(headerRE)) {
+    } else if (stream.peek() === '#' || (prevLineHasContent && stream.match(headerRE)) ) {
       state.header = true;
     } else if (stream.eat('>')) {
       state.indentation++;
@@ -94,6 +102,8 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
     if (state.strong) { styles.push(state.em ? emstrong : strong); }
     else if (state.em) { styles.push(em); }
     
+    if (state.code) { styles.push(code); }
+    
     if (state.header) { styles.push(header); }
     if (state.quote) { styles.push(quote); }
 
@@ -118,12 +128,39 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
       stream.next();
       return getType(state);
     }
+    
     if (ch === '`') {
-      return switchInline(stream, state, inlineElement(code, '`'));
+      var t = getType(state);
+      var before = stream.pos;
+      stream.eatWhile('`');
+      var difference = 1 + stream.pos - before;
+      if (!state.code) {
+        codeDepth = difference;
+        state.code = true;
+        return getType(state);
+      } else {
+        if (difference === codeDepth) { // Must be exact
+          state.code = false;
+          return t;
+        }
+        return getType(state);
+      }
+    } else if (state.code) {
+      return getType(state);
     }
-    if (ch === '[' && stream.match(/.*\](?:\(|\[)/, false)) {
+    
+    if (ch === '[' && stream.match(/.*\] ?(?:\(|\[)/, false)) {
       return switchInline(stream, state, linkText);
     }
+    
+    if (ch === '<' && stream.match(/^(https?|ftps?):\/\/(?:[^\\>]|\\.)+>/, true)) {
+      return switchInline(stream, state, inlineElement(linkinline, '>'));
+    }
+    
+    if (ch === '<' && stream.match(/^[^> \\]+@(?:[^\\>]|\\.)+>/, true)) {
+      return switchInline(stream, state, inlineElement(linkemail, '>'));
+    }
+    
     if (ch === '<' && stream.match(/^\w/, false)) {
       var md_inside = false;
       if (stream.string.indexOf(">")!=-1) {
@@ -143,10 +180,27 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
       
     var t = getType(state);
     if (ch === '*' || ch === '_') {
-      if (stream.eat(ch)) {
-        return (state.strong = !state.strong) ? getType(state) : t;
+      if (state.strong === ch && stream.eat(ch)) { // Remove STRONG
+        state.strong = false;
+        return t;
+      } else if (!state.strong && stream.eat(ch)) { // Add STRONG
+        state.strong = ch;
+        return getType(state);
+      } else if (state.em === ch) { // Remove EM
+        state.em = false;
+        return t;
+      } else if (!state.em) { // Add EM
+        state.em = ch;
+        return getType(state);
       }
-      return (state.em = !state.em) ? getType(state) : t;
+    } else if (ch === ' ') {
+      if (stream.eat('*') || stream.eat('_')) { // Probably surrounded by spaces
+        if (stream.peek() === ' ') { // Surrounded by spaces, ignore
+          return getType(state);
+        } else { // Not surrounded by spaces, back up pointer
+          stream.backUp(1);
+        }
+      }
     }
     
     return getType(state);
@@ -165,7 +219,10 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
   }
 
   function linkHref(stream, state) {
-    stream.eatSpace();
+    // Check if space, and return NULL if so (to avoid marking the space)
+    if(stream.eatSpace()){
+      return null;
+    }
     var ch = stream.next();
     if (ch === '(' || ch === '[') {
       return switchInline(stream, state, inlineElement(linkhref, ch === '(' ? ')' : ']'));
@@ -182,17 +239,22 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
   }
 
   function footnoteUrl(stream, state) {
-    stream.eatSpace();
-    stream.match(/^[^\s]+/, true);
+    // Check if space, and return NULL if so (to avoid marking the space)
+    if(stream.eatSpace()){
+      return null;
+    }
+    stream.match(/^[^\s]+(?:\s+(?:"(?:[^"\\]|\\\\|\\.)+"|'(?:[^'\\]|\\\\|\\.)+'|\((?:[^)\\]|\\\\|\\.)+\)))?/, true);
     state.f = state.inline = inlineNormal;
     return linkhref;
   }
 
   function inlineRE(endChar) {
     if (!inlineRE[endChar]) {
-      // match any not-escaped-non-endChar and any escaped char
-      // then match endChar or eol
-      inlineRE[endChar] = new RegExp('^(?:[^\\\\\\' + endChar + ']|\\\\.)*(?:\\' + endChar + '|$)');
+      // Escape endChar for RegExp (taken from http://stackoverflow.com/a/494122/526741)
+      endChar = (endChar+'').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+      // Match any non-endChar, escaped character, as well as the closing 
+      // endChar.
+      inlineRE[endChar] = new RegExp('^(?:[^\\\\]+?|\\\\.)*?(' + endChar + ')');
     }
     return inlineRE[endChar];
   }
@@ -244,7 +306,16 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
 
     token: function(stream, state) {
       if (stream.sol()) {
-        if (stream.match(/^\s*$/, true)) { return blankLine(state); }
+        if (stream.match(/^\s*$/, true)) {
+          prevLineHasContent = false;
+          return blankLine(state);
+        } else {
+          if(thisLineHasContent){
+            prevLineHasContent = true;
+            thisLineHasContent = false;
+          }
+          thisLineHasContent = true;
+        }
 
         // Reset state.header
         state.header = false;
