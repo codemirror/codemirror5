82702d5306d602da3ddcc4bde07ecda327c64f29
ks-ifware <kevin.senn@interfaceware.com>
2012-09-20 17:03:02 -0300

    Fixed multi-line Lua comment recognition
    Multi-line Lua comments start with "--[[", not "--["

--- mode/lua/lua.js
+++ mode/lua/lua.js
@@ -64,7 +64,7 @@ CodeMirror.defineMode("lua", function(config, parserConfig) {
   function normal(stream, state) {
     var ch = stream.next();
     if (ch == "-" && stream.eat("-")) {
-      if (stream.eat("["))
+      if (stream.eat("[") && stream.eat("["))
         return (state.cur = bracketed(readBracket(stream), "comment"))(stream, state);
       stream.skipToEnd();
       return "comment";
