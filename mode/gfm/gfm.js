27cc5eaa8b6c870da4a22321ac5eedcc0aa6bd3d
Brandon Frohs <bfrohs@gmail.com>
2012-10-03 00:31:26 -0400

    [Markdown] [GFM] Rewrite GFM to inherit changes from Markdown mode better.
    - Add options to Markdown mode to make it easier to extend
    - Add GFM text overlay
      - SHA, User@SHA, and User/Project@SHA
      - #Num, User/#Num, and User/Project#Num
      - Vanilla URLs
    - Add GFM-specific tests
    - Fix overlay code so blankLine() is called
    - Fix GFM for v3

--- mode/gfm/gfm.js
+++ mode/gfm/gfm.js
@@ -1,150 +1,94 @@
 CodeMirror.defineMode("gfm", function(config, parserConfig) {
-  var mdMode = CodeMirror.getMode(config, "markdown");
-  var aliases = {
-    html: "htmlmixed",
-    js: "javascript",
-    json: "application/json",
-    c: "text/x-csrc",
-    "c++": "text/x-c++src",
-    java: "text/x-java",
-    csharp: "text/x-csharp",
-    "c#": "text/x-csharp"
-  };
-
-  // make this lazy so that we don't need to load GFM last
-  var getMode = (function () {
-    var i, modes = {}, mimes = {}, mime;
-
-    var list = CodeMirror.listModes();
-    for (i = 0; i < list.length; i++) {
-      modes[list[i]] = list[i];
-    }
-    var mimesList = CodeMirror.listMIMEs();
-    for (i = 0; i < mimesList.length; i++) {
-      mime = mimesList[i].mime;
-      mimes[mime] = mimesList[i].mime;
-    }
-
-    for (var a in aliases) {
-      if (aliases[a] in modes || aliases[a] in mimes)
-        modes[a] = aliases[a];
-    }
-    
-    return function (lang) {
-      return modes[lang] ? CodeMirror.getMode(config, modes[lang]) : null;
-    };
-  }());
-
-  function markdown(stream, state) {
-    // intercept fenced code blocks
-    if (stream.sol() && stream.match(/^```([\w+#]*)/)) {
-      // try switching mode
-      state.localMode = getMode(RegExp.$1);
-      if (state.localMode)
-        state.localState = state.localMode.startState();
-
-      state.token = local;
-      return 'code';
-    }
-
-    return mdMode.token(stream, state.mdState);
+  var codeDepth = 0;
+  function blankLine(state) {
+    state.code = false;
+    return null;
   }
-
-  function local(stream, state) {
-    if (stream.sol() && stream.match(/^```/)) {
-      state.localMode = state.localState = null;
-      state.token = markdown;
-      return 'code';
-    }
-    else if (state.localMode) {
-      return state.localMode.token(stream, state.localState);
-    } else {
-      stream.skipToEnd();
-      return 'code';
-    }
-  }
-
-  // custom handleText to prevent emphasis in the middle of a word
-  // and add autolinking
-  function handleText(stream, mdState) {
-    var match;
-    if (stream.match(/^\w+:\/\/\S+/)) {
-      return 'link';
-    }
-    if (stream.match(/^[^\[*\\<>` _][^\[*\\<>` ]*[^\[*\\<>` _]/)) {
-      return mdMode.getType(mdState);
-    }
-    if (match = stream.match(/^[^\[*\\<>` ]+/)) {
-      var word = match[0];
-      if (word[0] === '_' && word[word.length-1] === '_') {
-        stream.backUp(word.length);
-        return undefined;
-      }
-      return mdMode.getType(mdState);
-    }
-    if (stream.eatSpace()) {
-      return null;
-    }
-  }
-
-  return {
+  var gfmOverlay = {
     startState: function() {
-      var mdState = mdMode.startState();
-      mdState.text = handleText;
-      return {token: markdown, mode: "markdown", mdState: mdState,
-              localMode: null, localState: null};
+      return {
+        code: false,
+        codeBlock: false,
+        ateSpace: false
+      };
     },
-
-    copyState: function(state) {
-      return {token: state.token, mdState: CodeMirror.copyState(mdMode, state.mdState),
-              localMode: state.localMode,
-              localState: state.localMode ? CodeMirror.copyState(state.localMode, state.localState) : null};
+    copyState: function(s) {
+      return {
+        code: s.code,
+        codeBlock: s.codeBlock,
+        ateSpace: s.ateSpace
+      };
     },
-
     token: function(stream, state) {
-        /* Parse GFM double bracket links */
-        var ch;
-        if ((ch = stream.peek()) != undefined && ch == '[') {
-            stream.next(); // Advance the stream
-
-            /* Only handle double bracket links */
-            if ((ch = stream.peek()) == undefined || ch != '[') {
-                stream.backUp(1);
-                return state.token(stream, state);
-            } 
-
-            while ((ch = stream.next()) != undefined && ch != ']') {}
-
-            if (ch == ']' && (ch = stream.next()) != undefined && ch == ']') 
-                return 'link';
-
-            /* If we did not find the second ']' */
-            stream.backUp(1);
+      // Hack to prevent formatting override inside code blocks (block and inline)
+      if (state.codeBlock) {
+        if (stream.match(/^```/)) {
+          state.codeBlock = false;
+          return null;
         }
-
-        /* Match GFM latex formulas, as well as latex formulas within '$' */
-        if (stream.match(/^\$[^\$]+\$/)) {
-            return "string";
+        stream.skipToEnd();
+        return null;
+      }
+      if (stream.sol()) {
+        state.code = false;
+      }
+      if (stream.sol() && stream.match(/^```/)) {
+        stream.skipToEnd();
+        state.codeBlock = true;
+        return null;
+      }
+      // If this block is changed, it may need to be updated in Markdown mode
+      if (stream.peek() === '`') {
+        stream.next();
+        var before = stream.pos;
+        stream.eatWhile('`');
+        var difference = 1 + stream.pos - before;
+        if (!state.code) {
+          codeDepth = difference;
+          state.code = true;
+        } else {
+          if (difference === codeDepth) { // Must be exact
+            state.code = false;
+          }
         }
-
-        if (stream.match(/^\\\((.*?)\\\)/)) {
-            return "string";
+        return null;
+      } else if (state.code) {
+        stream.next();
+        return null;
+      }
+      // Check if space. If so, links can be formatted later on
+      if (stream.eatSpace()) {
+        state.ateSpace = true;
+        return null;
+      }
+      if (stream.sol() || state.ateSpace) {
+        state.ateSpace = false;
+        if(stream.match(/^(?:[a-zA-Z0-9\-_]+\/)?(?:[a-zA-Z0-9\-_]+@)?(?:[a-f0-9]{7,40}\b)/)) {
+          // User/Project@SHA
+          // User@SHA
+          // SHA
+          return "link";
+        } else if (stream.match(/^(?:[a-zA-Z0-9\-_]+\/)?(?:[a-zA-Z0-9\-_]+)?#[0-9]+\b/)) {
+          // User/Project#Num
+          // User#Num
+          // #Num
+          return "link";
         }
-
-        if (stream.match(/^\$\$[^\$]+\$\$/)) {
-            return "string";
-        }
-        
-        if (stream.match(/^\\\[(.*?)\\\]/)) {
-            return "string";
-        }
-
-        return state.token(stream, state);
+      }
+      if (stream.match(/^((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i)) {
+        // URLs
+        // Taken from http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+        return "link";
+      }
+      stream.next();
+      return null;
     },
-
-    innerMode: function(state) {
-      if (state.token == markdown) return {state: state.mdState, mode: mdMode};
-      else return {state: state.localState, mode: state.localMode};
-    }
+    blankLine: blankLine
   };
-}, "markdown");
+  CodeMirror.defineMIME("gfmBase", {
+    name: "markdown",
+    underscoresBreakWords: false,
+    fencedCodeBlocks: true
+  });
+  return CodeMirror.overlayMode(CodeMirror.getMode(config, "gfmBase"), gfmOverlay);
+});
