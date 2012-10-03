adc7cdcf4a48bc0a343067b233c44ae8a9ca8ec4
Marijn Haverbeke <marijnh@gmail.com>
2012-10-03 22:28:47 +0200

    Fix regression due to  being removed.
    Closes #868

--- mode/rst/rst.js
+++ mode/rst/rst.js
@@ -18,17 +18,7 @@ CodeMirror.defineMode('rst', function(config, options) {
     }
 
     function hasMode(mode) {
-        if (mode) {
-            var modes = CodeMirror.listModes();
-
-            for (var i in modes) {
-                if (modes[i] == mode) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
+        return mode && CodeMirror.modes.hasOwnProperty(mode);
     }
 
     function getMode(mode) {
