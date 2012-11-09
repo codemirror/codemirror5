630adaf69243569b495872521aedfa7914e9f22f
Marijn Haverbeke <marijnh@gmail.com>
2012-11-09 17:11:51 +0100

    Refine indentLine behavior to leave multi-line comments alone

--- mode/clike/clike.js
+++ mode/clike/clike.js
@@ -140,8 +140,7 @@ CodeMirror.defineMode("clike", function(config, parserConfig) {
     },
 
     indent: function(state, textAfter) {
-      if (state.tokenize == tokenComment) return CodeMirror.Pass;
-      if (state.tokenize != tokenBase && state.tokenize != null) return 0;
+      if (state.tokenize != tokenBase && state.tokenize != null) return CodeMirror.Pass;
       var ctx = state.context, firstChar = textAfter && textAfter.charAt(0);
       if (ctx.type == "statement" && firstChar == "}") ctx = ctx.prev;
       var closing = firstChar == ctx.type;
