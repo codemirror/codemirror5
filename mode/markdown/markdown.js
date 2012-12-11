codemirror/codemirror5

commit 984e15502fb54aebdf6c272910a7543ed1efe0e7

[markdown mode] Fix highlighting for code blocks with internal indentation.

mode/markdown/markdown.js
@@ -374,6 +374,9 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
 
         state.f = state.block;
         var indentation = stream.match(/^\s*/, true)[0].replace(/\t/g, '    ').length;
+        var difference = Math.floor((indentation - state.indentation) / 4) * 4;
+        if (difference > 4) difference = 4;
+        indentation = state.indentation + difference;
         state.indentationDiff = indentation - state.indentation;
         state.indentation = indentation;
         if (indentation > 0) { return null; }

mode/markdown/test.js
@@ -20,6 +20,36 @@ MT.testMode(
     'comment', 'foo'
   ]
 );
+// Code blocks using 4 spaces with internal indentation
+MT.testMode(
+  'codeBlocksUsing4SpacesIndentation',
+  '    bar\n        hello\n            world\n    foo\nbar',
+  [
+    null, '    ',
+    'comment', 'bar',
+    null, '        ',
+    'comment', 'hello',
+    null, '            ',
+    'comment', 'world',
+    null, '    ',
+    'comment', 'foo',
+    null, 'bar'
+  ]
+);
+// Code blocks using 4 spaces with internal indentation
+MT.testMode(
+  'codeBlocksUsing4SpacesIndentation',
+  ' foo\n    bar\n        hello\n    world',
+  [
+    null, ' foo',
+    null, '    ',
+    'comment', 'bar',
+    null, '        ',
+    'comment', 'hello',
+    null, '    ',
+    'comment', 'world'
+  ]
+);
 
 // Code blocks using 1 tab (regardless of CodeMirror.indentWithTabs value)
 MT.testMode(
@@ -582,6 +612,24 @@ MT.testMode(
     'string', '+ bar',
     null, '            ',
     'comment', 'hello'
+  ]
+);
+// Code with internal indentation
+MT.testMode(
+  'listCodeIndentation',
+  '* foo\n\n        bar\n            hello\n                world\n        foo\n    bar',
+  [
+    'string', '* foo',
+    null, '        ',
+    'comment', 'bar',
+    null, '            ',
+    'comment', 'hello',
+    null, '                ',
+    'comment', 'world',
+    null, '        ',
+    'comment', 'foo',
+    null, '    ',
+    'string', 'bar'
   ]
 );
 // Code followed by text

commit f8c0ba16c13beff0e9a0a9e004e46129e10373aa

[markdown] Prevent leading spaces from being styled.
Regression introduced in 984e15502fb54aebdf6c272910a7543ed1efe0e7.

mode/markdown/markdown.js
@@ -456,10 +456,10 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
         var indentation = stream.match(/^\s*/, true)[0].replace(/\t/g, '    ').length;
         var difference = Math.floor((indentation - state.indentation) / 4) * 4;
         if (difference > 4) difference = 4;
-        indentation = state.indentation + difference;
-        state.indentationDiff = indentation - state.indentation;
-        state.indentation = indentation;
-        if (indentation > 0) { return null; }
+        var adjustedIndentation = state.indentation + difference;
+        state.indentationDiff = adjustedIndentation - state.indentation;
+        state.indentation = adjustedIndentation;
+        if (indentation > 0) return null;
       }
       return state.f(stream, state);
     },

mode/markdown/test.js
@@ -914,6 +914,17 @@ MT.testMode(
     'string', 'http://example.com/'
   ]
 );
+// Indented
+MT.testMode(
+  'labelIndented',
+  '   [foo]: http://example.com/',
+  [
+    null, '   ',
+    'link', '[foo]:',
+    null, ' ',
+    'string', 'http://example.com/'
+  ]
+);
 // Space in ID and title
 MT.testMode(
   'labelSpaceTitle',