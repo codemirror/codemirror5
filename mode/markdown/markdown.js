d2e6558e5495be2f67e115367ee6980d109a7826
Brandon Frohs <brandon@forewordreviews.com>
2012-12-11 10:49:21 -0500

    [markdown] Prevent leading spaces from being styled.
    Regression introduced in 984e15502fb54aebdf6c272910a7543ed1efe0e7.

--- mode/markdown/markdown.js
+++ mode/markdown/markdown.js
@@ -456,10 +456,10 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
         var indentation = stream.match(/^\s*/, true)[0].replace(/\t/g, '    ').length;
         var difference = Math.floor((indentation - state.indentation) / 4) * 4;
         if (difference > 4) difference = 4;
-        indentation = state.indentation + difference;
-        state.indentationDiff = indentation - state.indentation;
-        state.indentation = indentation;
-        if (indentation > 0) { return null; }
+        var adjustedIndentation = state.indentation + difference;
+        state.indentationDiff = adjustedIndentation - state.indentation;
+        state.indentation = adjustedIndentation;
+        if (indentation > 0) return null;
       }
       return state.f(stream, state);
     },
