codemirror/codemirror5

commit 47239048037dcfff0c4d34f2734d170f8330b13d

Support showIfHidden option in line widgets
Issue #1109

lib/codemirror.js
@@ -560,6 +560,17 @@ window.CodeMirror = (function() {
       if (nextIntact && nextIntact.to == lineNo) nextIntact = intact.shift();
       if (lineIsHidden(line)) {
         if (line.height != 0) updateLineHeight(line, 0);
+        if (line.widgets && cur.previousSibling) for (var i = 0; i < line.widgets.length; ++i)
+          if (line.widgets[i].showIfHidden) {
+            var prev = cur.previousSibling;
+            if (prev.nodeType == "pre") {
+              var wrap = elt("div", null, null, "position: relative");
+              prev.parentNode.replaceChild(wrap, prev);
+              wrap.appendChild(prev);
+              prev = wrap;
+            }
+            prev.appendChild(buildLineWidget(line.widgets[i], dims));
+          }
       } else if (nextIntact && nextIntact.from <= lineNo && nextIntact.to > lineNo) {
         // This line is intact. Skip to the actual node. Update its
         // line number if needed.
@@ -612,33 +623,36 @@ window.CodeMirror = (function() {
     if (line.bgClass)
       wrap.appendChild(elt("div", "\u00a0", line.bgClass + " CodeMirror-linebackground"));
     wrap.appendChild(lineElement);
-    if (line.widgets)
-      for (var i = 0, ws = line.widgets; i < ws.length; ++i) {
-        var widget = ws[i], node = elt("div", [widget.node], "CodeMirror-linewidget");
-        node.widget = widget;
-        if (widget.noHScroll) {
-          (wrap.alignable || (wrap.alignable = [])).push(node);
-          var width = dims.wrapperWidth;
-          node.style.left = dims.fixedPos + "px";
-          if (!widget.coverGutter) {
-            width -= dims.gutterTotalWidth;
-            node.style.paddingLeft = dims.gutterTotalWidth + "px";
-          }
-          node.style.width = width + "px";
-        }
-        if (widget.coverGutter) {
-          node.style.zIndex = 5;
-          node.style.position = "relative";
-          if (!widget.noHScroll) node.style.marginLeft = -dims.gutterTotalWidth + "px";
-        }
-        if (widget.above)
-          wrap.insertBefore(node, cm.options.lineNumbers && line.height != 0 ? gutterWrap : lineElement);
-        else
-          wrap.appendChild(node);
-      }
-
+    if (line.widgets) for (var i = 0, ws = line.widgets; i < ws.length; ++i) {
+      var widget = ws[i], node = buildLineWidget(widget, dims);
+      if (widget.above)
+        wrap.insertBefore(node, cm.options.lineNumbers && line.height != 0 ? gutterWrap : lineElement);
+      else
+        wrap.appendChild(node);
+    }
     if (ie_lt8) wrap.style.zIndex = 2;
     return wrap;
+  }
+
+  function buildLineWidget(widget, dims) {
+    var node = elt("div", [widget.node], "CodeMirror-linewidget");
+    node.widget = widget;
+    if (widget.noHScroll) {
+      (wrap.alignable || (wrap.alignable = [])).push(node);
+      var width = dims.wrapperWidth;
+      node.style.left = dims.fixedPos + "px";
+      if (!widget.coverGutter) {
+        width -= dims.gutterTotalWidth;
+        node.style.paddingLeft = dims.gutterTotalWidth + "px";
+      }
+      node.style.width = width + "px";
+    }
+    if (widget.coverGutter) {
+      node.style.zIndex = 5;
+      node.style.position = "relative";
+      if (!widget.noHScroll) node.style.marginLeft = -dims.gutterTotalWidth + "px";
+    }
+    return node;
   }
 
   // SELECTION / CURSOR

doc/manual.html
@@ -970,6 +970,9 @@
           <dt><code>above (boolean)</code></dt>
           <dd>Causes the widget to be placed above instead of below
           the text of the line.</dd>
+          <dt><code>showIfHidden (boolean)</code></dt>
+          <dd>When true, will cause the widget to be rendered even if
+          the line it is associated with is hidden.</dd>
         </dl>
       Note that the widget node will become a descendant of nodes with
       CodeMirror-specific CSS classes, and those classes might in some

commit 55d4f13cbd669cf4aaf5ef9b50044941bcfa1c61

Fix bug introduced in 47239048037dcfff0c4d34f2734d170f8330b13d

lib/codemirror.js
@@ -569,7 +569,7 @@ window.CodeMirror = (function() {
               wrap.appendChild(prev);
               prev = wrap;
             }
-            prev.appendChild(buildLineWidget(line.widgets[i], dims));
+            prev.appendChild(buildLineWidget(line.widgets[i], prev, dims));
           }
       } else if (nextIntact && nextIntact.from <= lineNo && nextIntact.to > lineNo) {
         // This line is intact. Skip to the actual node. Update its
@@ -624,7 +624,7 @@ window.CodeMirror = (function() {
       wrap.appendChild(elt("div", "\u00a0", line.bgClass + " CodeMirror-linebackground"));
     wrap.appendChild(lineElement);
     if (line.widgets) for (var i = 0, ws = line.widgets; i < ws.length; ++i) {
-      var widget = ws[i], node = buildLineWidget(widget, dims);
+      var widget = ws[i], node = buildLineWidget(widget, wrap, dims);
       if (widget.above)
         wrap.insertBefore(node, cm.options.lineNumbers && line.height != 0 ? gutterWrap : lineElement);
       else
@@ -634,7 +634,7 @@ window.CodeMirror = (function() {
     return wrap;
   }
 
-  function buildLineWidget(widget, dims) {
+  function buildLineWidget(widget, wrap, dims) {
     var node = elt("div", [widget.node], "CodeMirror-linewidget");
     node.widget = widget;
     if (widget.noHScroll) {