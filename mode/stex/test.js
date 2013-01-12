cd23459a9daf4ba88cf57af6569d4f7687ac1d6d
Marijn Haverbeke <marijnh@gmail.com>
2013-01-12 14:07:08 +0100

    [tests] Overhaul mode testing framework
    Tests are now specified in a less redundant format, and should be
    easier to read and write.

--- mode/stex/test.js
+++ mode/stex/test.js
@@ -1,343 +1,104 @@
-var MT = ModeTest;
-MT.modeName = 'stex';
-MT.modeOptions = {};
+(function() {
+  var mode = CodeMirror.getMode({tabSize: 4}, "stex");
+  function MT(name) { test.mode(name, mode, Array.prototype.slice.call(arguments, 1)); }
 
-MT.testMode(
-  'word',
-  'foo',
-  [
-    null, 'foo'
-  ]
-);
+  MT("word",
+     "foo");
 
-MT.testMode(
-  'twoWords',
-  'foo bar',
-  [
-    null, 'foo bar'
-  ]
-);
+  MT("twoWords",
+     "foo bar");
 
-MT.testMode(
-  'beginEndDocument',
-  '\\begin{document}\n\\end{document}',
-  [
-    'tag', '\\begin',
-    'bracket', '{',
-    'atom', 'document',
-    'bracket', '}',
-    'tag', '\\end',
-    'bracket', '{',
-    'atom', 'document',
-    'bracket', '}'
-  ]
-);
+  MT("beginEndDocument",
+     "[tag \\begin][bracket {][atom document][bracket }]",
+     "[tag \\end][bracket {][atom document][bracket }]");
 
-MT.testMode(
-  'beginEndEquation',
-  '\\begin{equation}\n  E=mc^2\n\\end{equation}',
-  [
-    'tag', '\\begin',
-    'bracket', '{',
-    'atom', 'equation',
-    'bracket', '}',
-    null, '  E=mc^2',
-    'tag', '\\end',
-    'bracket', '{',
-    'atom', 'equation',
-    'bracket', '}'
-  ]
-);
+  MT("beginEndEquation",
+     "[tag \\begin][bracket {][atom equation][bracket }]",
+     "  E=mc^2",
+     "[tag \\end][bracket {][atom equation][bracket }]");
 
-MT.testMode(
-  'beginModule',
-  '\\begin{module}[]',
-  [
-    'tag', '\\begin',
-    'bracket', '{',
-    'atom', 'module',
-    'bracket', '}[]'
-  ]
-);
+  MT("beginModule",
+     "[tag \\begin][bracket {][atom module][bracket }[[]]]");
 
-MT.testMode(
-  'beginModuleId',
-  '\\begin{module}[id=bbt-size]',
-  [
-    'tag', '\\begin',
-    'bracket', '{',
-    'atom', 'module',
-    'bracket', '}[',
-    null, 'id=bbt-size',
-    'bracket', ']'
-  ]
-);
+  MT("beginModuleId",
+     "[tag \\begin][bracket {][atom module][bracket }[[]id=bbt-size[bracket ]]]");
 
-MT.testMode(
-  'importModule',
-  '\\importmodule[b-b-t]{b-b-t}',
-  [
-    'tag', '\\importmodule',
-    'bracket', '[',
-    'string', 'b-b-t',
-    'bracket', ']{',
-    'builtin', 'b-b-t',
-    'bracket', '}'
-  ]
-);
+  MT("importModule",
+     "[tag \\importmodule][bracket [[][string b-b-t][bracket ]]{][builtin b-b-t][bracket }]");
 
-MT.testMode(
-  'importModulePath',
-  '\\importmodule[\\KWARCslides{dmath/en/cardinality}]{card}',
-  [
-    'tag', '\\importmodule',
-    'bracket', '[',
-    'tag', '\\KWARCslides',
-    'bracket', '{',
-    'string', 'dmath/en/cardinality',
-    'bracket', '}]{',
-    'builtin', 'card',
-    'bracket', '}'
-  ]
-);
+  MT("importModulePath",
+     "[tag \\importmodule][bracket [[][tag \\KWARCslides][bracket {][string dmath/en/cardinality][bracket }]]{][builtin card][bracket }]");
 
-MT.testMode(
-  'psForPDF',
-  '\\PSforPDF[1]{#1}', // could treat #1 specially
-  [
-    'tag', '\\PSforPDF',
-    'bracket', '[',
-    'atom', '1',
-    'bracket', ']{',
-    null, '#1',
-    'bracket', '}'
-  ]
-);
+  MT("psForPDF",
+     "[tag \\PSforPDF][bracket [[][atom 1][bracket ]]{]#1[bracket }]");
 
-MT.testMode(
-  'comment',
-  '% foo',
-  [
-    'comment', '% foo'
-  ]
-);
+  MT("comment",
+     "[comment % foo]");
 
-MT.testMode(
-  'tagComment',
-  '\\item% bar',
-  [
-    'tag', '\\item',
-    'comment', '% bar'
-  ]
-);
+  MT("tagComment",
+     "[tag \\item][comment % bar]");
 
-MT.testMode(
-  'commentTag',
-  ' % \\item',
-  [
-    null, ' ',
-    'comment', '% \\item'
-  ]
-);
+  MT("commentTag",
+     " [comment % \\item]");
 
-MT.testMode(
-  'commentLineBreak',
-  '%\nfoo',
-  [
-    'comment', '%',
-    null, 'foo'
-  ]
-);
+  MT("commentLineBreak",
+     "[comment %]",
+     "foo");
 
-MT.testMode(
-  'tagErrorCurly',
-  '\\begin}{',
-  [
-    'tag', '\\begin',
-    'error', '}',
-    'bracket', '{'
-  ]
-);
+  MT("tagErrorCurly",
+     "[tag \\begin][error }][bracket {]");
 
-MT.testMode(
-  'tagErrorSquare',
-  '\\item]{',
-  [
-    'tag', '\\item',
-    'error', ']',
-    'bracket', '{'
-  ]
-);
+  MT("tagErrorSquare",
+     "[tag \\item][error ]]][bracket {]");
 
-MT.testMode(
-  'commentCurly',
-  '% }',
-  [
-    'comment', '% }'
-  ]
-);
+  MT("commentCurly",
+     "[comment % }]");
 
-MT.testMode(
-  'tagHash',
-  'the \\# key',
-  [
-    null, 'the ',
-    'tag', '\\#',
-    null, ' key'
-  ]
-);
+  MT("tagHash",
+     "the [tag \\#] key");
 
-MT.testMode(
-  'tagNumber',
-  'a \\$5 stetson',
-  [
-    null, 'a ',
-    'tag', '\\$',
-  'atom', 5,
-  null, ' stetson'
-  ]
-);
+  MT("tagNumber",
+     "a [tag \\$][atom 5] stetson");
 
-MT.testMode(
-  'tagPercent',
-  '100\\% beef',
-  [
-    'atom', '100',
-    'tag', '\\%',
-    null, ' beef'
-  ]
-);
+  MT("tagPercent",
+     "[atom 100][tag \\%] beef");
 
-MT.testMode(
-  'tagAmpersand',
-  'L \\& N',
-  [
-    null, 'L ',
-    'tag', '\\&',
-    null, ' N'
-  ]
-);
+  MT("tagAmpersand",
+     "L [tag \\&] N");
 
-MT.testMode(
-  'tagUnderscore',
-  'foo\\_bar',
-  [
-    null, 'foo',
-    'tag', '\\_',
-    null, 'bar'
-  ]
-);
+  MT("tagUnderscore",
+     "foo[tag \\_]bar");
 
-MT.testMode(
-  'tagBracketOpen',
-  '\\emph{\\{}',
-  [
-    'tag', '\\emph',
-    'bracket', '{',
-    'tag', '\\{',
-    'bracket', '}'
-  ]
-);
+  MT("tagBracketOpen",
+     "[tag \\emph][bracket {][tag \\{][bracket }]");
 
-MT.testMode(
-  'tagBracketClose',
-  '\\emph{\\}}',
-  [
-    'tag', '\\emph',
-    'bracket', '{',
-    'tag', '\\}',
-    'bracket', '}'
-  ]
-);
+  MT("tagBracketClose",
+     "[tag \\emph][bracket {][tag \\}][bracket }]");
 
-MT.testMode(
-  'tagLetterNumber',
-  'section \\S1',
-  [
-    null, 'section ',
-    'tag', '\\S',
-    'atom', '1'
-  ]
-);
+  MT("tagLetterNumber",
+     "section [tag \\S][atom 1]");
 
-MT.testMode(
-  'textTagNumber',
-  'para \\P2',
-  [
-    null, 'para ',
-    'tag', '\\P',
-    'atom', '2'
-  ]
-);
+  MT("textTagNumber",
+     "para [tag \\P][atom 2]");
 
-MT.testMode(
-  'thinspace',
-  'x\\,y', // thinspace
-  [
-    null, 'x',
-    'tag', '\\,',
-    null, 'y'
-  ]
-);
+  MT("thinspace",
+     "x[tag \\,]y");
 
-MT.testMode(
-  'thickspace',
-  'x\\;y', // thickspace
-  [
-    null, 'x',
-    'tag', '\\;',
-    null, 'y'
-  ]
-);
+  MT("thickspace",
+     "x[tag \\;]y");
 
-MT.testMode(
-  'negativeThinspace',
-  'x\\!y', // negative thinspace
-  [
-    null, 'x',
-    'tag', '\\!',
-    null, 'y'
-  ]
-);
+  MT("negativeThinspace",
+     "x[tag \\!]y");
 
-MT.testMode(
-  'periodNotSentence',
-  'J.\\ L.\\ is', // period not ending a sentence
-  [
-    null, 'J.\\ L.\\ is'
-  ]
-); // maybe could be better
+  MT("periodNotSentence",
+     "J.\\ L.\\ is");
 
-MT.testMode(
-  'periodSentence',
-  'X\\@. The', // period ending a sentence
-  [
-    null, 'X',
-    'tag', '\\@',
-    null, '. The'
-  ]
-);
+  MT("periodSentence",
+     "X[tag \\@]. The");
 
-MT.testMode(
-  'italicCorrection',
-  '{\\em If\\/} I', // italic correction
-  [
-    'bracket', '{',
-    'tag', '\\em',
-    null, ' If',
-    'tag', '\\/',
-    'bracket', '}',
-    null, ' I'
-  ]
-);
+  MT("italicCorrection",
+     "[bracket {][tag \\em] If[tag \\/][bracket }] I");
 
-MT.testMode(
-  'tagBracket',
-  '\\newcommand{\\pop}',
-  [
-    'tag', '\\newcommand',
-    'bracket', '{',
-    'tag', '\\pop',
-    'bracket', '}'
-  ]
-);
+  MT("tagBracket",
+     "[tag \\newcommand][bracket {][tag \\pop][bracket }]");
+})();
--- mode/xquery/test.js
+++ mode/xquery/test.js
@@ -1,77 +1,64 @@
-// Initiate ModeTest and set defaults
-var MT = ModeTest;
-MT.modeName = "xquery";
-MT.modeOptions = {};
+// Don't take these too seriously -- the expected results appear to be
+// based on the results of actual runs without any serious manual
+// verification. If a change you made causes them to fail, the test is
+// as likely to wrong as the code.
 
-MT.testMode("eviltest",
-            'xquery version &quot;1.0-ml&quot;;\
-      (: this is\
-       : a \
-         "comment" :)\
-      let $let := &lt;x attr=&quot;value&quot;&gt;&quot;test&quot;&lt;func&gt;function() $var {function()} {$var}&lt;/func&gt;&lt;/x&gt;\
-      let $joe:=1\
-      return element element {\
-          attribute attribute { 1 },\
-          element test { &#39;a&#39; }, \
-          attribute foo { &quot;bar&quot; },\
-          fn:doc()[ foo/@bar eq $let ],\
-          //x }    \
-       \
-      (: a more \'evil\' test :)\
-      (: Modified Blakeley example (: with nested comment :) ... :)\
-      declare private function local:declare() {()};\
-      declare private function local:private() {()};\
-      declare private function local:function() {()};\
-      declare private function local:local() {()};\
-      let $let := &lt;let&gt;let $let := &quot;let&quot;&lt;/let&gt;\
-      return element element {\
-          attribute attribute { try { xdmp:version() } catch($e) { xdmp:log($e) } },\
-          attribute fn:doc { &quot;bar&quot; castable as xs:string },\
-          element text { text { &quot;text&quot; } },\
-          fn:doc()[ child::eq/(@bar | attribute::attribute) eq $let ],\
-          //fn:doc\
-      }', ["keyword","xquery",null," ","keyword","version",null," ","variable","&quot;1","keyword",".","atom","0","keyword","-","variable","ml&quot;","def variable",";",null,"      ","comment","(: this is       : a          \"comment\" :)",null,"      ","keyword","let",null," ","variable","$let",null," ","keyword",":=",null," ","variable","&lt;x",null," ","variable","attr","keyword","=","variable","&quot;value&quot;&gt;&quot;test&quot;&lt;func&gt","def variable",";function","","()",null," ","variable","$var",null," ","","{","keyword","function","","()}",null," ","","{","variable","$var","","}","variable","&lt;","keyword","/","variable","func&gt;&lt;","keyword","/","variable","x&gt;",null,"      ","keyword","let",null," ","variable","$joe","keyword",":=","atom","1",null,"      ","keyword","return",null," ","keyword","element",null," ","variable","element",null," ","","{",null,"          ","keyword","attribute",null," ","variable","attribute",null," ","","{",null," ","atom","1",null," ","","},",null,"          ","keyword","element",null," ","variable","test",null," ","","{",null," ","variable","&#39;a&#39;",null," ","","},",null,"           ","keyword","attribute",null," ","variable","foo",null," ","","{",null," ","variable","&quot;bar&quot;",null," ","","},",null,"          ","def variable","fn:doc","","()[",null," ","variable","foo","keyword","/","variable","@bar",null," ","keyword","eq",null," ","variable","$let",null," ","","],",null,"          ","keyword","//","variable","x",null," ","","}",null,"                 ","comment","(: a more 'evil' test :)",null,"      ","comment","(: Modified Blakeley example (: with nested comment :) ... :)",null,"      ","keyword","declare",null," ","keyword","private",null," ","keyword","function",null," ","def variable","local:declare","","()",null," ","","{()}","variable",";",null,"      ","keyword","declare",null," ","keyword","private",null," ","keyword","function",null," ","def variable","local:private","","()",null," ","","{()}","variable",";",null,"      ","keyword","declare",null," ","keyword","private",null," ","keyword","function",null," ","def variable","local:function","","()",null," ","","{()}","variable",";",null,"      ","keyword","declare",null," ","keyword","private",null," ","keyword","function",null," ","def variable","local:local","","()",null," ","","{()}","variable",";",null,"      ","keyword","let",null," ","variable","$let",null," ","keyword",":=",null," ","variable","&lt;let&gt;let",null," ","variable","$let",null," ","keyword",":=",null," ","variable","&quot;let&quot;&lt;","keyword","/let","variable","&gt;",null,"      ","keyword","return",null," ","keyword","element",null," ","variable","element",null," ","","{",null,"          ","keyword","attribute",null," ","variable","attribute",null," ","","{",null," ","keyword","try",null," ","","{",null," ","def variable","xdmp:version","","()",null," ","","}",null," ","keyword","catch","","(","variable","$e","",")",null," ","","{",null," ","def variable","xdmp:log","","(","variable","$e","",")",null," ","","}",null," ","","},",null,"          ","keyword","attribute",null," ","variable","fn:doc",null," ","","{",null," ","variable","&quot;bar&quot;",null," ","variable","castable",null," ","keyword","as",null," ","atom","xs:string",null," ","","},",null,"          ","keyword","element",null," ","variable","text",null," ","","{",null," ","keyword","text",null," ","","{",null," ","variable","&quot;text&quot;",null," ","","}",null," ","","},",null,"          ","def variable","fn:doc","","()[",null," ","qualifier","child::","variable","eq","keyword","/","","(","variable","@bar",null," ","keyword","|",null," ","qualifier","attribute::","variable","attribute","",")",null," ","keyword","eq",null," ","variable","$let",null," ","","],",null,"          ","keyword","//","variable","fn:doc",null,"      ","","}"]);
+(function() {
+  var mode = CodeMirror.getMode({tabSize: 4}, "xquery");
+  function MT(name) { test.mode(name, mode, Array.prototype.slice.call(arguments, 1)); }
 
-MT.testMode("testEmptySequenceKeyword",
-            '"foo" instance of empty-sequence()',
-            ["string","\"foo\"",null," ","keyword","instance",null," ","keyword","of",null," ","keyword","empty-sequence","","()"]);
+  MT("eviltest",
+     "[keyword xquery] [keyword version] [variable &quot;1][keyword .][atom 0][keyword -][variable ml&quot;][def&variable ;]      [comment (: this is       : a          \"comment\" :)]",
+     "      [keyword let] [variable $let] [keyword :=] [variable &lt;x] [variable attr][keyword =][variable &quot;value&quot;&gt;&quot;test&quot;&lt;func&gt][def&variable ;function]() [variable $var] {[keyword function]()} {[variable $var]}[variable &lt;][keyword /][variable func&gt;&lt;][keyword /][variable x&gt;]",
+     "      [keyword let] [variable $joe][keyword :=][atom 1]",
+     "      [keyword return] [keyword element] [variable element] {",
+     "          [keyword attribute] [variable attribute] { [atom 1] },",
+     "          [keyword element] [variable test] { [variable &#39;a&#39;] },           [keyword attribute] [variable foo] { [variable &quot;bar&quot;] },",
+     "          [def&variable fn:doc]()[[ [variable foo][keyword /][variable @bar] [keyword eq] [variable $let] ]],",
+     "          [keyword //][variable x] }                 [comment (: a more 'evil' test :)]",
+     "      [comment (: Modified Blakeley example (: with nested comment :) ... :)]",
+     "      [keyword declare] [keyword private] [keyword function] [def&variable local:declare]() {()}[variable ;]",
+     "      [keyword declare] [keyword private] [keyword function] [def&variable local:private]() {()}[variable ;]",
+     "      [keyword declare] [keyword private] [keyword function] [def&variable local:function]() {()}[variable ;]",
+     "      [keyword declare] [keyword private] [keyword function] [def&variable local:local]() {()}[variable ;]",
+     "      [keyword let] [variable $let] [keyword :=] [variable &lt;let&gt;let] [variable $let] [keyword :=] [variable &quot;let&quot;&lt;][keyword /let][variable &gt;]",
+     "      [keyword return] [keyword element] [variable element] {",
+     "          [keyword attribute] [variable attribute] { [keyword try] { [def&variable xdmp:version]() } [keyword catch]([variable $e]) { [def&variable xdmp:log]([variable $e]) } },",
+     "          [keyword attribute] [variable fn:doc] { [variable &quot;bar&quot;] [variable castable] [keyword as] [atom xs:string] },",
+     "          [keyword element] [variable text] { [keyword text] { [variable &quot;text&quot;] } },",
+     "          [def&variable fn:doc]()[[ [qualifier child::][variable eq][keyword /]([variable @bar] [keyword |] [qualifier attribute::][variable attribute]) [keyword eq] [variable $let] ]],",
+     "          [keyword //][variable fn:doc]",
+     "      }");
 
+  MT("testEmptySequenceKeyword",
+     "[string \"foo\"] [keyword instance] [keyword of] [keyword empty-sequence]()");
 
-MT.testMode("testMultiAttr",
-            '<p a1="foo" a2="bar">hello world</p>',
-            ["tag","<p ","attribute","a1","","=","string","\"foo\"",null," ","attribute","a2","","=","string","\"bar\"","tag",">","variable","hello",null," ","variable","world","tag","</p>"]);
+  MT("testMultiAttr",
+     "[tag <p ][attribute a1]=[string \"foo\"] [attribute a2]=[string \"bar\"][tag >][variable hello] [variable world][tag </p>]");
 
-MT.testMode("test namespaced variable",
-            'declare namespace e = "http://example.com/ANamespace";\
-declare variable $e:exampleComThisVarIsNotRecognized as element(*) external;',
-            ["keyword","declare",null," ","keyword","namespace",null," ","variable","e",null," ","keyword","=",null," ","string","\"http://example.com/ANamespace\"","variable",";declare",null," ","keyword","variable",null," ","variable","$e:exampleComThisVarIsNotRecognized",null," ","keyword","as",null," ","keyword","element","","(","keyword","*","",")",null," ","variable","external;"]);
+  MT("test namespaced variable",
+     "[keyword declare] [keyword namespace] [variable e] [keyword =] [string \"http://example.com/ANamespace\"][variable ;declare] [keyword variable] [variable $e:exampleComThisVarIsNotRecognized] [keyword as] [keyword element]([keyword *]) [variable external;]");
 
-MT.testMode("test EQName variable",
-            'declare variable $"http://www.example.com/ns/my":var := 12;\
-<out>{$"http://www.example.com/ns/my":var}</out>',
-            ["keyword","declare",null," ","keyword","variable",null," ","variable","$\"http://www.example.com/ns/my\":var",null," ","keyword",":=",null," ","atom","12","variable",";","tag","<out>","","{","variable","$\"http://www.example.com/ns/my\":var","","}","tag","</out>"]);
+  MT("test EQName variable",
+     "[keyword declare] [keyword variable] [variable $\"http://www.example.com/ns/my\":var] [keyword :=] [atom 12][variable ;]",
+     "[tag <out>]{[variable $\"http://www.example.com/ns/my\":var]}[tag </out>]");
 
-MT.testMode("test EQName function",
-            'declare function "http://www.example.com/ns/my":fn ($a as xs:integer) as xs:integer {\
-   $a + 2\
-};\
-<out>{"http://www.example.com/ns/my":fn(12)}</out>',
-            ["keyword","declare",null," ","keyword","function",null," ","def variable","\"http://www.example.com/ns/my\":fn",null," ","","(","variable","$a",null," ","keyword","as",null," ","atom","xs:integer","",")",null," ","keyword","as",null," ","atom","xs:integer",null," ","","{",null,"   ","variable","$a",null," ","keyword","+",null," ","atom","2","","}","variable",";","tag","<out>","","{","def variable","\"http://www.example.com/ns/my\":fn","","(","atom","12","",")}","tag","</out>"]);
+  MT("test EQName function",
+     "[keyword declare] [keyword function] [def&variable \"http://www.example.com/ns/my\":fn] ([variable $a] [keyword as] [atom xs:integer]) [keyword as] [atom xs:integer] {",
+     "   [variable $a] [keyword +] [atom 2]",
+     "}[variable ;]",
+     "[tag <out>]{[def&variable \"http://www.example.com/ns/my\":fn]([atom 12])}[tag </out>]");
 
-MT.testMode("test EQName function with single quotes",
-            'declare function \'http://www.example.com/ns/my\':fn ($a as xs:integer) as xs:integer {\
-   $a + 2\
-};\
-<out>{\'http://www.example.com/ns/my\':fn(12)}</out>',
-            ["keyword","declare",null," ","keyword","function",null," ","def variable","'http://www.example.com/ns/my':fn",null," ","","(","variable","$a",null," ","keyword","as",null," ","atom","xs:integer","",")",null," ","keyword","as",null," ","atom","xs:integer",null," ","","{",null,"   ","variable","$a",null," ","keyword","+",null," ","atom","2","","}","variable",";","tag","<out>","","{","def variable","'http://www.example.com/ns/my':fn","","(","atom","12","",")}","tag","</out>"]);
+  MT("test EQName function with single quotes",
+     "[keyword declare] [keyword function] [def&variable 'http://www.example.com/ns/my':fn] ([variable $a] [keyword as] [atom xs:integer]) [keyword as] [atom xs:integer] {",
+     "   [variable $a] [keyword +] [atom 2]",
+     "}[variable ;]",
+     "[tag <out>]{[def&variable 'http://www.example.com/ns/my':fn]([atom 12])}[tag </out>]");
 
-MT.testMode("testProcessingInstructions",
-            'data(<?target content?>) instance of xs:string',
-            ["def variable","data","","(","comment meta","<?target content?>","",")",null," ","keyword","instance",null," ","keyword","of",null," ","atom","xs:string"]);
+  MT("testProcessingInstructions",
+     "[def&variable data]([comment&meta <?target content?>]) [keyword instance] [keyword of] [atom xs:string]");
 
-MT.testMode("testQuoteEscapeDouble",
-            'let $rootfolder := "c:\\builds\\winnt\\HEAD\\qa\\scripts\\"\
-let $keysfolder := concat($rootfolder, "keys\\")\
-return\
-$keysfolder',
-            ["keyword","let",null," ","variable","$rootfolder",null," ","keyword",":=",null," ","string","\"c:\\builds\\winnt\\HEAD\\qa\\scripts\\\"","keyword","let",null," ","variable","$keysfolder",null," ","keyword",":=",null," ","def variable","concat","","(","variable","$rootfolder","",",",null," ","string","\"keys\\\"","",")","variable","return$keysfolder"]);
+  MT("testQuoteEscapeDouble",
+     "[keyword let] [variable $rootfolder] [keyword :=] [string \"c:\\builds\\winnt\\HEAD\\qa\\scripts\\\"]",
+     "[keyword let] [variable $keysfolder] [keyword :=] [def&variable concat]([variable $rootfolder], [string \"keys\\\"])");
+})();
