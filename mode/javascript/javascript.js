d01e3de0706d74ff92d420960b1398898441ed72
Marijn Haverbeke <marijnh@gmail.com>
2013-02-02 13:32:23 +0100

    [javascript mode] Fix parsing of binary operators

--- mode/javascript/javascript.js
+++ mode/javascript/javascript.js
@@ -111,8 +111,8 @@ CodeMirror.defineMode("javascript", function(config, parserConfig) {
       }
     }
     else if (ch == "#") {
-        stream.skipToEnd();
-        return ret("error", "error");
+      stream.skipToEnd();
+      return ret("error", "error");
     }
     else if (isOperatorChar.test(ch)) {
       stream.eatWhile(isOperatorChar);
@@ -283,8 +283,11 @@ CodeMirror.defineMode("javascript", function(config, parserConfig) {
   }
     
   function maybeoperator(type, value) {
-    if (type == "operator" && /\+\+|--/.test(value)) return cont(maybeoperator);
-    if (type == "operator" && value == "?") return cont(expression, expect(":"), expression);
+    if (type == "operator") {
+      if (/\+\+|--/.test(value)) return cont(maybeoperator);
+      if (value == "?") return cont(expression, expect(":"), expression);
+      return cont(expression);
+    }
     if (type == ";") return;
     if (type == "(") return cont(pushlex(")"), commasep(expression, ")"), poplex, maybeoperator);
     if (type == ".") return cont(property, maybeoperator);
