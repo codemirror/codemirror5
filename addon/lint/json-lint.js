50ab187f4ea611f32d396ada23fc680a26cafb0e
Marijn Haverbeke <marijnh@gmail.com>
2013-02-18 11:13:52 +0100

    [lint addon] Start integrating
    - Link to external jsonlint.js (don't want dependencies in repo if
      I can help it)
    
    - Use a single demo file (prevent demo/ from becoming a huge mess)
    
    - Make whitespace and newlines conform to other files, fix linter errors

--- addon/lint/json-lint.js
+++ addon/lint/json-lint.js
@@ -1,24 +1,20 @@
 (function() {
 
-	CodeMirror.jsonValidator = function(contents, collector, cm) {
-		try {
-			jsonlint.parseError = function(str, hash) {
+  CodeMirror.jsonValidator = function(contents, collector) {
+    jsonlint.parseError = function(str, hash) {
+      var severity = 'error';
+      var loc = hash.loc;
+      var lineStart = loc.first_line - 1;
+      var lineEnd = hash.line;// loc.last_line - 1;
+      var charStart = loc.first_column;
+      var charEnd = loc.last_column;
 
-				var severity = 'error';
-				var loc = hash.loc;
-				var lineStart = loc.first_line - 1;
-				var lineEnd = hash.line;// loc.last_line - 1;
-				var charStart = loc.first_column
-				var charEnd = loc.last_column;
+      collector.addAnnotation(severity, lineStart, charStart,
+			      lineEnd, charEnd, str);
 
-				collector.addAnnotation(severity, lineStart, charStart,
-						lineEnd, charEnd, str);
-
-			};
-			jsonlint.parse(contents);
-		} catch (parseException) {
-			//alert(parseException);
-		}
-	};
+    };
+    try { jsonlint.parse(contents); }
+    catch(e) {}
+  };
 
 })();
