262bf5264501cac42ac3d2fc957c1b80ef13894c
satchmorun <rulfzid@gmail.com>
2013-03-11 15:35:09 -0500

    [addon closebrackets] Add smart backspace behavior
    When any specified pair is empty, backspacing will also
    delete the closing bracket.

--- addon/edit/closebrackets.js
+++ addon/edit/closebrackets.js
@@ -10,7 +10,10 @@
   });
 
   function buildKeymap(pairs) {
-    var map = {name : "autoCloseBrackets"};
+    var map = {
+      name : "autoCloseBrackets",
+      Backspace: buildBackspacer(pairs)
+    };
     for (var i = 0; i < pairs.length; i += 2) (function(left, right) {
       function maybeOverwrite(cm) {
         var cur = cm.getCursor(), ahead = cm.getRange(cur, CodeMirror.Pos(cur.line, cur.ch + 1));
@@ -25,5 +28,22 @@
       if (left != right) map["'" + right + "'"] = maybeOverwrite;
     })(pairs.charAt(i), pairs.charAt(i + 1));
     return map;
+  }
+
+  function buildBackspacer(pairs) {
+    var pairmap = {};
+
+    for (var i = 0; i < pairs.length; i += 2)
+      pairmap[pairs.charAt(i)] = pairs.charAt(i + 1);
+
+    return function(cm) {
+      var cur = cm.getCursor(),
+          from = CodeMirror.Pos(cur.line, cur.ch - 1),
+          to = CodeMirror.Pos(cur.line, cur.ch + 1),
+          str = cm.getRange(from, to);
+
+      if (pairmap[str.charAt(0)] !== str.charAt(1)) return CodeMirror.Pass;
+      cm.replaceRange('', from, to);
+    };
   }
 })();
