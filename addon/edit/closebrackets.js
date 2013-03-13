db6242871c860c62f7c987edd032be2797378caf
Andrey Lushnikov <lushnikov@chromium.org>
2013-03-13 18:24:23 +0400

    Improve closebrackets.js addon
    - Do not insert closing bracket if a spacechar or closing bracket
      follows cursor
    - Do not handle backspace if selection is not empty
    - Do not overwrite anything if selection is not empty
    - If selection is not empty and open bracket is inserted - surround
      selection with brackets.

--- addon/edit/closebrackets.js
+++ addon/edit/closebrackets.js
@@ -1,5 +1,6 @@
 (function() {
   var DEFAULT_BRACKETS = "()[]{}''\"\"";
+  var SPACE_CHAR_REGEX = /\s/;
 
   CodeMirror.defineOption("autoCloseBrackets", false, function(cm, val, old) {
     var wasOn = old && old != CodeMirror.Init;
@@ -13,6 +14,7 @@
     var map = {
       name : "autoCloseBrackets",
       Backspace: function(cm) {
+        if (cm.somethingSelected()) return CodeMirror.Pass;
         var cur = cm.getCursor(), line = cm.getLine(cur.line);
         if (cur.ch && cur.ch < line.length &&
             pairs.indexOf(line.slice(cur.ch - 1, cur.ch + 1)) % 2 == 0)
@@ -21,16 +23,27 @@
           return CodeMirror.Pass;
       }
     };
+    var closingBrackets = [];
     for (var i = 0; i < pairs.length; i += 2) (function(left, right) {
+      if (left != right) closingBrackets.push(right);
+      function surround(cm) {
+          var selection = cm.getSelection();
+          cm.replaceSelection(left + selection + right);
+      }
       function maybeOverwrite(cm) {
         var cur = cm.getCursor(), ahead = cm.getRange(cur, CodeMirror.Pos(cur.line, cur.ch + 1));
-        if (ahead != right) return CodeMirror.Pass;
+        if (ahead != right || cm.somethingSelected()) return CodeMirror.Pass;
         else cm.execCommand("goCharRight");
       }
       map["'" + left + "'"] = function(cm) {
+        if (cm.somethingSelected()) return surround(cm);
         if (left == right && maybeOverwrite(cm) != CodeMirror.Pass) return;
-        var cur = cm.getCursor("start"), ahead = CodeMirror.Pos(cur.line, cur.ch + 1);
-        cm.replaceSelection(left + right, {head: ahead, anchor: ahead});
+        var cur = cm.getCursor(), ahead = CodeMirror.Pos(cur.line, cur.ch + 1);
+        var line = cm.getLine(cur.line), nextChar = line.charAt(cur.ch);
+        if (line.length == cur.ch || closingBrackets.indexOf(nextChar) >= 0 || SPACE_CHAR_REGEX.test(nextChar))
+          cm.replaceSelection(left + right, {head: ahead, anchor: ahead});
+        else
+          return CodeMirror.Pass;
       };
       if (left != right) map["'" + right + "'"] = maybeOverwrite;
     })(pairs.charAt(i), pairs.charAt(i + 1));
