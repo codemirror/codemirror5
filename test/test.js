7fcb11276e50601eb35a1c0a32bf810047e49988
Marijn Haverbeke <marijnh@gmail.com>
2013-05-08 12:00:23 +0200

    [bidi support] Overhaul algorithm to deal with corner case
    When a level 2 section intersected with a level 0 one, an
    assumption in the visual motion code was violated, resulting
    in bogus motion and thus infinite loops in the charCoords code.

--- test/test.js
+++ test/test.js
@@ -1013,7 +1013,8 @@ testCM("verticalMovementCommandsWrapping", function(cm) {
 
 testCM("rtlMovement", function(cm) {
   forEach(["خحج", "خحabcخحج", "abخحخحجcd", "abخde", "abخح2342خ1حج", "خ1ح2خح3حxج",
-           "خحcd", "1خحcd", "abcdeح1ج", "خمرحبها مها!", "foobarر"], function(line) {
+           "خحcd", "1خحcd", "abcdeح1ج", "خمرحبها مها!", "foobarر",
+           "<img src=\"/בדיקה3.jpg\">"], function(line) {
     var inv = line.charAt(0) == "خ";
     cm.setValue(line + "\n"); cm.execCommand(inv ? "goLineEnd" : "goLineStart");
     var cursor = byClassName(cm.getWrapperElement(), "CodeMirror-cursor")[0];
