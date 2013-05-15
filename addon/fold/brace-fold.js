9a8282fd1d310b3bf61d101b230b4766a933688c
Alex Piggott <apiggott@ikanow.com>
2013-05-15 13:14:57 +0200

    [brace-fold addon] Also handle square brackets

--- addon/fold/brace-fold.js
+++ addon/fold/brace-fold.js
@@ -3,17 +3,23 @@ CodeMirror.braceRangeFinder = function(cm, start) {
   var at = lineText.length, startChar, tokenType;
   for (; at > 0;) {
     var found = lineText.lastIndexOf("{", at);
-    if (found < start.ch) break;
+    var startToken = '{', endToken = '}';
+    if (found < start.ch) {
+      found = lineText.lastIndexOf("[", at);
+      if (found < start.ch) break;
+      startToken = '['; endToken = ']';
+    }
+
     tokenType = cm.getTokenAt(CodeMirror.Pos(line, found + 1)).type;
     if (!/^(comment|string)/.test(tokenType)) { startChar = found; break; }
     at = found - 1;
   }
-  if (startChar == null || lineText.lastIndexOf("}") > startChar) return;
+  if (startChar == null || lineText.lastIndexOf(startToken) > startChar) return;
   var count = 1, lastLine = cm.lineCount(), end, endCh;
   outer: for (var i = line + 1; i < lastLine; ++i) {
     var text = cm.getLine(i), pos = 0;
     for (;;) {
-      var nextOpen = text.indexOf("{", pos), nextClose = text.indexOf("}", pos);
+      var nextOpen = text.indexOf(startToken, pos), nextClose = text.indexOf(endToken, pos);
       if (nextOpen < 0) nextOpen = text.length;
       if (nextClose < 0) nextClose = text.length;
       pos = Math.min(nextOpen, nextClose);
