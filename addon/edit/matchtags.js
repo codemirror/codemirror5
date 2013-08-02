4342a59bd1ed71b231810a94191587ca3e24303b
Daniel Huigens <d.huigens@gmail.com>
2013-08-02 17:01:52 +0200

    [matchtags addon] Support multiple selections

--- addon/edit/matchtags.js
+++ addon/edit/matchtags.js
@@ -11,21 +11,25 @@
   function doMatchTags(cm) {
     cm.operation(function() {
       if (cm.state.matchedTags) { cm.state.matchedTags(); cm.state.matchedTags = null; }
-
-      var cur = cm.getCursor();
-      var match = CodeMirror.findMatchingTag(cm, cur) || CodeMirror.findEnclosingTag(cm, cur);
-      if (!match) return;
-      var one = cm.markText(match.open.from, match.open.to, {className: "CodeMirror-matchingbracket"});
-      var two = cm.markText(match.close.from, match.close.to, {className: "CodeMirror-matchingbracket"});
-      cm.state.matchedTags = function() { one.clear(); two.clear(); };
+      var markers = [];
+      cm.withSelection(function() {
+        var cur = cm.getCursor();
+        var match = CodeMirror.findMatchingTag(cm, cur) || CodeMirror.findEnclosingTag(cm, cur);
+        if (!match) return;
+        markers.push(cm.markText(match.open.from, match.open.to, {className: "CodeMirror-matchingbracket"}));
+        markers.push(cm.markText(match.close.from, match.close.to, {className: "CodeMirror-matchingbracket"}));
+      });
+      cm.state.matchedTags = function() { for (var i = 0; i < markers.length; i++) markers[i].clear(); };
     });
   }
 
   CodeMirror.commands.toMatchingTag = function(cm) {
-    var found = CodeMirror.findMatchingTag(cm, cm.getCursor());
-    if (found) {
-      var other = found.at == "close" ? found.open : found.close;
-      cm.setSelection(other.to, other.from);
-    }
+    cm.withSelection(function() {
+      var found = CodeMirror.findMatchingTag(cm, cm.getCursor());
+      if (found) {
+        var other = found.at == "close" ? found.open : found.close;
+        cm.setSelection(other.to, other.from);
+      }
+    });
   };
 })();
