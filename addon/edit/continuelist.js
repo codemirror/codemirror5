323265737793d87fdd357157a308c43c32828bc6
Daniel Huigens <d.huigens@gmail.com>
2013-08-04 13:45:20 +0200

    [continuelist addon] Support multiple selections

--- addon/edit/continuelist.js
+++ addon/edit/continuelist.js
@@ -5,21 +5,23 @@
       unorderedBullets = '*+-';
 
   CodeMirror.commands.newlineAndIndentContinueMarkdownList = function(cm) {
-    var pos = cm.getCursor(),
-        inList = cm.getStateAfter(pos.line).list,
-        match;
+    cm.withSelection(function() {
+      var pos = cm.getCursor(),
+          inList = cm.getStateAfter(pos.line).list,
+          match;
 
-    if (!inList || !(match = cm.getLine(pos.line).match(listRE))) {
-      cm.execCommand('newlineAndIndent');
-      return;
-    }
+      if (!inList || !(match = cm.getLine(pos.line).match(listRE))) {
+        cm.execCommand('newlineAndIndent');
+        return;
+      }
 
-    var indent = match[1], after = match[4];
-    var bullet = unorderedBullets.indexOf(match[2]) >= 0
-      ? match[2]
-      : (parseInt(match[3], 10) + 1) + '.';
+      var indent = match[1], after = match[4];
+      var bullet = unorderedBullets.indexOf(match[2]) >= 0
+        ? match[2]
+        : (parseInt(match[3], 10) + 1) + '.';
 
-    cm.replaceSelection('\n' + indent + bullet + after, 'end');
+      cm.replaceSelection('\n' + indent + bullet + after, 'end');
+    });
   };
 
 }());
