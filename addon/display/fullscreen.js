69c8610dce41eabad58ba5c20f094554c78785fe
Marijn Haverbeke <marijnh@gmail.com>
2013-08-20 16:40:08 +0200

    [fullscreen addon] Add

---
+++ addon/display/fullscreen.js
@@ -0,0 +1,26 @@
+(function() {
+  "use strict";
+
+  CodeMirror.defineOption("fullScreen", false, function(cm, val, old) {
+    if (old == CodeMirror.Init) old = false;
+    if (!old == !val) return;
+    if (val) setFullscreen(cm);
+    else setNormal(cm);
+  });
+
+  function setFullscreen(cm) {
+    cm.state.restoreScreenScrollPos = {top: window.pageYOffset, left: window.pageXOffset};
+    cm.getWrapperElement().className += " CodeMirror-fullscreen";
+    document.documentElement.style.overflow = "hidden";
+    cm.refresh();
+  }
+
+  function setNormal(cm) {
+    var wrap = cm.getWrapperElement();
+    wrap.className = wrap.className.replace(/\s*CodeMirror-fullscreen\b/, "");
+    document.documentElement.style.overflow = "";
+    var scroll = cm.state.restoreScreenScrollPos;
+    window.scrollTo(scroll.left, scroll.top);
+    cm.refresh();
+  }
+})();
