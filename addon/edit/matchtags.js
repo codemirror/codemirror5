9e8dc9479b1db61523ab45e796d5aee5f944fe67
sathyamoorthi <sathyamoorthi10@gmail.com>
2013-08-26 10:27:40 +0530

    white space removed to satisfy Travis.

--- addon/edit/matchtags.js
+++ addon/edit/matchtags.js
@@ -28,12 +28,12 @@
       var cur = cm.getCursor(), range = cm.getViewport();
       range.from = Math.min(range.from, cur.line); range.to = Math.max(cur.line + 1, range.to);
       var match = CodeMirror.findMatchingTag(cm, cur, range);
-      if (!match) return;     
+      if (!match) return;
       if(cm.state.matchBothTags) {
         var hit = match.at == "open" ? match.open : match.close;
         if(hit)cm.state.tagHit = cm.markText(hit.from, hit.to, {className: "CodeMirror-matchingtag"});
       }
-      var other = match.at == "close" ? match.open : match.close;      
+      var other = match.at == "close" ? match.open : match.close;
       if (other)
         cm.state.tagOther = cm.markText(other.from, other.to, {className: "CodeMirror-matchingtag"});
       else
