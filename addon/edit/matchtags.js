107ca187bbdd373585fbb0fd5c8302fa6b6bb4f0
sathyamoorthi <sathyamoorthi10@gmail.com>
2013-08-26 10:06:08 +0530

    option added to highlight both tags.

--- addon/edit/matchtags.js
+++ addon/edit/matchtags.js
@@ -8,6 +8,7 @@
       clear(cm);
     }
     if (val) {
+      cm.state.matchBothTags = (val.bothTags);
       cm.on("cursorActivity", doMatchTags);
       cm.on("viewportChange", maybeUpdateMatch);
       doMatchTags(cm);
@@ -15,10 +16,9 @@
   });
 
   function clear(cm) {
-    if (cm.state.matchedTag) {
-      cm.state.matchedTag.clear();
-      cm.state.matchedTag = null;
-    }
+    if (cm.state.tagHit) cm.state.tagHit.clear();
+    if (cm.state.tagOther) cm.state.tagOther.clear();
+    cm.state.tagHit = null; cm.state.tagOther = null;  
   }
 
   function doMatchTags(cm) {
@@ -28,16 +28,20 @@
       var cur = cm.getCursor(), range = cm.getViewport();
       range.from = Math.min(range.from, cur.line); range.to = Math.max(cur.line + 1, range.to);
       var match = CodeMirror.findMatchingTag(cm, cur, range);
-      if (!match) return;
-      var other = match.at == "close" ? match.open : match.close;
+      if (!match) return;     
+      if(cm.state.matchBothTags) {
+        var hit = match.at == "open" ? match.open : match.close;
+        if(hit)cm.state.tagHit = cm.markText(hit.from, hit.to, {className: "CodeMirror-matchingtag"});
+      }
+      var other = match.at == "close" ? match.open : match.close;      
       if (other)
-        cm.state.matchedTag = cm.markText(other.from, other.to, {className: "CodeMirror-matchingtag"});
+        cm.state.tagOther = cm.markText(other.from, other.to, {className: "CodeMirror-matchingtag"});
       else
         cm.state.failedTagMatch = true;
     });
   }
 
-  function maybeUpdateMatch(cm) {
+  function maybeUpdateMatch(cm) {    
     if (cm.state.failedTagMatch) doMatchTags(cm);
   }
 
