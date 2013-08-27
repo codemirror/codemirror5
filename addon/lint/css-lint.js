be12635c1d5d7758f7422025836dc8b27eeb0746
Marijn Haverbeke <marijnh@gmail.com>
2013-08-27 12:04:10 +0200

    [css-lint addon] Don't add new deprecated code, fix trailing space
    Issue #1782

--- addon/lint/css-lint.js
+++ addon/lint/css-lint.js
@@ -6,11 +6,12 @@ CodeMirror.registerHelper("lint", "css", function(text) {
   for ( var i = 0; i < messages.length; i++) {
     message = messages[i];
     var startLine = message.line -1, endLine = message.line -1, startCol = message.col -1, endCol = message.col;
-    found.push({from: CodeMirror.Pos(startLine, startCol),
+    found.push({
+      from: CodeMirror.Pos(startLine, startCol),
       to: CodeMirror.Pos(endLine, endCol),
       message: message.message,
-      severity : message.type});  
+      severity : message.type
+    });
   }
   return found;
 });
-CodeMirror.cssValidator = CodeMirror.lint.css; // deprecated
