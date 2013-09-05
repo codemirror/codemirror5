22ea8d9dbc41c108d67675d7b3a8cbdde7e7133d
Marijn Haverbeke <marijnh@gmail.com>
2013-09-05 11:32:33 +0200

    [matchtags addon] Don't match tags when there is a selection
    Closes #1802

--- addon/edit/matchtags.js
+++ addon/edit/matchtags.js
@@ -25,6 +25,7 @@
     cm.state.failedTagMatch = false;
     cm.operation(function() {
       clear(cm);
+      if (cm.somethingSelected()) return;
       var cur = cm.getCursor(), range = cm.getViewport();
       range.from = Math.min(range.from, cur.line); range.to = Math.max(cur.line + 1, range.to);
       var match = CodeMirror.findMatchingTag(cm, cur, range);
