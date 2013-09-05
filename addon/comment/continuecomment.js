fab90b772626b054ba82a81f26b9caa35a356f93
Marijn Haverbeke <marijnh@gmail.com>
2013-09-05 15:10:51 +0200

    [continuecomment addon] Clean up, support line comment blocks

--- addon/comment/continuecomment.js
+++ addon/comment/continuecomment.js
@@ -1,35 +1,43 @@
 (function() {
   var modes = ["clike", "css", "javascript"];
   for (var i = 0; i < modes.length; ++i)
-    CodeMirror.extendMode(modes[i], {blockCommentStart: "/*",
-                                     blockCommentEnd: "*/",
-                                     blockCommentContinue: " * "});
+    CodeMirror.extendMode(modes[i], {blockCommentContinue: " * "});
 
   function continueComment(cm) {
     var pos = cm.getCursor(), token = cm.getTokenAt(pos);
+    if (token.type != "comment") return CodeMirror.Pass;
     var mode = CodeMirror.innerMode(cm.getMode(), token.state).mode;
-    var space;
 
-    if (token.type == "comment" && mode.blockCommentStart && mode.blockCommentContinue) {
+    var insert;
+    if (mode.blockCommentStart && mode.blockCommentContinue) {
       var end = token.string.indexOf(mode.blockCommentEnd);
       var full = cm.getRange(CodeMirror.Pos(pos.line, 0), CodeMirror.Pos(pos.line, token.end)), found;
       if (end != -1 && end == token.string.length - mode.blockCommentEnd.length) {
         // Comment ended, don't continue it
       } else if (token.string.indexOf(mode.blockCommentStart) == 0) {
-        space = full.slice(0, token.start);
-        if (!/^\s*$/.test(space)) {
-          space = "";
-          for (var i = 0; i < token.start; ++i) space += " ";
+        insert = full.slice(0, token.start);
+        if (!/^\s*$/.test(insert)) {
+          insert = "";
+          for (var i = 0; i < token.start; ++i) insert += " ";
         }
       } else if ((found = full.indexOf(mode.blockCommentContinue)) != -1 &&
                  found + mode.blockCommentContinue.length > token.start &&
                  /^\s*$/.test(full.slice(0, found))) {
-        space = full.slice(0, found);
+        insert = full.slice(0, found);
+      }
+      if (insert != null) insert += mode.blockCommentContinue;
+    }
+    if (insert == null && mode.lineComment) {
+      var line = cm.getLine(pos.line), found = line.indexOf(mode.lineComment);
+      if (found > -1) {
+        insert = line.slice(0, found);
+        if (/\S/.test(insert)) insert = null;
+        else insert += mode.lineComment + line.slice(found + mode.lineComment.length).match(/^\s*/)[0];
       }
     }
 
-    if (space != null)
-      cm.replaceSelection("\n" + space + mode.blockCommentContinue, "end");
+    if (insert != null)
+      cm.replaceSelection("\n" + insert, "end");
     else
       return CodeMirror.Pass;
   }
@@ -37,8 +45,10 @@
   CodeMirror.defineOption("continueComments", null, function(cm, val, prev) {
     if (prev && prev != CodeMirror.Init)
       cm.removeKeyMap("continueComment");
-    var map = {name: "continueComment"};
-    map[typeof val == "string" ? val : "Enter"] = continueComment;
-    cm.addKeyMap(map);
+    if (val) {
+      var map = {name: "continueComment"};
+      map[typeof val == "string" ? val : "Enter"] = continueComment;
+      cm.addKeyMap(map);
+    }
   });
 })();
