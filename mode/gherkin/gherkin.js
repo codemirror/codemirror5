e34340ac0c77777e6da691bf66e4f9f6fc11d047
Darius Roberts <darius.roberts@gmail.com>
2013-09-25 09:36:54 -0400

    adding MIT licence block from gherkin-brackets

--- mode/gherkin/gherkin.js
+++ mode/gherkin/gherkin.js
@@ -1,8 +1,27 @@
 /*
+The MIT License (MIT)
+Copyright © 2013 Glenn Jorde, http://tregusti.com/
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
 Gherkin mode - http://www.cukes.info/
-Ported to CodeMirror by Darius Roberts <darius.roberts@gmail.com>
 Report bugs/issues here: https://github.com/marijnh/CodeMirror/issues
-GitHub: @jdar
 */
 
 // Following Objs from Brackets implementation: https://github.com/tregusti/brackets-gherkin/blob/master/main.js
