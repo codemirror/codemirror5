dad387da5bf0bc5887e607603f1fc27d586fb167
Marijn Haverbeke <marijnh@gmail.com>
2013-10-14 10:48:17 +0200

    [indent-fold addon] Fix coding style, use lastLine not lineCount
    Issue #1859

--- addon/fold/indent-fold.js
+++ addon/fold/indent-fold.js
@@ -1,25 +1,26 @@
 CodeMirror.registerHelper("fold", "indent", function(cm, start) {
-  var foldEnded = function(myIndent, curColumn, prevColumn, numLines) {
-    return ( (curColumn < myIndent) ||
-             (curColumn == myIndent && prevColumn >= myIndent) ||
-             (curColumn > myIndent && i == numLines-1) );
-  };
-
-  var numLines = cm.lineCount(),
+  var lastLine = cm.lastLine(),
       tabSize = cm.getOption("tabSize"),
       firstLine = cm.getLine(start.line),
       myIndent = CodeMirror.countColumn(firstLine, null, tabSize);
 
-  for (var i = start.line + 1 ; i < numLines ; i++) {
+  function foldEnded(curColumn, prevColumn) {
+    return curColumn < myIndent ||
+      (curColumn == myIndent && prevColumn >= myIndent) ||
+      (curColumn > myIndent && i == lastLine);
+  }
+
+  for (var i = start.line + 1; i <= lastLine; i++) {
     var curColumn = CodeMirror.countColumn(cm.getLine(i), null, tabSize);
     var prevColumn = CodeMirror.countColumn(cm.getLine(i-1), null, tabSize);
 
-    if (foldEnded(myIndent, curColumn, prevColumn, numLines)) {
-      var lastFoldLineNumber = (curColumn > myIndent && i == numLines-1) ? i : i-1;
+    if (foldEnded(curColumn, prevColumn)) {
+      var lastFoldLineNumber = curColumn > myIndent && i == lastLine ? i : i-1;
       var lastFoldLine = cm.getLine(lastFoldLineNumber);
       return {from: CodeMirror.Pos(start.line, firstLine.length),
               to: CodeMirror.Pos(lastFoldLineNumber, lastFoldLine.length)};
     }
   }
 });
+
 CodeMirror.indentRangeFinder = CodeMirror.fold.indent; // deprecated
