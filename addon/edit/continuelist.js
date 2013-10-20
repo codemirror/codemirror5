65d995ab6b79d53c0f9abed162bfd38697dca553
Brandon Frohs <bfrohs@gmail.com>
2013-10-20 13:22:28 -0400

    [continuelist] Should continue list even after special chars (space, !, etc)

--- addon/edit/continuelist.js
+++ addon/edit/continuelist.js
@@ -6,7 +6,7 @@
 
   CodeMirror.commands.newlineAndIndentContinueMarkdownList = function(cm) {
     var pos = cm.getCursor(),
-        inList = cm.getStateAfter(pos.line).list,
+        inList = cm.getStateAfter(pos.line).list !== false,
         match;
 
     if (!inList || !(match = cm.getLine(pos.line).match(listRE))) {
