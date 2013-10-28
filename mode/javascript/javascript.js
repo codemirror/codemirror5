8d000ddae0935f73e32ef517841c4df21db175e2
Marijn Haverbeke <marijnh@gmail.com>
2013-10-28 19:40:54 +0100

    [javascript mode] Fix bug in recognizing local variable in nested context

--- mode/javascript/javascript.js
+++ mode/javascript/javascript.js
@@ -165,6 +165,10 @@ CodeMirror.defineMode("javascript", function(config, parserConfig) {
   function inScope(state, varname) {
     for (var v = state.localVars; v; v = v.next)
       if (v.name == varname) return true;
+    for (var cx = state.context; cx; cx = cx.prev) {
+      for (var v = cx.vars; v; v = v.next)
+        if (v.name == varname) return true;
+    }
   }
 
   function parseJS(state, style, type, content, stream) {
