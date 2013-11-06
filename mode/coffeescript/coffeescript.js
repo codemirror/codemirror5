codemirror/codemirror5

commit 567a94b89cbe8bf12f6a210e2e395c5c147272a9

[coffeescript mode] Some refinement in indentation
Issue #1932

mode/coffeescript/coffeescript.js
@@ -213,6 +213,8 @@ CodeMirror.defineMode("coffeescript", function(conf) {
     if (type !== "coffee") {
       align = null;
       alignOffset = stream.column() + stream.current().length;
+    } else if (state.scope.align) {
+      state.scope.align = false;
     }
     state.scope = {
       offset: offset,
@@ -268,7 +270,6 @@ CodeMirror.defineMode("coffeescript", function(conf) {
     }
     if (((current === "->" || current === "=>") &&
          !state.lambda &&
-         state.scope.type == "coffee" &&
          !stream.peek())
         || style === "indent") {
       indent(stream, state);
@@ -292,9 +293,10 @@ CodeMirror.defineMode("coffeescript", function(conf) {
     }
     delimiter_index = "])}".indexOf(current);
     if (delimiter_index !== -1) {
-      if (dedent(stream, state)) {
-        return ERRORCLASS;
-      }
+      while (state.scope.type == "coffee" && state.scope.prev)
+        state.scope = state.scope.prev;
+      if (state.scope.type == current)
+        state.scope = state.scope.prev;
     }
     if (state.dedent > 0 && stream.eol() && state.scope.type == "coffee") {
       if (state.scope.prev) state.scope = state.scope.prev;
@@ -333,11 +335,14 @@ CodeMirror.defineMode("coffeescript", function(conf) {
 
     indent: function(state, text) {
       if (state.tokenize != tokenBase) return 0;
-      var closes = state.scope.type === (text && text.charAt(0));
-      if (state.scope.align)
-        return state.scope.alignOffset - (closes ? 1 : 0);
+      var scope = state.scope;
+      var closer = "])}".indexOf(text.charAt(0)) > -1;
+      if (closer) while (scope.type == "coffee" && scope.prev) scope = scope.prev;
+      var closes = scope.type === text.charAt(0);
+      if (scope.align)
+        return scope.alignOffset - (closes ? 1 : 0);
       else
-        return (closes ? state.scope.prev : state.scope).offset;
+        return (closes ? scope.prev : scope).offset;
     },
 
     lineComment: "#",

commit 4da56b598000a451423cf38598e413b10b8a4f62

[coffeescript mode] Fix bug introduced by 567a94b89cbe8b
Issue #1932

mode/coffeescript/coffeescript.js
@@ -336,9 +336,9 @@ CodeMirror.defineMode("coffeescript", function(conf) {
     indent: function(state, text) {
       if (state.tokenize != tokenBase) return 0;
       var scope = state.scope;
-      var closer = "])}".indexOf(text.charAt(0)) > -1;
+      var closer = text && "])}".indexOf(text.charAt(0)) > -1;
       if (closer) while (scope.type == "coffee" && scope.prev) scope = scope.prev;
-      var closes = scope.type === text.charAt(0);
+      var closes = closer && scope.type === text.charAt(0);
       if (scope.align)
         return scope.alignOffset - (closes ? 1 : 0);
       else