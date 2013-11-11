51d6810d35511ad96349eb6a91fe46ff43ddbd7e
Thaddee Tyl <thaddee.tyl@gmail.com>
2013-11-11 08:35:07 +0100

    [go mode] Add ')' as electric character.
    As a result, the common pattern
    
    import (
    	"foo"
    	"bar"
    	)
    
    will re-indent automatically to the gofmt-blessed
    
    import (
    	"foo"
    	"bar"
    )

--- mode/go/go.js
+++ mode/go/go.js
@@ -158,7 +158,7 @@ CodeMirror.defineMode("go", function(config) {
       else return ctx.indented + (closing ? 0 : indentUnit);
     },
 
-    electricChars: "{}:",
+    electricChars: "{}):",
     blockCommentStart: "/*",
     blockCommentEnd: "*/",
     lineComment: "//"
