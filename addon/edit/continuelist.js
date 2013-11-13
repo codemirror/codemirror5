ab2bf531135b15a4a9ce93167c139f020708a453
Marijn Haverbeke <marijnh@gmail.com>
2013-11-13 11:05:11 +0100

    [continuelist addon] Adjust to multi-selections
    Issue #778

--- addon/edit/continuelist.js
+++ addon/edit/continuelist.js
@@ -1,25 +1,27 @@
 (function() {
-  'use strict';
+  "use strict";
 
   var listRE = /^(\s*)([*+-]|(\d+)\.)(\s*)/,
-      unorderedBullets = '*+-';
+      unorderedBullets = "*+-";
 
   CodeMirror.commands.newlineAndIndentContinueMarkdownList = function(cm) {
-    var pos = cm.getCursor(),
-        inList = cm.getStateAfter(pos.line).list !== false,
-        match;
+    var ranges = cm.listSelections(), replacements = [];
+    for (var i = 0; i < ranges.length; i++) {
+      var pos = ranges[i].head, match;
+      var inList = cm.getStateAfter(pos.line).list !== false;
 
-    if (!inList || !(match = cm.getLine(pos.line).match(listRE))) {
-      cm.execCommand('newlineAndIndent');
-      return;
+      if (!ranges[i].empty() || !inList || !(match = cm.getLine(pos.line).match(listRE))) {
+        cm.execCommand("newlineAndIndent");
+        return;
+      }
+      var indent = match[1], after = match[4];
+      var bullet = unorderedBullets.indexOf(match[2]) >= 0
+        ? match[2]
+        : (parseInt(match[3], 10) + 1) + ".";
+
+      replacements[i] = "\n" + indent + bullet + after;
     }
 
-    var indent = match[1], after = match[4];
-    var bullet = unorderedBullets.indexOf(match[2]) >= 0
-      ? match[2]
-      : (parseInt(match[3], 10) + 1) + '.';
-
-    cm.replaceSelection('\n' + indent + bullet + after, 'end');
+    cm.replaceSelections(replacements, null, "+insert");
   };
-
 }());
