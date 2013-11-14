108f1ffb71f3a4d5a25cc84ae62f1507eaf0ff15
mini-emmy <emmy.hoyes@gmail.com>
2013-11-14 12:38:18 +0000

    Code styling checked

--- addon/hint/clike-hint.js
+++ addon/hint/clike-hint.js
@@ -19,10 +19,9 @@
   //Call this from outside environment to add more words to the list that might be specific to your component
   //or taken from a current view model. They will show first in the list.
   CodeMirror.addKeywords=function(keywords){
-	  addedKeywords=keywords;
+    addedKeywords=keywords;
   };
   
-
   function scriptHint(editor, _keywords, getToken) {
     // Find the token at the cursor
     var cur = editor.getCursor(), token = getToken(editor, cur), tprop = token;
@@ -46,24 +45,22 @@
   //Offers completion for C-sharp, C++, Java and Scala
   function csharpHint(editor) {	
     return scriptHint(editor, getAllKeywords(csharpKeywords), function (e, cur) {return e.getTokenAt(cur);});
-  }
+    }
   CodeMirror.csharpHintHint = csharpHint; // deprecated
   CodeMirror.registerHelper("hint", "csharp", csharpHint);
   
-  
   function javaHint(editor) {
-	    return scriptHint(editor, getAllKeywords(javaKeywords), function (e, cur) {return e.getTokenAt(cur);});
-	  }
+    return scriptHint(editor, getAllKeywords(javaKeywords), function (e, cur) {return e.getTokenAt(cur);});
+    }
   CodeMirror.javaHint = javaHint; // deprecated
   CodeMirror.registerHelper("hint", "java", javaHint);
   
   function scalaHint(editor) {
-	    return scriptHint(editor, getAllKeywords(scalaKeywords), function (e, cur) {return e.getTokenAt(cur);});
-	  }
+    return scriptHint(editor, getAllKeywords(scalaKeywords), function (e, cur) {return e.getTokenAt(cur);});
+    }
   CodeMirror.scalaHint = scalaHint; // deprecated
   CodeMirror.registerHelper("hint", "scala", scalaHint);
   
- 
   var addedKeywords="";
   
   var clikeKeywordString="abstract assert boolean break byte case catch char class const do default double else enum false "+
@@ -95,29 +92,28 @@
   "StringContext Symbol Throwable Traversable TraversableOnce Tuple Unit Vector :: #:: ";
   
   function getAllKeywords(keywordString) {
-	  var newKeywords=(addedKeywords+clikeKeywordString+keywordString).split(" ");
-	  return newKeywords;
+    var newKeywords=(addedKeywords+clikeKeywordString+keywordString).split(" ");
+    return newKeywords;
   }
  
   function getCompletions(token, context, _keywords) {
     var found = [], start = token.string;
     function maybeAdd(str) {
-    	//searches with all lower case
-   	 	if (str.indexOf(start.toLowerCase()) == 0 && !arrayContains(found, str)) found.push(str);
-   	 	//searches with first letter upper case
-        if (str.indexOf(start.charAt(0).toUpperCase() + start.slice(1)) == 0 && !arrayContains(found, str)) found.push(str);
+     //searches with all lower case
+    if (str.indexOf(start.toLowerCase()) == 0 && !arrayContains(found, str)) found.push(str);
+    //searches with first letter upper case
+    if (str.indexOf(start.charAt(0).toUpperCase() + start.slice(1)) == 0 && !arrayContains(found, str)) found.push(str);
     }
 
-    function gatherCompletions(_obj) {
-        forEach(_keywords,maybeAdd); 
+  function gatherCompletions(_obj) {
+    forEach(_keywords,maybeAdd); 
     }
 
     if (context) {
-
       var obj = context.pop(), base;
 
       if (obj.type== "variable")
-          base = obj.string;
+        base = obj.string;
       while (base != null && context.length)
         base = base[context.pop().string];
       if (base != null) gatherCompletions(base);
