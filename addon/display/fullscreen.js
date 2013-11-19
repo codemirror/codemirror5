f4c99c2e10bc6ac46b00b06a0ba8d0e778812c8a
魏鹏刚 <weipenggang@163.com>
2013-11-19 14:55:52 +0800

    Update fullscreen.js
    Solve the problem of conflict with 'merge'.

--- addon/display/fullscreen.js
+++ addon/display/fullscreen.js
@@ -12,7 +12,8 @@
     var wrap = cm.getWrapperElement();
     cm.state.fullScreenRestore = {scrollTop: window.pageYOffset, scrollLeft: window.pageXOffset,
                                   width: wrap.style.width, height: wrap.style.height};
-    wrap.style.width = wrap.style.height = "";
+    wrap.style.width = "";
+    wrap.style.height = "auto";
     wrap.className += " CodeMirror-fullscreen";
     document.documentElement.style.overflow = "hidden";
     cm.refresh();
