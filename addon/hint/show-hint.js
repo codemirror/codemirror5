6907aaf716483dad3f3474e3cfaef0ec36475200
Abe Fettig <abe@fettig.net>
2013-11-27 12:06:00 -0500

    [show-hint addon] Be more precise in identifying hint element
    ... to prevent misbehavior in cases where the rendered hint element
    contains an LI in its child nodes

--- addon/hint/show-hint.js
+++ addon/hint/show-hint.js
@@ -144,9 +144,9 @@
     return ourMap;
   }
 
-  function getHintElement(stopAt, el) {
-    while (el && el != stopAt) {
-      if (el.nodeName.toUpperCase() === "LI") return el;
+  function getHintElement(hintsElement, el) {
+    while (el && el != hintsElement) {
+      if (el.nodeName.toUpperCase() === "LI" && el.parentNode == hintsElement) return el;
       el = el.parentNode;
     }
   }
