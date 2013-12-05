codemirror/codemirror5

commit a6aa48eee2c8262a8763f1e39afdf8a79a130a41

[xml mode] Reset state when giving up on a bogus tag
Issue #2023

mode/xml/xml.js
@@ -110,6 +110,8 @@ CodeMirror.defineMode("xml", function(config, parserConfig) {
       return null;
     } else if (ch == "<") {
       state.tokenize = inText;
+      state.state = baseState;
+      state.tagName = state.tagStart = null;
       var next = state.tokenize(stream, state);
       return next ? next + " error" : "error";
     } else if (/[\'\"]/.test(ch)) {

commit 3ba5c3b9299d9d34df7238ff457cb2b2beb14a1e

[xml mode] Fix bug introduced by a6aa48eee2c8262a8763f1e39afdf8a79a130a41

mode/xml/xml.js
@@ -195,7 +195,7 @@ CodeMirror.defineMode("xml", function(config, parserConfig) {
           !Kludges.contextGrabbers[parentTagName].hasOwnProperty(nextTagName)) {
         return;
       }
-      popContext();
+      popContext(state);
     }
   }
 