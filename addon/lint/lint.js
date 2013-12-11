febe3acfe42bc1f94279fe6dd81a93946d12fa32
eborden <evan@evan-borden.com>
2013-12-11 11:44:31 -0500

    [javascript-lint addon] Put JSHint options in a sub-object
    Passing options causes issues in jshint since lint options are also
    passed. These options cause bad option errors in the annotation list.

--- addon/lint/lint.js
+++ addon/lint/lint.js
@@ -112,7 +112,7 @@
     if (options.async)
       options.getAnnotations(cm, updateLinting, options);
     else
-      updateLinting(cm, options.getAnnotations(cm.getValue(), options));
+      updateLinting(cm, options.getAnnotations(cm.getValue(), options.options));
   }
 
   function updateLinting(cm, annotationsNotSorted) {
