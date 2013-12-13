73a3efe1ce0f8c50686f7f6a5961307615c77c65
Marijn Haverbeke <marijnh@gmail.com>
2013-12-13 20:38:42 +0100

    [linter] Check for reused loop variables

--- test/lint/lint.js
+++ test/lint/lint.js
@@ -95,8 +95,25 @@ function checkFile(fileName) {
     },
     FunctionExpression: function(node) {
       if (node.id) fail("Named function expression", node.loc);
+    },
+    ForStatement: function(node) {
+      checkReusedIndex(node);
     }
   }, scopePasser);
+
+  function checkReusedIndex(node) {
+    if (!node.init || node.init.type != "VariableDeclaration") return;
+    var name = node.init.declarations[0].id.name;
+    walk.recursive(node.body, null, {
+      Function: function() {},
+      VariableDeclaration: function(node, st, c) {
+        for (var i = 0; i < node.declarations.length; i++)
+          if (node.declarations[i].id.name == name)
+            fail("redefined loop variable", node.declarations[i].id.loc);
+        walk.base.VariableDeclaration(node, st, c);
+      }
+    });
+  }
 
   if (!globalsSeen.exports) {
     var allowedGlobals = Object.create(topAllowedGlobals), m;
