d84acac4f92b14b46c16f157bdf8243ce314b2b5
zziuni <zziuni@gmail.com>
2013-12-19 17:22:48 +0900

    [sql hint] support multi-queries on the table alias
    Now, Sql-hint assume one query to search for table of alias.
    It support multi-queries for alias.

--- addon/hint/sql-hint.js
+++ addon/hint/sql-hint.js
@@ -3,6 +3,10 @@
 
   var tables;
   var keywords;
+  var CONS = {
+    QUERY_DIV: ";",
+    ALIAS_KEYWORD: "AS"
+  };
 
   function getKeywords(editor) {
     var mode = editor.doc.modeOption;
@@ -34,11 +38,10 @@
     var string = token.string.substr(1);
     var prevCur = CodeMirror.Pos(cur.line, token.start);
     var table = editor.getTokenAt(prevCur).string;
-    var columns = tables[table];
-    if(!columns) {
+    if( !tables.hasOwnProperty( table ) ){
       table = findTableByAlias(table, editor);
     }
-    columns = tables[table];
+    var columns = tables[table];
     if(!columns) {
       return;
     }
@@ -46,32 +49,72 @@
         function(w) {return "." + w;});
   }
 
-  function eachWord(line, f) {
-    var words = line.text.split(" ");
-    for(var i = 0; i < words.length; i++) {
-      f(words[i]);
+  function eachWord(lineText, f) {
+    if( !lineText ){return;}
+    var excepted = /[,;]/g;
+    var words = lineText.split( " " );
+    for( var i = 0; i < words.length; i++ ){
+      f( words[i]?words[i].replace( excepted, '' ) : '' );
     }
   }
 
-  // Tries to find possible table name from alias.
+  function convertCurToNumber( cur ){
+    // max characters of a line is 999,999.
+    return cur.line + cur.ch / Math.pow( 10, 6 );
+  }
+
+  function convertNumberToCur( num ){
+    return CodeMirror.Pos(Math.floor( num ), +num.toString().split( '.' ).pop());
+  }
+
   function findTableByAlias(alias, editor) {
+    var doc = editor.doc;
+    var fullQuery = doc.getValue();
     var aliasUpperCase = alias.toUpperCase();
     var previousWord = "";
     var table = "";
+    var separator = [];
+    var validRange = {
+      start: CodeMirror.Pos( 0, 0 ),
+      end: CodeMirror.Pos( editor.lastLine(), editor.getLineHandle( editor.lastLine() ).length )
+    };
 
-    editor.eachLine(function(line) {
-      eachWord(line, function(word) {
+    //add separator
+    var indexOfSeparator = fullQuery.indexOf( CONS.QUERY_DIV );
+    while( indexOfSeparator != -1 ){
+      separator.push( doc.posFromIndex(indexOfSeparator));
+      indexOfSeparator = fullQuery.indexOf( CONS.QUERY_DIV, indexOfSeparator+1);
+    }
+    separator.unshift( CodeMirror.Pos( 0, 0 ) );
+    separator.push( CodeMirror.Pos( editor.lastLine(), editor.getLineHandle( editor.lastLine() ).text.length ) );
+
+    //find valieRange
+    var prevItem = 0;
+    var current = convertCurToNumber( editor.getCursor() );
+    for( var i=0; i< separator.length; i++){
+      var _v = convertCurToNumber( separator[i] );
+      if( current > prevItem && current <= _v ){
+        validRange = { start: convertNumberToCur( prevItem ), end: convertNumberToCur( _v ) };
+        break;
+      }
+      prevItem = _v;
+    }
+
+    var query = doc.getRange(validRange.start, validRange.end, false);
+
+    for(var i=0; i < query.length; i++){
+      var lineText = query[i];
+      eachWord( lineText, function( word ){
         var wordUpperCase = word.toUpperCase();
-        if(wordUpperCase === aliasUpperCase) {
-          if(tables.hasOwnProperty(previousWord)) {
+        if( wordUpperCase === aliasUpperCase && tables.hasOwnProperty( previousWord ) ){
             table = previousWord;
-          }
         }
-        if(wordUpperCase !== "AS") {
+        if( wordUpperCase !== CONS.ALIAS_KEYWORD ){
           previousWord = word;
         }
       });
-    });
+      if( table ){ break; }
+    }
     return table;
   }
 
@@ -80,9 +123,7 @@
     keywords = keywords || getKeywords(editor);
     var cur = editor.getCursor();
     var token = editor.getTokenAt(cur);
-
     var result = [];
-
     var search = token.string.trim();
 
     addMatches(result, search, keywords,
