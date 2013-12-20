414a5bca40ddcc919c37ad6d420ba2d3185d1be1
Peter Kroon <plakroon@gmail.com>
2013-12-20 21:47:16 +0100

    Correct tab position in replacerstring
    "\n\t\n"
    
    Doesn't fix described problem, but does fix tab position.

--- addon/edit/closetag.js
+++ addon/edit/closetag.js
@@ -64,7 +64,7 @@
 
     var doIndent = indentTags && indexOf(indentTags, lowerTagName) > -1;
     var curPos = doIndent ? CodeMirror.Pos(pos.line + 1, 0) : CodeMirror.Pos(pos.line, pos.ch + 1);
-    cm.replaceSelection(">" + (doIndent ? "\t\n\n" : "") + "</" + tagName + ">",
+    cm.replaceSelection(">" + (doIndent ? "\n\t\n" : "") + "</" + tagName + ">",
                         {head: curPos, anchor: curPos});
     if (doIndent) {
       cm.indentLine(pos.line + 1);
