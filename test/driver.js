9228a29bc0d6fb5ada1a2186dd9a27dffecb6af6
Marijn Haverbeke <marijnh@gmail.com>
2013-12-27 17:27:31 +0100

    Clean up f5b8e1000dcd3d2e080ee5fd845eb333eefc04a5

--- test/driver.js
+++ test/driver.js
@@ -124,3 +124,8 @@ function countTests() {
   }
   return sum;
 }
+
+function parseTestFilter(s) {
+  if (/_\*$/.test(s)) return new RegExp("^" + s.slice(0, s.length - 2), "i");
+  else return new RegExp(s, "i");
+}
