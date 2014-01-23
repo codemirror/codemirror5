loganlinn/CodeMirror

Rename confusing variable

lib/codemirror.js
@@ -35,7 +35,7 @@ window.CodeMirror = (function() {
   if (opera_version && opera_version >= 15) { opera = false; webkit = true; }
   // Some browsers use the wrong event properties to signal cmd/ctrl on OS X
   var flipCtrlCmd = mac && (qtwebkit || opera && (opera_version == null || opera_version < 12.11));
-  var captureMiddleClick = gecko || (ie && !ie_lt9);
+  var captureRightClick = gecko || (ie && !ie_lt9);
 
   // Optimize some code when these features are not used
   var sawReadOnlySpans = false, sawCollapsedSpans = false;
@@ -1566,7 +1566,7 @@ window.CodeMirror = (function() {
     // Gecko browsers fire contextmenu *after* opening the menu, at
     // which point we can't mess with it anymore. Context menu is
     // handled in onMouseDown for Gecko.
-    if (!captureMiddleClick) on(d.scroller, "contextmenu", function(e) {onContextMenu(cm, e);});
+    if (!captureRightClick) on(d.scroller, "contextmenu", function(e) {onContextMenu(cm, e);});
 
     on(d.scroller, "scroll", function() {
       if (d.scroller.clientHeight) {
@@ -1711,7 +1711,7 @@ window.CodeMirror = (function() {
 
     switch (e_button(e)) {
     case 3:
-      if (captureMiddleClick) onContextMenu.call(cm, cm, e);
+      if (captureRightClick) onContextMenu.call(cm, cm, e);
       return;
     case 2:
       if (webkit) cm.state.lastMiddleDown = +new Date;
@@ -2234,7 +2234,7 @@ window.CodeMirror = (function() {
     }
 
     if (ie && !ie_lt9) prepareSelectAllHack();
-    if (captureMiddleClick) {
+    if (captureRightClick) {
       e_stop(e);
       var mouseup = function() {
         off(window, "mouseup", mouseup);

codemirror/codemirror5

lib/codemirror.js
@@ -36,7 +36,7 @@ window.CodeMirror = (function() {
   if (opera_version && opera_version >= 15) { opera = false; webkit = true; }
   // Some browsers use the wrong event properties to signal cmd/ctrl on OS X
   var flipCtrlCmd = mac && (qtwebkit || opera && (opera_version == null || opera_version < 12.11));
-  var captureMiddleClick = gecko || (old_ie && !ie_lt9);
+  var captureRightClick = gecko || (old_ie && !ie_lt9);
 
   // Optimize some code when these features are not used
   var sawReadOnlySpans = false, sawCollapsedSpans = false;
@@ -1583,7 +1583,7 @@ window.CodeMirror = (function() {
     // Gecko browsers fire contextmenu *after* opening the menu, at
     // which point we can't mess with it anymore. Context menu is
     // handled in onMouseDown for Gecko.
-    if (!captureMiddleClick) on(d.scroller, "contextmenu", function(e) {onContextMenu(cm, e);});
+    if (!captureRightClick) on(d.scroller, "contextmenu", function(e) {onContextMenu(cm, e);});
 
     on(d.scroller, "scroll", function() {
       if (d.scroller.clientHeight) {
@@ -1729,7 +1729,7 @@ window.CodeMirror = (function() {
 
     switch (e_button(e)) {
     case 3:
-      if (captureMiddleClick) onContextMenu.call(cm, cm, e);
+      if (captureRightClick) onContextMenu.call(cm, cm, e);
       return;
     case 2:
       if (webkit) cm.state.lastMiddleDown = +new Date;
@@ -2251,7 +2251,7 @@ window.CodeMirror = (function() {
     }
 
     if (old_ie && !ie_lt9) prepareSelectAllHack();
-    if (captureMiddleClick) {
+    if (captureRightClick) {
       e_stop(e);
       var mouseup = function() {
         off(window, "mouseup", mouseup);