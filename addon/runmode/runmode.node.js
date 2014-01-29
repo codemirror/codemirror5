76c97344df8c533f311b9013affe6553c2d40635
Marijn Haverbeke <marijnh@gmail.com>
2014-01-29 10:23:35 +0100

    [runmode addon] Fix missing parameter in runmode.node.js
    Closes #2188

--- addon/runmode/runmode.node.js
+++ addon/runmode/runmode.node.js
@@ -99,7 +99,7 @@ exports.getMode = function(options, spec) {
 };
 exports.registerHelper = exports.registerGlobalHelper = Math.min;
 
-exports.runMode = function(string, modespec, callback) {
+exports.runMode = function(string, modespec, callback, options) {
   var mode = exports.getMode({indentUnit: 2}, modespec);
   var lines = splitLines(string), state = (options && options.state) || exports.startState(mode);
   for (var i = 0, e = lines.length; i < e; ++i) {
