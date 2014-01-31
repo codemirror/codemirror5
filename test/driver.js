5df3bcc403b3242494c214a52b7629309db40fbe
Marijn Haverbeke <marijnh@gmail.com>
2014-01-31 17:59:30 +0100

    [test driver] Sanitize skipping tests

--- test/driver.js
+++ test/driver.js
@@ -44,23 +44,25 @@ function testCM(name, run, opts, expectedFail) {
 function runTests(callback) {
   var totalTime = 0;
   function step(i) {
-    if (i === tests.length){
-      running = false;
-      return callback("done");
-    } 
-    var test = tests[i], expFail = test.expectedFail, startTime = +new Date;
-    if (filters.length) {
-      for (var j = 0; j < filters.length; j++) {
-        if (test.name.match(filters[j])) {
-          break;
-        }
+    for (;;) {
+      if (i === tests.length) {
+        running = false;
+        return callback("done");
       }
-      if (j == filters.length) {      
+      var test = tests[i], skip = false;
+      if (filters.length) {
+        skip = true;
+        for (var j = 0; j < filters.length; j++)
+          if (test.name.match(filters[j])) skip = false;
+      }
+      if (skip) {
         callback("skipped", test.name, message);
-        return step(i + 1);
+        i++;
+      } else {
+        break;
       }
     }
-    var threw = false;
+    var expFail = test.expectedFail, startTime = +new Date, threw = false;
     try {
       var message = test.func();
     } catch(e) {
