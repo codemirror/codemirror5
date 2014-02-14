af81dba2347908a53ca0a0027d5073596dd0311f
mtaran-google <mtaran@google.com>
2014-02-14 12:52:42 -0800

    [emacs] Make delete & backspace delete the selection when present

--- keymap/emacs.js
+++ keymap/emacs.js
@@ -273,9 +273,21 @@
     "Ctrl-F": move(byChar, 1), "Ctrl-B": move(byChar, -1),
     "Right": move(byChar, 1), "Left": move(byChar, -1),
     "Ctrl-D": function(cm) { killTo(cm, byChar, 1); },
-    "Delete": function(cm) { killTo(cm, byChar, 1); },
+    "Delete": function(cm) { 
+      if (cm.somethingSelected()) {
+        kill(cm.getCursor("start"), cm.getCursor("end"), true);
+      } else {
+        killTo(cm, byChar, 1); 
+      }
+    },
     "Ctrl-H": function(cm) { killTo(cm, byChar, -1); },
-    "Backspace": function(cm) { killTo(cm, byChar, -1); },
+    "Backspace": function(cm) { 
+      if (cm.somethingSelected()) {
+        kill(cm.getCursor("start"), cm.getCursor("end"), true);
+      } else {
+        killTo(cm, byChar, -1); 
+      }
+    },
 
     "Alt-F": move(byWord, 1), "Alt-B": move(byWord, -1),
     "Alt-D": function(cm) { killTo(cm, byWord, 1); },
