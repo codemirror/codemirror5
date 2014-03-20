28c2f3ee0af54db2b62f72fe1a55cd9343167a16
Yunchi Luo <mightyguava@gmail.com>
2014-03-20 19:00:10 +0100

    [vim] Clean up and simplify selectCompanionObject a bit

--- keymap/vim.js
+++ keymap/vim.js
@@ -2766,7 +2766,7 @@
     function selectCompanionObject(cm, symb, inclusive) {
       var cur = cm.getCursor(), start, end;
 
-      var specifiedRegExp = ({
+      var bracketRegexp = ({
         '(': /[()]/, ')': /[()]/,
         '[': /[[\]]/, ']': /[[\]]/,
         '{': /[{}]/, '}': /[{}]/})[symb];
@@ -2775,31 +2775,31 @@
         '[': '[', ']': '[',
         '{': '{', '}': '{'})[symb];
       var curChar = cm.getLine(cur.line).charAt(cur.ch);
-      // Due to the behavior of scanForBracket, we need an offset in boundary case.
+      // Due to the behavior of scanForBracket, we need to add an offset if the
+      // cursor is on a matching open bracket.
       var offset = curChar === openSym ? 1 : 0;
 
-      start = cm.scanForBracket(Pos(cur.line, cur.ch+offset), -1, null, specifiedRegExp);
-      end   = cm.scanForBracket(Pos(cur.line, cur.ch+offset), 1, null, specifiedRegExp);
+      start = cm.scanForBracket(Pos(cur.line, cur.ch + offset), -1, null, bracketRegexp);
+      end = cm.scanForBracket(Pos(cur.line, cur.ch + offset), 1, null, bracketRegexp);
 
-      if (typeof start == "undefined" || typeof end == "undefined") {
-        start = cur;
-        end   = cur;
+      if (!start || !end) {
+        return { start: cur, end: cur };
+      }
+
+      start = start.pos;
+      end = end.pos;
+
+      if ((start.line == end.line && start.ch > end.ch)
+          || (start.line > end.line)) {
+        var tmp = start;
+        start = end;
+        end = tmp;
+      }
+
+      if (inclusive) {
+        end.ch += 1;
       } else {
-        start = start.pos;
-        end   = end.pos;
-
-        if ((start.line == end.line && start.ch > end.ch)
-            || (start.line > end.line)) {
-          var tmp = start;
-          start = end;
-          end = tmp;
-        }
-
-        if (inclusive) {
-          end.ch += 1;
-        } else {
-          start.ch += 1;
-        }
+        start.ch += 1;
       }
 
       return { start: start, end: end };
