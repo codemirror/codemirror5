df3b3d3de89d5aa9aca72d157e716b9a97da54ff
Marijn Haverbeke <marijnh@gmail.com>
2014-03-22 13:08:45 +0100

    [haxe mode] Add comment styles

--- mode/haxe/haxe.js
+++ mode/haxe/haxe.js
@@ -422,7 +422,10 @@ CodeMirror.defineMode("haxe", function(config, parserConfig) {
       else return lexical.indented + (closing ? 0 : indentUnit);
     },
 
-    electricChars: "{}"
+    electricChars: "{}",
+    blockCommentStart: "/*",
+    blockCommentEnd: "*/",
+    lineComment: "//"
   };
 });
 
