9129b7dc242e13ec5c3675fd021e52817a6ba4ea
paris <pariskasidiaris@gmail.com>
2014-03-24 03:06:02 +0000

    Added index.html
    Also added addon/mode/overlay.js to CommonJS/AMD requirements.

--- mode/django/django.js
+++ mode/django/django.js
@@ -1,12 +1,15 @@
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"), require("../htmlmixed/htmlmixed"));
+    mod(require("../../lib/codemirror"), require("../htmlmixed/htmlmixed"),
+        require("../../addon/mode/overlay"));
   else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror", "../htmlmixed/htmlmixed"], mod);
+    define(["../../lib/codemirror", "../htmlmixed/htmlmixed",
+            "../../addon/mode/overlay"], mod);
   else // Plain browser env
     mod(CodeMirror);
 })(function(CodeMirror) {
   "use strict";
+
   CodeMirror.defineMode("django:inner", function() {
     var keywords = ["block", "endblock", "for", "endfor", "in", "true", "false",
                     "loop", "none", "self", "super", "if", "endif", "as", "not", "and",
