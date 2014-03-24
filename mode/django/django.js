777e222a392b838344aca9cd12ea1e2e7ed138a2
paris <pariskasidiaris@gmail.com>
2014-03-24 02:18:23 +0000

    Fixed indentation and added CommonJS/AMD support

--- mode/django/django.js
+++ mode/django/django.js
@@ -1,50 +1,59 @@
-"use strict";
-CodeMirror.defineMode("django:inner", function() {
-  var keywords = ["block", "endblock", "for", "endfor", "in", "true", "false",
-                  "loop", "none", "self", "super", "if", "endif", "as", "not", "and",
-                  "else", "import", "with", "endwith", "without", "context", "ifequal", "endifequal",
-                  "ifnotequal", "endifnotequal", "extends", "include", "load", "length"];
-    keywords = new RegExp("^((" + keywords.join(")|(") + "))\\b");
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"), require("../htmlmixed/htmlmixed"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror", "../htmlmixed/htmlmixed"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
+})(function(CodeMirror) {
+  "use strict";
+  CodeMirror.defineMode("django:inner", function() {
+    var keywords = ["block", "endblock", "for", "endfor", "in", "true", "false",
+                    "loop", "none", "self", "super", "if", "endif", "as", "not", "and",
+                    "else", "import", "with", "endwith", "without", "context", "ifequal", "endifequal",
+                    "ifnotequal", "endifnotequal", "extends", "include", "load", "length"];
+      keywords = new RegExp("^((" + keywords.join(")|(") + "))\\b");
 
-    function tokenBase (stream, state) {
-        var ch = stream.next();
-        if (ch == "{") {
-            if (ch = stream.eat(/\{|%|#/)) {
-                state.tokenize = inTag(ch);
-                return "tag";
-            }
-        }
-    }
-    function inTag (close) {
-        if (close == "{") {
-            close = "}";
-        }
-        return function (stream, state) {
-            var ch = stream.next();
-            if ((ch == close) && stream.eat("}")) {
-                state.tokenize = tokenBase;
-                return "tag";
-            }
-            if (stream.match(keywords)) {
-                return "keyword";
-            }
-            return close == "#" ? "comment" : "string";
-        };
-    }
-    return {
-        startState: function () {
-            return {tokenize: tokenBase};
-        },
-        token: function (stream, state) {
-            return state.tokenize(stream, state);
-        }
-    };
+      function tokenBase (stream, state) {
+          var ch = stream.next();
+          if (ch == "{") {
+              if (ch = stream.eat(/\{|%|#/)) {
+                  state.tokenize = inTag(ch);
+                  return "tag";
+              }
+          }
+      }
+      function inTag (close) {
+          if (close == "{") {
+              close = "}";
+          }
+          return function (stream, state) {
+              var ch = stream.next();
+              if ((ch == close) && stream.eat("}")) {
+                  state.tokenize = tokenBase;
+                  return "tag";
+              }
+              if (stream.match(keywords)) {
+                  return "keyword";
+              }
+              return close == "#" ? "comment" : "string";
+          };
+      }
+      return {
+          startState: function () {
+              return {tokenize: tokenBase};
+          },
+          token: function (stream, state) {
+              return state.tokenize(stream, state);
+          }
+      };
+  });
+
+  CodeMirror.defineMode("django", function(config) {
+    var htmlBase = CodeMirror.getMode(config, "text/html");
+    var djangoInner = CodeMirror.getMode(config, "django:inner");
+    return CodeMirror.overlayMode(htmlBase, djangoInner);
+  });
+
+  CodeMirror.defineMIME("text/x-django", "django");
 });
-
-CodeMirror.defineMode("django", function(config) {
-  var htmlBase = CodeMirror.getMode(config, "text/html");
-  var djangoInner = CodeMirror.getMode(config, "django:inner");
-  return CodeMirror.overlayMode(htmlBase, djangoInner);
-});
-
-CodeMirror.defineMIME("text/x-django", "django");
