71fee62a7d5775a5058dbfae917044cf2219338b
as3boyan <denis.flash.as3@gmail.com>
2014-03-25 18:00:33 +0200

    Fix for Haxe mode
    Fix indent for types

--- mode/haxe/haxe.js
+++ mode/haxe/haxe.js
@@ -322,6 +322,7 @@ CodeMirror.defineMode("haxe", function(config, parserConfig) {
   function typedef (type, value)
   {
   if(type == "variable" && /[A-Z]/.test(value.charAt(0))) { registerimport(value); return cont(); }
+  else if (type == "type" && /[A-Z]/.test(value.charAt(0))) { return cont(); }
   }
 
   function maybelabel(type) {
