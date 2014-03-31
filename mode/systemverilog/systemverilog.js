512a594321b8bcbb7df899603e4e9ce59f18792e
Doug Wikle <dwikle@gmail.com>
2014-03-31 18:26:45 -0400

    [systemverilog mode] Wrapped module in AMD and CommonJS-supporting shims

--- mode/systemverilog/systemverilog.js
+++ mode/systemverilog/systemverilog.js
@@ -1,3 +1,13 @@
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
+})(function(CodeMirror) {
+"use strict";
+
 CodeMirror.defineMode("systemverilog", function(config, parserConfig) {
 
   var indentUnit = config.indentUnit,
@@ -328,3 +338,5 @@ CodeMirror.defineMode("systemverilog", function(config, parserConfig) {
     name: "systemverilog"
   });
 }());
+
+});
