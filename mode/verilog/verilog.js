b4bc93454838f104e91c28440b894f0b51197e0d
Doug Wikle <dwikle@gmail.com>
2014-04-02 08:50:58 -0400

    [verilog mode] Changed from using electricChars to electricInput
    Now reindentation is only invoked when a closing keyword is present
    on the current line, or on bracket chars.

--- mode/verilog/verilog.js
+++ mode/verilog/verilog.js
@@ -251,11 +251,28 @@ CodeMirror.defineMode("verilog", function(config, parserConfig) {
     }
   }
 
+  function buildElectricInputRegEx() {
+    // Reindentation should occur on any bracket char: {}()[]
+    // or on a match of any of the block closing keywords, at
+    // the end of a line
+    var allClosings = [];
+    for (var i in openClose) {
+      if (openClose[i]) {
+        var closings = openClose[i].split(";");
+        for (var j in closings) {
+          allClosings.push(closings[j]);
+        }
+      }
+    }
+    var re = new RegExp("[{}()\\[\\]]|(" + allClosings.join("|") + ")$");
+    return re;
+  }
+
   // Interface
   return {
 
-    // Characters to force current line to reindent
-    electricChars: "{}()[];abcdefghijklmnopqrstuvwxyz",
+    // Regex to force current line to reindent
+    electricInput: buildElectricInputRegEx(),
 
     startState: function(basecolumn) {
       return {
