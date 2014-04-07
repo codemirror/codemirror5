d848c8c52f301974e989490e63214f8e0c779242
Marijn Haverbeke <marijnh@gmail.com>
2014-04-07 11:20:38 +0200

    [merge addon] Verify that diff is up to date in left/rightChunks
    Issue #2438

--- addon/merge/merge.js
+++ addon/merge/merge.js
@@ -46,6 +46,14 @@
     }
   };
 
+  function ensureDiff(dv) {
+    if (dv.diffOutOfDate) {
+      dv.diff = getDiff(dv.orig.getValue(), dv.edit.getValue());
+      dv.diffOutOfDate = false;
+      CodeMirror.signal(dv.edit, "updateDiff", dv.diff);
+    }
+  }
+
   function registerUpdate(dv) {
     var edit = {from: 0, to: 0, marked: []};
     var orig = {from: 0, to: 0, marked: []};
@@ -58,11 +66,7 @@
         clearMarks(dv.orig, orig.marked, dv.classes);
         edit.from = edit.to = orig.from = orig.to = 0;
       }
-      if (dv.diffOutOfDate) {
-        dv.diff = getDiff(dv.orig.getValue(), dv.edit.getValue());
-        dv.diffOutOfDate = false;
-        CodeMirror.signal(dv.edit, "updateDiff", dv.diff);
-      }
+      ensureDiff(dv);
       if (dv.showDifferences) {
         updateMarks(dv.edit, dv.diff, edit, DIFF_INSERT, dv.classes);
         updateMarks(dv.orig, dv.diff, orig, DIFF_DELETE, dv.classes);
@@ -355,10 +359,10 @@
       if (this.left) this.left.setShowDifferences(val);
     },
     rightChunks: function() {
-      return this.right && getChunks(this.right.diff);
+      return this.right && getChunks(this.right);
     },
     leftChunks: function() {
-      return this.left && getChunks(this.left.diff);
+      return this.left && getChunks(this.left);
     }
   };
 
@@ -409,9 +413,10 @@
       f(startOrig, orig.line + 1, startEdit, edit.line + 1);
   }
 
-  function getChunks(diff) {
+  function getChunks(dv) {
+    ensureDiff(dv);
     var collect = [];
-    iterateChunks(diff, function(topOrig, botOrig, topEdit, botEdit) {
+    iterateChunks(dv.diff, function(topOrig, botOrig, topEdit, botEdit) {
       collect.push({origFrom: topOrig, origTo: botOrig,
                     editFrom: topEdit, editTo: botEdit});
     });
