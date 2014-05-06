60ab165bfa2bfc81b97d3b2b2d16c9665002adf5
Marijn Haverbeke <marijnh@gmail.com>
2014-05-06 11:04:27 +0200

    [dylan mode] Add module loading shim
    Closes #2533

--- mode/dylan/dylan.js
+++ mode/dylan/dylan.js
@@ -1,3 +1,13 @@
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
+})(function(CodeMirror) {
+"use strict";
+
 CodeMirror.defineMode("dylan", function(_config) {
   // Words
   var words = {
@@ -282,3 +292,5 @@ CodeMirror.defineMode("dylan", function(_config) {
 });
 
 CodeMirror.defineMIME("text/x-dylan", "dylan");
+
+});
