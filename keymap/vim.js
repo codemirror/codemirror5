8baac5f8278eeb688cb25107573a8e5ac42771c0
binny <hackbinnyarora@gmail.com>
2014-06-06 07:04:22 +0530

    implemented as 2 instances of the same class

--- keymap/vim.js
+++ keymap/vim.js
@@ -638,9 +638,10 @@
         // Recording latest f, t, F or T motion command.
         lastChararacterSearch: {increment:0, forward:true, selectedCharacter:''},
         registerController: new RegisterController({}),
-        // search history buffers
-        searchHistoryController: new SearchHistoryController({}),
-        exCommandHistoryController: new ExCommandHistoryController({})
+        // search history buffer
+        searchHistoryController: new HistoryController({}),
+        // ex Command history buffer
+        exCommandHistoryController : new HistoryController({})
       };
       for (var optionName in options) {
         var option = options[optionName];
@@ -911,18 +912,18 @@
         }
       }
     };
-    function SearchHistoryController() {
+    function HistoryController() {
         this.historyBuffer = [];
         this.iterator;
         this.initialPrefix = null;
     }
-    SearchHistoryController.prototype = {
-      // the query argument here acts a user entered prefix for a small time
+    HistoryController.prototype = {
+      // the input argument here acts a user entered prefix for a small time
       // until we start autocompletion in which case it is the autocompleted.
-      nextMatch: function (query, up) {
+      nextMatch: function (input, up) {
         var historyBuffer = this.historyBuffer;
         var dir = up ? -1 : 1;
-        if (this.initialPrefix === null) this.initialPrefix = query;
+        if (this.initialPrefix === null) this.initialPrefix = input;
         for (var i = this.iterator + dir; up ? i >= 0 : i < historyBuffer.length; i+= dir) {
           var element = historyBuffer[i];
           for (var j = 0; j <= element.length; j++) {
@@ -937,49 +938,13 @@
           this.iterator = historyBuffer.length;
           return this.initialPrefix;
         }
-        // return the last autocompleted query as it is.
-        if (i < 0 ) return query;
+        // return the last autocompleted query or exCommand as it is.
+        if (i < 0 ) return input;
       },
-      pushQuery: function(query) {
-        var index = this.historyBuffer.indexOf(query);
+      pushInput: function(input) {
+        var index = this.historyBuffer.indexOf(input);
         if (index > -1) this.historyBuffer.splice(index, 1);
-        if (query.length) this.historyBuffer.push(query);
-      },
-      reset: function() {
-        this.initialPrefix = null;
-        this.iterator = this.historyBuffer.length;
-      }
-    };
-    function ExCommandHistoryController() {
-      this.historyBuffer = [];
-      this.iterator;
-      this.initialPrefix = null;
-    }
-    ExCommandHistoryController.prototype = {
-      // same logic as in search history controller
-      nextMatch: function (command, up) {
-        var historyBuffer = this.historyBuffer;
-        var dir = up ? -1 : 1;
-        if (this.initialPrefix === null) this.initialPrefix = command;
-        for (var i = this.iterator + dir; up ? i >= 0 : i < historyBuffer.length; i+= dir) {
-          var element = historyBuffer[i];
-          for (var j = 0; j <= element.length; j++) {
-            if (this.initialPrefix == element.substring(0, j)) {
-              this.iterator = i;
-              return element;
-            }
-          }
-        }
-        if (i >= historyBuffer.length) {
-          this.iterator = historyBuffer.length;
-          return this.initialPrefix;
-        }
-        if (i < 0 ) return command;
-      },
-      pushInput: function(command) {
-        var index = this.historyBuffer.indexOf(command);
-        if (index > -1) this.historyBuffer.splice(index, 1);
-        if (command.length) this.historyBuffer.push(command);
+        if (input.length) this.historyBuffer.push(input);
       },
       reset: function() {
         this.initialPrefix = null;
@@ -1172,7 +1137,7 @@
         var originalQuery = getSearchState(cm).getQuery();
         var originalScrollPos = cm.getScrollInfo();
         function handleQuery(query, ignoreCase, smartCase) {
-          vimGlobalState.searchHistoryController.pushQuery(query);
+          vimGlobalState.searchHistoryController.pushInput(query);
           vimGlobalState.searchHistoryController.reset();
           try {
             updateSearchQuery(cm, query, ignoreCase, smartCase);
@@ -1221,7 +1186,7 @@
         function onPromptKeyDown(e, query, close) {
           var keyName = CodeMirror.keyName(e);
           if (keyName == 'Esc' || keyName == 'Ctrl-C' || keyName == 'Ctrl-[') {
-            vimGlobalState.searchHistoryController.pushQuery(query);
+            vimGlobalState.searchHistoryController.pushInput(query);
             vimGlobalState.searchHistoryController.reset();
             updateSearchQuery(cm, originalQuery);
             clearSearchHighlight(cm);
