e3e455b25c3fde3152a65cc4a10ed76fb017d067
Travis Heppe <heppe@google.com>
2014-06-09 06:50:59 -0700

    [vim] Asynchronous confirm for :global when the confirm option is specified.

--- keymap/vim.js
+++ keymap/vim.js
@@ -3403,7 +3403,7 @@
       { name: 'redo', shortName: 'red' },
       { name: 'set', shortName: 'set' },
       { name: 'sort', shortName: 'sor' },
-      { name: 'substitute', shortName: 's' },
+      { name: 'substitute', shortName: 's', possiblyAsync: true },
       { name: 'nohlsearch', shortName: 'noh' },
       { name: 'delmarks', shortName: 'delm' },
       { name: 'registers', shortName: 'reg', excludeFromCommandHistory: true },
@@ -3413,7 +3413,7 @@
       this.buildCommandMap_();
     };
     Vim.ExCommandDispatcher.prototype = {
-      processCommand: function(cm, input) {
+      processCommand: function(cm, input, opt_params) {
         var vim = cm.state.vim;
         var commandHistoryRegister = vimGlobalState.registerController.getRegister(':');
         var previousCommand = commandHistoryRegister.toString();
@@ -3423,7 +3423,7 @@
         var inputStream = new CodeMirror.StringStream(input);
         // update ": with the latest command whether valid or invalid
         commandHistoryRegister.setText(input);
-        var params = {};
+        var params = opt_params || {};
         params.input = input;
         try {
           this.parseInput_(cm, inputStream, params);
@@ -3431,6 +3431,7 @@
           showConfirm(cm, e);
           throw e;
         }
+        var command;
         var commandName;
         if (!params.commandName) {
           // If only a line range is defined, move to the line.
@@ -3438,7 +3439,7 @@
             commandName = 'move';
           }
         } else {
-          var command = this.matchCommand_(params.commandName);
+          command = this.matchCommand_(params.commandName);
           if (command) {
             commandName = command.name;
             if (command.excludeFromCommandHistory) {
@@ -3464,6 +3465,12 @@
         }
         try {
           exCommands[commandName](cm, params);
+          // Possibly asynchronous commands (e.g. substitute, which might have a
+          // user confirmation), are responsible for calling the callback when
+          // done. All others have it taken care of for them here.
+          if ((!command || !command.possiblyAsync) && params.callback) {
+            params.callback();
+          }
         } catch(e) {
           showConfirm(cm, e);
           throw e;
@@ -3835,8 +3842,8 @@
           cmd = tokens.slice(1, tokens.length).join('/');
         }
         if (regexPart) {
-        // If regex part is empty, then use the previous query. Otherwise use
-        // the regex part as the new query.
+          // If regex part is empty, then use the previous query. Otherwise
+          // use the regex part as the new query.
           try {
            updateSearchQuery(cm, regexPart, true /** ignoreCase */,
              true /** smartCase */);
@@ -3845,7 +3852,8 @@
            return;
           }
         }
-        // now that we have the regexPart, search for regex matches in the specified range of lines
+        // now that we have the regexPart, search for regex matches in the
+        // specified range of lines
         var query = getSearchState(cm).getQuery();
         var matchedLines = [], content = '';
         for (var i = lineStart; i <= lineEnd; i++) {
@@ -3860,10 +3868,17 @@
           showConfirm(cm, content);
           return;
         }
-        for (var i = 0; i < matchedLines.length ; i++) {
-          var command = matchedLines[i] + cmd;
-          exCommandDispatcher.processCommand(cm, command);
-        }
+        var index = 0;
+        var nextCommand = function() {
+          if (index < matchedLines.length) {
+            var command = matchedLines[index] + cmd;
+            exCommandDispatcher.processCommand(cm, command, {
+              callback: nextCommand
+            });
+          }
+          index++;
+        };
+        nextCommand();
       },
       substitute: function(cm, params) {
         if (!cm.getSearchCursor) {
@@ -3935,7 +3950,7 @@
         }
         var startPos = clipCursorToContent(cm, Pos(lineStart, 0));
         var cursor = cm.getSearchCursor(query, startPos);
-        doReplace(cm, confirm, lineStart, lineEnd, cursor, query, replacePart);
+        doReplace(cm, confirm, lineStart, lineEnd, cursor, query, replacePart, params.callback);
       },
       redo: CodeMirror.commands.redo,
       undo: CodeMirror.commands.undo,
@@ -4024,9 +4039,10 @@
     * @param {RegExp} query Query for performing matches with.
     * @param {string} replaceWith Text to replace matches with. May contain $1,
     *     $2, etc for replacing captured groups using Javascript replace.
+    * @param {function()} callback A callback for when the replace is done.
     */
     function doReplace(cm, confirm, lineStart, lineEnd, searchCursor, query,
-        replaceWith) {
+        replaceWith, callback) {
       // Set up all the functions.
       cm.state.vim.exMode = true;
       var done = false;
@@ -4067,6 +4083,7 @@
           vim.exMode = false;
           vim.lastHPos = vim.lastHSPos = lastPos.ch;
         }
+        if (callback) { callback(); };
       }
       function onPromptKeyDown(e, _value, close) {
         // Swallow all keys.
@@ -4078,7 +4095,13 @@
           case 'N':
             next(); break;
           case 'A':
-            cm.operation(replaceAll); break;
+            // replaceAll contains a call to close of its own. We don't want it
+            // to fire too early or multiple times.
+            var savedCallback = callback;
+            callback = undefined;
+            cm.operation(replaceAll);
+            callback = savedCallback;
+            break;
           case 'L':
             replace();
             // fall through and exit.
@@ -4100,6 +4123,7 @@
       }
       if (!confirm) {
         replaceAll();
+        if (callback) { callback(); };
         return;
       }
       showPrompt(cm, {
