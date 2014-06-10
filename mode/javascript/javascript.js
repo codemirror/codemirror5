f7b4edfa221f12d3ddce506219bc4839e158c1c7
Alexandre Bique <bique.alexandre@gmail.com>
2014-06-10 16:51:40 +0200

    Add mime-type application/x-javascript

--- mode/javascript/javascript.js
+++ mode/javascript/javascript.js
@@ -672,6 +672,7 @@ CodeMirror.registerHelper("wordChars", "javascript", /[\\w$]/);
 CodeMirror.defineMIME("text/javascript", "javascript");
 CodeMirror.defineMIME("text/ecmascript", "javascript");
 CodeMirror.defineMIME("application/javascript", "javascript");
+CodeMirror.defineMIME("application/x-javascript", "javascript");
 CodeMirror.defineMIME("application/ecmascript", "javascript");
 CodeMirror.defineMIME("application/json", {name: "javascript", json: true});
 CodeMirror.defineMIME("application/x-json", {name: "javascript", json: true});
