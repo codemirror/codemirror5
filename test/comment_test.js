1e78035dd3a7fedc762210a57c852073dbe3d19d
mtaran-google <mtaran@google.com>
2014-06-23 17:21:18 -0700

    [comment tests] Test ctrl+/ on inline block comments

--- test/comment_test.js
+++ test/comment_test.js
@@ -9,6 +9,9 @@ namespace = "comment_";
   }
 
   var simpleProg = "function foo() {\n  return bar;\n}";
+  var inlineBlock = "foo(/* bar */ true);";
+  var inlineBlocks = "foo(/* bar */ true, /* baz */ false);";
+  var multiLineInlineBlock = ["above();", "foo(/* bar */ true);", "below();"];
 
   test("block", "javascript", function(cm) {
     cm.blockComment(Pos(0, 3), Pos(3, 0), {blockCommentLead: " *"});
@@ -18,6 +21,17 @@ namespace = "comment_";
     cm.blockComment(Pos(0, 3), Pos(2, 0), {blockCommentLead: " *"});
     cm.uncomment(Pos(0, 3), Pos(2, 0), {blockCommentLead: " *"});
   }, simpleProg, simpleProg);
+
+  test("blockToggle2", "javascript", function(cm) {
+    cm.setCursor({line: 0, ch: 7 /* inside the block comment */});
+    cm.execCommand("toggleComment");
+  }, inlineBlock, "foo(bar true);");
+
+  // This test should work but currently fails.
+  // test("blockToggle3", "javascript", function(cm) {
+  //   cm.setCursor({line: 0, ch: 7 /* inside the first block comment */});
+  //   cm.execCommand("toggleComment");
+  // }, inlineBlocks, "foo(bar true, /* baz */ false);");
 
   test("line", "javascript", function(cm) {
     cm.lineComment(Pos(1, 1), Pos(1, 1));
@@ -35,6 +49,29 @@ namespace = "comment_";
   test("fallbackToLine", "ruby", function(cm) {
     cm.blockComment(Pos(0, 0), Pos(1));
   }, "def blah()\n  return hah\n", "# def blah()\n#   return hah\n");
+
+  test("ignoreExternalBlockComments", "javascript", function(cm) {
+    cm.execCommand("toggleComment");
+  }, inlineBlocks, "// " + inlineBlocks);
+
+  test("ignoreExternalBlockComments2", "javascript", function(cm) {
+    cm.setCursor({line: 0, ch: null /* eol */});
+    cm.execCommand("toggleComment");
+  }, inlineBlocks, "// " + inlineBlocks);
+
+  test("ignoreExternalBlockCommentsMultiLineAbove", "javascript", function(cm) {
+    cm.setSelection({line: 0, ch: 0}, {line: 1, ch: 1});
+    cm.execCommand("toggleComment");
+  }, multiLineInlineBlock.join("\n"), ["// " + multiLineInlineBlock[0],
+                                       "// " + multiLineInlineBlock[1],
+                                       multiLineInlineBlock[2]].join("\n"));
+
+  test("ignoreExternalBlockCommentsMultiLineBelow", "javascript", function(cm) {
+    cm.setSelection({line: 1, ch: 13 /* after end of block comment */}, {line: 2, ch: 1});
+    cm.execCommand("toggleComment");
+  }, multiLineInlineBlock.join("\n"), [multiLineInlineBlock[0],
+                                       "// " + multiLineInlineBlock[1],
+                                       "// " + multiLineInlineBlock[2]].join("\n"));
 
   test("commentRange", "javascript", function(cm) {
     cm.blockComment(Pos(1, 2), Pos(1, 13), {fullLines: false});
