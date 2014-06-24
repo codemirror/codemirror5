da716e0bda0a00429f072309f55435b95f7496e4
Marijn Haverbeke <marijnh@gmail.com>
2014-06-24 21:14:47 +0200

    [sublime] Fix null dereference bug introduced by #2655

--- keymap/sublime.js
+++ keymap/sublime.js
@@ -494,6 +494,7 @@
 
   function findAndGoTo(cm, forward) {
     var target = getTarget(cm);
+    if (!target) return;
     var query = target.query;
     var cur = cm.getSearchCursor(query, forward ? target.to : target.from);
 
@@ -512,6 +513,7 @@
   cmds[map["Shift-" + ctrl + "F3"] = "findUnderPrevious"] = function(cm) { findAndGoTo(cm,false); };
   cmds[map["Alt-F3"] = "findAllUnder"] = function(cm) {
     var target = getTarget(cm);
+    if (!target) return;
     var cur = cm.getSearchCursor(target.query);
     var matches = [];
     var primaryIndex = -1;
