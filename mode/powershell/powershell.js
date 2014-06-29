20641be6a4973f7914d4a7ef4261f78ebe969411
Andrey Shchekin <ashmind@gmail.com>
2014-06-30 00:20:00 +1200

    [powershell mode] Implement actual PowerShell syntax.

--- mode/powershell/powershell.js
+++ mode/powershell/powershell.js
@@ -1,191 +1,345 @@
-CodeMirror.defineMode("powershell", function() {
-    var ERRORCLASS = 'error';
+// Initially based on CodeMirror Python mode, copyright (c) by Marijn Haverbeke and others
+// PowerShell mode, copyright (c) Andrey Shchekin, VapidWorx and others
+// Distributed under an MIT license: http://codemirror.net/LICENSE
 
-    function wordRegexp(words) {
-        return new RegExp("^((" + words.join(")|(") + "))\\b");
+(function(mod) {
+  'use strict';
+  if (typeof exports == 'object' && typeof module == 'object') // CommonJS
+    mod(require('codemirror'));
+  else if (typeof define == 'function' && define.amd) // AMD
+    define(['codemirror'], mod);
+  else // Plain browser env
+    mod(window.CodeMirror);
+})(function(CodeMirror) {
+'use strict';
+
+CodeMirror.defineMode('powershell', function() {
+    function buildRegexp(patterns, options) {
+        options = options || {};
+        var prefix = options.prefix !== undefined ? options.prefix : '^';
+        var suffix = options.suffix !== undefined ? options.suffix : '\\b';
+
+        for (var i = 0; i < patterns.length; i++) {
+            if (patterns[i] instanceof RegExp) {
+                patterns[i] = patterns[i].source;
+            }
+            else {
+                patterns[i] = patterns[i].replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+            }
+        }
+
+        return new RegExp(prefix + '(' + patterns.join('|') + ')' + suffix, 'i');
     }
 
-    var wordOperators = wordRegexp(['-eq', '-ne', '-gt', '-lt', '-le', '-ge']);
-    var commonkeywords = ['begin', 'break', 'continue', 'do', 'default', 'else', 'elseif',
-  					  'end', 'filter', 'for', 'foreach', 'function', 'if', 'in', 'param', 
-						  'process', 'return', 'switch', 'until', 'where', 'while'];
+    var notCharacterOrDash = '(?=[^A-Z\\d\\-_]|$)';
+    var keywords = buildRegexp([
+        /begin|break|catch|continue|data|default|do|dynamicparam/,
+        /else|elseif|end|exit|filter|finally|for|foreach|from|function|if|in/,
+        /param|process|return|switch|throw|trap|try|until|where|while/
+    ], { suffix: notCharacterOrDash });
 
-	var isOperatorChar = /[+\-*&^%:=<>!|\/]/;
-	var isString = /("|')(\`?.)*?\1/;
-	
-	var keywords = wordRegexp(commonkeywords);
-    //var builtins = wordRegexp(commonBuiltins);
+    var punctuation = /[\[\]{},;`\.]|@[({]/;
+    var wordOperators = buildRegexp([
+        'f',
+        /b?not/,
+        /[ic]?split/, 'join',
+        /is(not)?/, 'as',
+        /[ic]?(eq|ne|[gl][te])/,
+        /[ic]?(not)?(like|match|contains)/,
+        /[ic]?replace/,
+        /b?(and|or|xor)/
+    ], { prefix: '-' });
+    var symbolOperators = /[+\-*\/%]=|\+\+|--|\.\.|[+\-*&^%:=<>!|\/]/;
+    var operators = buildRegexp([wordOperators, symbolOperators], { suffix: '' });
 
-    var indentInfo = null;
+    var numbers = /^[+-]?(0x[\da-f]+|(\d+(\.\d+)?|\.\d*)(e[\+\-]?\d+)?)[ld]?([kmgtp]b)?/i;
+
+    var identifiers = /^[A-Za-z\_][A-Za-z\-\_\d]*\b/;
+
+    var symbolBuiltins = /[A-Z]:|%|\?/i;
+    var namedBuiltins = buildRegexp([
+        /Add-(Computer|Content|History|Member|PSSnapin|Type)/,
+        /Checkpoint-Computer/,
+        /Clear-(Content|EventLog|History|Host|Item(Property)?|Variable)/,
+        /Compare-Object/,
+        /Complete-Transaction/,
+        /Connect-PSSession/,
+        /ConvertFrom-(Csv|Json|SecureString|StringData)/,
+        /Convert-Path/,
+        /ConvertTo-(Csv|Html|Json|SecureString|Xml)/,
+        /Copy-Item(Property)?/,
+        /Debug-Process/,
+        /Disable-(ComputerRestore|PSBreakpoint|PSRemoting|PSSessionConfiguration)/,
+        /Disconnect-PSSession/,
+        /Enable-(ComputerRestore|PSBreakpoint|PSRemoting|PSSessionConfiguration)/,
+        /(Enter|Exit)-PSSession/,
+        /Export-(Alias|Clixml|Console|Counter|Csv|FormatData|ModuleMember|PSSession)/,
+        /ForEach-Object/,
+        /Format-(Custom|List|Table|Wide)/,
+        new RegExp('Get-(Acl|Alias|AuthenticodeSignature|ChildItem|Command|ComputerRestorePoint|Content|ControlPanelItem|Counter|Credential'
+          + '|Culture|Date|Event|EventLog|EventSubscriber|ExecutionPolicy|FormatData|Help|History|Host|HotFix|Item|ItemProperty|Job'
+          + '|Location|Member|Module|PfxCertificate|Process|PSBreakpoint|PSCallStack|PSDrive|PSProvider|PSSession|PSSessionConfiguration'
+          + '|PSSnapin|Random|Service|TraceSource|Transaction|TypeData|UICulture|Unique|Variable|Verb|WinEvent|WmiObject)'),
+        /Group-Object/,
+        /Import-(Alias|Clixml|Counter|Csv|LocalizedData|Module|PSSession)/,
+        /ImportSystemModules/,
+        /Invoke-(Command|Expression|History|Item|RestMethod|WebRequest|WmiMethod)/,
+        /Join-Path/,
+        /Limit-EventLog/,
+        /Measure-(Command|Object)/,
+        /Move-Item(Property)?/,
+        new RegExp('New-(Alias|Event|EventLog|Item(Property)?|Module|ModuleManifest|Object|PSDrive|PSSession|PSSessionConfigurationFile'
+          + '|PSSessionOption|PSTransportOption|Service|TimeSpan|Variable|WebServiceProxy|WinEvent)'),
+        /Out-(Default|File|GridView|Host|Null|Printer|String)/,
+        /Pause/,
+        /(Pop|Push)-Location/,
+        /Read-Host/,
+        /Receive-(Job|PSSession)/,
+        /Register-(EngineEvent|ObjectEvent|PSSessionConfiguration|WmiEvent)/,
+        /Remove-(Computer|Event|EventLog|Item(Property)?|Job|Module|PSBreakpoint|PSDrive|PSSession|PSSnapin|TypeData|Variable|WmiObject)/,
+        /Rename-(Computer|Item(Property)?)/,
+        /Reset-ComputerMachinePassword/,
+        /Resolve-Path/,
+        /Restart-(Computer|Service)/,
+        /Restore-Computer/,
+        /Resume-(Job|Service)/,
+        /Save-Help/,
+        /Select-(Object|String|Xml)/,
+        /Send-MailMessage/,
+        new RegExp('Set-(Acl|Alias|AuthenticodeSignature|Content|Date|ExecutionPolicy|Item(Property)?|Location|PSBreakpoint|PSDebug'
+          + '|PSSessionConfiguration|Service|StrictMode|TraceSource|Variable|WmiInstance)'),
+        /Show-(Command|ControlPanelItem|EventLog)/,
+        /Sort-Object/,
+        /Split-Path/,
+        /Start-(Job|Process|Service|Sleep|Transaction|Transcript)/,
+        /Stop-(Computer|Job|Process|Service|Transcript)/,
+        /Suspend-(Job|Service)/,
+        /TabExpansion2/,
+        /Tee-Object/,
+        /Test-(ComputerSecureChannel|Connection|ModuleManifest|Path|PSSessionConfigurationFile)/,
+        /Trace-Command/,
+        /Unblock-File/,
+        /Undo-Transaction/,
+        /Unregister-(Event|PSSessionConfiguration)/,
+        /Update-(FormatData|Help|List|TypeData)/,
+        /Use-Transaction/,
+        /Wait-(Event|Job|Process)/,
+        /Where-Object/,
+        /Write-(Debug|Error|EventLog|Host|Output|Progress|Verbose|Warning)/,
+        /cd|help|mkdir|more|oss|prompt/,
+        /ac|asnp|cat|cd|chdir|clc|clear|clhy|cli|clp|cls|clv|cnsn|compare|copy|cp|cpi|cpp|cvpa|dbp|del|diff|dir|dnsn|ebp/,
+        /echo|epal|epcsv|epsn|erase|etsn|exsn|fc|fl|foreach|ft|fw|gal|gbp|gc|gci|gcm|gcs|gdr|ghy|gi|gjb|gl|gm|gmo|gp|gps/,
+        /group|gsn|gsnp|gsv|gu|gv|gwmi|h|history|icm|iex|ihy|ii|ipal|ipcsv|ipmo|ipsn|irm|ise|iwmi|iwr|kill|lp|ls|man|md/,
+        /measure|mi|mount|move|mp|mv|nal|ndr|ni|nmo|npssc|nsn|nv|ogv|oh|popd|ps|pushd|pwd|r|rbp|rcjb|rcsn|rd|rdr|ren|ri/,
+        /rjb|rm|rmdir|rmo|rni|rnp|rp|rsn|rsnp|rujb|rv|rvpa|rwmi|sajb|sal|saps|sasv|sbp|sc|select|set|shcm|si|sl|sleep|sls/,
+        /sort|sp|spjb|spps|spsv|start|sujb|sv|swmi|tee|trcm|type|where|wjb|write/,
+    ], { prefix: '', suffix: '' });
+    var variableBuiltins = buildRegexp([
+        /[$?^_]|Args|ConfirmPreference|ConsoleFileName|DebugPreference|Error|ErrorActionPreference|ErrorView|ExecutionContext/,
+        /FormatEnumerationLimit|Home|Host|Input|MaximumAliasCount|MaximumDriveCount|MaximumErrorCount|MaximumFunctionCount/,
+        /MaximumHistoryCount|MaximumVariableCount|MyInvocation|NestedPromptLevel|OutputEncoding|Pid|Profile|ProgressPreference/,
+        /PSBoundParameters|PSCommandPath|PSCulture|PSDefaultParameterValues|PSEmailServer|PSHome|PSScriptRoot|PSSessionApplicationName/,
+        /PSSessionConfigurationName|PSSessionOption|PSUICulture|PSVersionTable|Pwd|ShellId|StackTrace|VerbosePreference/,
+        /WarningPreference|WhatIfPreference/,
+
+        /Event|EventArgs|EventSubscriber|Sender/,
+        /Matches|Ofs|ForEach|LastExitCode|PSCmdlet|PSItem|PSSenderInfo|This/,
+        /true|false|null/
+    ], { prefix: '\\$', suffix: '' });
+
+    var builtins = buildRegexp([ symbolBuiltins, namedBuiltins, variableBuiltins ], { suffix: notCharacterOrDash });
+
+    var grammar = {
+        keyword: keywords,
+        number: numbers,
+        operator: operators,
+        builtin: builtins,
+        punctuation: punctuation,
+        indetifier: identifiers
+    };
 
     // tokenizers
     function tokenBase(stream, state) {
-        
+        // Handle Comments
+        //var ch = stream.peek();
 
-		
-		// Handle Comments        
-		//var ch = stream.peek();
-		
-		   if (stream.match(keywords)) {
-           return('variable-2');
+        if (stream.eatSpace()) {
+            return null;
         }
-		
-		   if (stream.match(isString)) {
-           return('string');
-        }
-		
-			if (stream.match(wordOperators)) {
-           return('variable-2');
-        }
-			if (stream.match(isOperatorChar)) {
-			return('variable-1');
-		}
-			
-		
-		// Handle Variables
 
-				
-        // Handle Number Literals
-        if (stream.match(/^[0-9\.]/, false)) {
-            var floatLiteral = false;
-            // Floats
-            if (stream.match(/^\d*\.\d+(e[\+\-]?\d+)?/i)) { floatLiteral = true; }
-            if (stream.match(/^\d+\.\d*/)) { floatLiteral = true; }
-            if (stream.match(/^\.\d+/)) { floatLiteral = true; }
-            if (floatLiteral) {
-                // Float literals may be "imaginary"
-                stream.eat(/J/i);
-                return 'number';
-            }
-            // Integers
-            var intLiteral = false;
-            // Hex
-            if (stream.match(/^0x[0-9a-f]+/i)) { intLiteral = true; }
-            // Binary
-            if (stream.match(/^0b[01]+/i)) { intLiteral = true; }
-            // Octal
-            if (stream.match(/^0o[0-7]+/i)) { intLiteral = true; }
-            // Decimal
-            if (stream.match(/^[1-9]\d*(e[\+\-]?\d+)?/)) {
-                // Decimal literals may be "imaginary"
-                stream.eat(/J/i);
-                // TODO - Can you have imaginary longs?
-                intLiteral = true;
-            }
-            // Zero by itself with no other piece of number.
-            if (stream.match(/^0(?![\dx])/i)) { intLiteral = true; }
-            if (intLiteral) {
-                // Integer literals may be "long"
-                stream.eat(/L/i);
-                return 'number';
+        var parent = state.returnStack[state.returnStack.length - 1];
+        if (parent && parent.shouldReturnFrom(state)) {
+            state.tokenize = parent.tokenize;
+            state.returnStack.pop();
+            return state.tokenize(stream, state);
+        }
+
+        if (stream.eat('(')) {
+            state.bracketNesting += 1;
+            return 'punctuation';
+        }
+
+        if (stream.eat(')')) {
+            state.bracketNesting -= 1;
+            return 'punctuation';
+        }
+
+        for (var key in grammar) {
+            if (stream.match(grammar[key])) {
+                return key;
             }
         }
 
-		var ch = stream.next();
+        // single-quote string
+        if (stream.match(/'([^']|'')+'/)) {
+            return 'string';
+        }
 
-		if (ch === '$') {
-			if (stream.eat('{')) {
-				state.tokenize = tokenVariable;
-				return tokenVariable(stream, state);
-			} else {
-			stream.eatWhile(/[\w\\\-]/);
-			return 'variable-2';
-			}
-		}
-		
+        var ch = stream.next();
+        if (ch === '$') {
+            return tokenVariable(stream, state);
+        }
+
+        // double-quote string
+        if (ch === '"') {
+            return tokenDoubleQuoteString(stream, state);
+        }
+
         if (ch === '<' && stream.eat('#')) {
-			state.tokenize = tokenComment;
-			return tokenComment(stream, state);
-			}
-        
-		if (ch === '#') {
+            state.tokenize = tokenComment;
+            return tokenComment(stream, state);
+        }
+
+        if (ch === '#') {
             stream.skipToEnd();
             return 'comment';
         }
 
-		if (ch === '@' && stream.eat('\"')) {
-				state.tokenize = tokenMultiString;
-				return tokenMultiString(stream, state);
-			}
-		
-		//if (isOperatorChar.test(ch)) {
-		//	stream.eat;
-			//stream.next;
-		//	return("variable-1");
-		//	}
+        if (ch === '@') {
+            var quoteMatch = stream.eat(/["']/);
+            if (quoteMatch && stream.eol()) {
+                state.tokenize = tokenMultiString;
+                state.startQuote = quoteMatch[0];
+                return tokenMultiString(stream, state);
+            }
+        }
 
         stream.next();
-        return ERRORCLASS;
+        return 'error';
     }
 
-	
-	function tokenComment(stream, state) {
-    var maybeEnd = false, ch;
-      while ((ch = stream.next()) != null) {
-      if (maybeEnd && ch == ">") {
-        state.tokenize = tokenBase;
-        break;
-      }
-      maybeEnd = (ch === '#');
+    function tokenDoubleQuoteString(stream, state) {
+        var ch;
+        while((ch = stream.peek()) != null) {
+            if (ch === '$') {
+                state.tokenize = tokenInterpolation;
+                return 'string';
+            }
+
+            stream.next();
+            if (ch === '`') {
+                stream.next();
+                continue;
+            }
+
+            if (ch === '"' && !stream.eat('"')) {
+                state.tokenize = tokenBase;
+                return 'string';
+            }
+        }
+
+        return 'error';
     }
-    return("comment");
-	}
-   
-	function tokenVariable(stream, state) {
-      while ((ch = stream.next()) != null) {
-      if (ch == "}") {
-        state.tokenize = tokenBase;
-        break;
-      }
+
+    function tokenInterpolation(stream, state) {
+        if (stream.match('$(')) {
+            var savedBracketNesting = state.bracketNesting;
+            state.returnStack.push({
+                /*jshint loopfunc:true */
+                shouldReturnFrom: function(state) {
+                    return state.bracketNesting === savedBracketNesting;
+                },
+                tokenize: tokenDoubleQuoteString
+            });
+            state.tokenize = tokenBase;
+            state.bracketNesting += 1;
+            return 'punctuation';
+        } else {
+            stream.next();
+            state.returnStack.push({
+                shouldReturnFrom: function() { return true; },
+                tokenize: tokenDoubleQuoteString
+            });
+            state.tokenize = tokenVariable;
+            return state.tokenize(stream, state);
+        }
     }
-    return("variable-2");
-	}
-	
-	function tokenMultiString(stream, state) {
-    var maybeEnd = false, ch;
-      while ((ch = stream.next()) != null) {
-      if (maybeEnd && ch == "@") {
-        state.tokenize = tokenBase;
-        break;
-      }
-      maybeEnd = (ch === '"');
+
+    function tokenComment(stream, state) {
+        var maybeEnd = false, ch;
+        while ((ch = stream.next()) != null) {
+          if (maybeEnd && ch == '>') {
+              state.tokenize = tokenBase;
+              break;
+          }
+          maybeEnd = (ch === '#');
+        }
+        return 'comment';
     }
-    return("string");
-	}
-	
-    function tokenLexer(stream, state) {
-        //indentInfo = null;
-        var style = state.tokenize(stream, state);
-        //var current = stream.current();
-        return style;
+
+    function tokenVariable(stream, state) {
+        if (stream.eat('{')) {
+            state.tokenize = tokenVariableWithBraces;
+            return tokenVariableWithBraces(stream, state);
+        } else {
+            stream.eatWhile(/[\w\\\-:]/);
+            state.tokenize = tokenBase;
+            return 'variable-2';
+        }
+    }
+
+    function tokenVariableWithBraces(stream, state) {
+        var ch;
+        while ((ch = stream.next()) != null) {
+            if (ch === '}') {
+                state.tokenize = tokenBase;
+                break;
+            }
+        }
+        return 'variable-2';
+    }
+
+    function tokenMultiString(stream, state) {
+        var quote = state.startQuote;
+        if (stream.sol() && stream.match(new RegExp(quote + '@'))) {
+            state.tokenize = tokenBase;
+        }
+        else {
+            stream.skipToEnd();
+        }
+
+        return 'string';
     }
 
     var external = {
-        startState: function(basecolumn) {
+        startState: function() {
             return {
-              tokenize: tokenBase,
-              scopes: [{offset:basecolumn || 0, type:'py'}],
-              lastToken: null,
-              lambda: false,
-              dedent: 0
-          };
+                returnStack: [],
+                bracketNesting: 0,
+                tokenize: tokenBase
+            };
         },
 
         token: function(stream, state) {
-            var style = tokenLexer(stream, state);
-            state.lastToken = {style:style, content: stream.current()};
-            if (stream.eol() && stream.lambda) {
-                state.lambda = false;
-            }
-
-            return style;
+            return state.tokenize(stream, state);
         },
 
-		blockCommentStart: "<#",
-		blockCommentEnd: "#>",
-        lineComment: "#"
+        blockCommentStart: '<#',
+        blockCommentEnd: '#>',
+        lineComment: '#'
     };
     return external;
 });
 
-CodeMirror.defineMIME("text/x-powershell", "powershell");
+CodeMirror.defineMIME('text/x-powershell', 'powershell');
+});
