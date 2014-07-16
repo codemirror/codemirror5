59138ec896618d730046fda7002d57eef02bd6a8
Marijn Haverbeke <marijnh@gmail.com>
2014-07-16 12:55:59 +0200

    [javascript mode] Indent properly in case of function arg in wrapped arg list

--- mode/javascript/javascript.js
+++ mode/javascript/javascript.js
@@ -298,6 +298,8 @@ CodeMirror.defineMode("javascript", function(config, parserConfig) {
     var result = function() {
       var state = cx.state, indent = state.indented;
       if (state.lexical.type == "stat") indent = state.lexical.indented;
+      else for (var outer = state.lexical; outer && outer.type == ")" && outer.align; outer = outer.prev)
+        indent = outer.indented;
       state.lexical = new JSLexical(indent, cx.stream.column(), type, null, state.lexical, info);
     };
     result.lex = true;
