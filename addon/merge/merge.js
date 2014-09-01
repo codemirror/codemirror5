9b87ca9ca4f8c81f84ff755e921ddaf3aa7373fc
Marijn Haverbeke <marijnh@gmail.com>
2014-09-01 13:34:35 +0200

    [merge addon] Add diff_match_patch as explicit dependency
    Closes #2783

--- addon/merge/merge.js
+++ addon/merge/merge.js
@@ -1,17 +1,17 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
+// declare global: diff_match_patch, DIFF_INSERT, DIFF_DELETE, DIFF_EQUAL
+
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
+    mod(require("../../lib/codemirror"), require("diff_match_patch"));
   else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
+    define(["../../lib/codemirror", "diff_match_patch"], mod);
   else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
+    mod(CodeMirror, diff_match_patch);
+})(function(CodeMirror, diff_match_patch) {
   "use strict";
-  // declare global: diff_match_patch, DIFF_INSERT, DIFF_DELETE, DIFF_EQUAL
-
   var Pos = CodeMirror.Pos;
   var svgNS = "http://www.w3.org/2000/svg";
 
