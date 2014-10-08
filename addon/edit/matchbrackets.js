451e4b8d68fc0820144583ceb7484181fe2a3fe5
Marijn Haverbeke <marijnh@gmail.com>
2014-10-08 17:30:38 +0200

    Make inputStyle an option, move relevant logic into TextareaInput
    Add a dummy ContentEditableInput

--- addon/edit/matchbrackets.js
+++ addon/edit/matchbrackets.js
@@ -81,7 +81,7 @@
     if (marks.length) {
       // Kludge to work around the IE bug from issue #1193, where text
       // input stops going to the textare whever this fires.
-      if (ie_lt8 && cm.state.focused) cm.display.input.focus();
+      if (ie_lt8 && cm.state.focused) cm.focus();
 
       var clear = function() {
         cm.operation(function() {
