0f62924c42a171816ac2fb52993a11765d761d17
paris <paris@sourcelair.com>
2014-10-23 12:51:35 +0300

    Started working on Dockerfile syntax highlighting
    - Created files in tree
    - Created rule for comments

---
+++ mode/dockerfile/dockerfile.js
@@ -0,0 +1,28 @@
+// CodeMirror, copyright (c) by Marijn Haverbeke and others
+// Distributed under an MIT license: http://codemirror.net/LICENSE
+
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"), require("../../addon/mode/simple"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror", "../../addon/mode/simple"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
+})(function(CodeMirror) {
+  "use strict";
+  
+  var directives = ["from", "maintainer", "run", "cmd", "expose", "env",
+                    "add", "copy", "entrypoint", "volume", "user", "workdir",
+                    "onbuild"];
+
+  CodeMirror.defineSimpleMode("dockerfile", {
+    start: [
+      {
+        regex: /#.*/,
+        token: "comment"
+      }
+    ]
+  });
+
+  CodeMirror.defineMIME("text/x-dockerfile", "dockerfile");
+});
