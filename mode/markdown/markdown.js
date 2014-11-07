ea29bcb1d0e251722520440b6f8cad7c7d892408
Dale Jung <dale@dalejung.com>
2014-11-06 22:02:24 -0500

    BUG: having meta.js is miessing up aliases

--- mode/markdown/markdown.js
+++ mode/markdown/markdown.js
@@ -3,9 +3,9 @@
 
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror", require("../xml/xml"), require("../meta.js")));
+    mod(require("../../lib/codemirror", require("../xml/xml"), require("../meta")));
   else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror", "../xml/xml", "../meta.js"], mod);
+    define(["../../lib/codemirror", "../xml/xml", "../meta"], mod);
   else // Plain browser env
     mod(CodeMirror);
 })(function(CodeMirror) {
