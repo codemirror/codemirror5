20df65fb2042f0de779bd2f9e2cb41bf3863c558
Scott Goodhew <sgoodhew@atlassian.com>
2014-11-13 11:13:07 +1100

    Ensure that closeOnEnter can preventDefault
    When closeOnEnter is set the enter event never gets default prevented
    this causes issues for IE9/10, this way implementors can optionally
    preventDefault if they want

--- addon/dialog/dialog.js
+++ addon/dialog/dialog.js
@@ -73,7 +73,7 @@
           CodeMirror.e_stop(e);
           close();
         }
-        if (e.keyCode == 13) callback(inp.value);
+        if (e.keyCode == 13) callback(inp.value, e);
       });
 
       if (options.closeOnBlur !== false) CodeMirror.on(inp, "blur", close);
