e0326500f0e90f481d998632b7ec25268cf04a74
Moritz Schwörer <mr.mosch@gmail.com>
2014-11-26 14:01:43 +0100

    [markdown mode] Fix bad require call

--- mode/markdown/markdown.js
+++ mode/markdown/markdown.js
@@ -3,7 +3,7 @@
 
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror", require("../xml/xml"), require("../meta")));
+    mod(require("../../lib/codemirror"), require("../xml/xml"), require("../meta"));
   else if (typeof define == "function" && define.amd) // AMD
     define(["../../lib/codemirror", "../xml/xml", "../meta"], mod);
   else // Plain browser env
