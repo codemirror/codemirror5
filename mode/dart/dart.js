1d821f88ed89205628813edb424f97ec3946a9a3
Marijn Haverbeke <marijnh@gmail.com>
2014-12-02 11:56:08 +0100

    [dart mode] Simplify, make autoloading work
    Issue #2952

--- mode/dart/dart.js
+++ mode/dart/dart.js
@@ -11,44 +11,28 @@
 })(function(CodeMirror) {
   "use strict";
 
-  function def(mimes, mode) {
-    if (typeof mimes == "string") mimes = [mimes];
-    var words = [];
-    function add(obj) {
-      if (obj) for (var prop in obj) if (obj.hasOwnProperty(prop))
-        words.push(prop);
-    }
-    add(mode.keywords);
-    add(mode.builtin);
-    add(mode.atoms);
-    if (words.length) {
-      mode.helperType = mimes[0];
-      CodeMirror.registerHelper("hintWords", mimes[0], words);
-    }
+  var keywords = ("this super static final const abstract class extends external factory " +
+    "implements get native operator set typedef with enum throw rethrow " +
+    "assert break case continue default in return new deferred async await " +
+    "try catch finally do else for if switch while import library export " +
+    "part of show hide is").split(" ");
+  var blockKeywords = "try catch finally do else for if switch while".split(" ");
+  var atoms = "true false null".split(" ");
+  var builtins = "void bool num int double dynamic var String".split(" ");
 
-    for (var i = 0; i < mimes.length; ++i)
-      CodeMirror.defineMIME(mimes[i], mode);
-  }
-
-  function words(str) {
-    var obj = {}, words = str.split(" ");
+  function set(words) {
+    var obj = {};
     for (var i = 0; i < words.length; ++i) obj[words[i]] = true;
     return obj;
   }
 
-  def("application/dart", {
+  CodeMirror.defineMIME("application/dart", {
     name: "clike",
-    keywords: words(
-      "this super static final const abstract class extends external factory " +
-      "implements get native operator set typedef with enum throw rethrow " +
-      "assert break case continue default in return new deferred async await " +
-      "try catch finally do else for if switch while import library export " +
-      "part of show hide is"
-    ),
+    keywords: set(keywords),
     multiLineStrings: true,
-    blockKeywords: words("try catch finally do else for if switch while"),
-    builtin: words("void bool num int double dynamic var String"),
-    atoms: words("true false null"),
+    blockKeywords: set(blockKeywords),
+    builtin: set(builtins),
+    atoms: set(atoms),
     hooks: {
       "@": function(stream) {
         stream.eatWhile(/[\w\$_]/);
@@ -56,4 +40,11 @@
       }
     }
   });
+
+  CodeMirror.registerHelper("hintWords", "application/dart", keywords.concat(atoms).concat(builtins));
+
+  // This is needed to make loading through meta.js work.
+  CodeMirror.defineMode("dart", function(conf) {
+    return CodeMirror.getMode(conf, "application/dart");
+  }, "clike");
 });
