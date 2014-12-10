2a083f19cde965b06222a676fe1ff8b56089627a
Marijn Haverbeke <marijnh@gmail.com>
2014-12-10 17:23:45 +0100

    [hint addons] Don't include text after the cursor in completion
    Completing inside a word used to try and take the whole word into
    account. This is not typical completion behavior, and often not what
    you want. After this change, the addons only take the part of the
    word in front of the cursor into account.

--- addon/hint/javascript-hint.js
+++ addon/hint/javascript-hint.js
@@ -30,15 +30,20 @@
 
   function scriptHint(editor, keywords, getToken, options) {
     // Find the token at the cursor
-    var cur = editor.getCursor(), token = getToken(editor, cur), tprop = token;
+    var cur = editor.getCursor(), token = getToken(editor, cur);
     if (/\b(?:string|comment)\b/.test(token.type)) return;
     token.state = CodeMirror.innerMode(editor.getMode(), token.state).state;
 
     // If it's not a 'word-style' token, ignore the token.
     if (!/^[\w$_]*$/.test(token.string)) {
-      token = tprop = {start: cur.ch, end: cur.ch, string: "", state: token.state,
-                       type: token.string == "." ? "property" : null};
+      token = {start: cur.ch, end: cur.ch, string: "", state: token.state,
+               type: token.string == "." ? "property" : null};
+    } else if (token.end > cur.ch) {
+      token.end = cur.ch;
+      token.string = token.string.slice(0, cur.ch - token.start);
     }
+
+    var tprop = token;
     // If it is a property, find out what it is a property of.
     while (tprop.type == "property") {
       tprop = getToken(editor, Pos(cur.line, tprop.start));
