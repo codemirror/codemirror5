f61fe9275f07b2dc71ebb328bad755b0d9fd1882
Marijn Haverbeke <marijnh@gmail.com>
2014-12-22 08:07:17 +0100

    [simple mode addon] When indenting, stop scanning on non-dedenting token
    See http://discuss.codemirror.net/t/possible-bug-in-simple-mode-indenting-behavior/109

--- addon/mode/simple.js
+++ addon/mode/simple.js
@@ -194,12 +194,15 @@
       var pos = state.indent.length - 1, rules = states[state.state];
       scan: for (;;) {
         for (var i = 0; i < rules.length; i++) {
-          var rule = rules[i], m = rule.regex.exec(textAfter);
-          if (m && m[0]) {
-            if (rule.data.dedent && rule.data.dedentIfLineStart !== false) pos--;
-            if (rule.next || rule.push) rules = states[rule.next || rule.push];
-            textAfter = textAfter.slice(m[0].length);
-            continue scan;
+          var rule = rules[i];
+          if (rule.data.dedent && rule.data.dedentIfLineStart !== false) {
+            var m = rule.regex.exec(textAfter);
+            if (m && m[0]) {
+              pos--;
+              if (rule.next || rule.push) rules = states[rule.next || rule.push];
+              textAfter = textAfter.slice(m[0].length);
+              continue scan;
+            }
           }
         }
         break;
