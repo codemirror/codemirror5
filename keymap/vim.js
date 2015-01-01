d0a2ddaa0454f6ea00d4c151ce9c1c15a3d84014
Yunchi Luo <mightyguava@gmail.com>
2014-12-31 23:21:33 -0500

    [vim] Enable clearing search scrollbar annotation
    https://github.com/codemirror/CodeMirror/issues/3012

--- keymap/vim.js
+++ keymap/vim.js
@@ -3792,8 +3792,13 @@
       });
     }
     function clearSearchHighlight(cm) {
+      var state = getSearchState(cm);
       cm.removeOverlay(getSearchState(cm).getOverlay());
-      getSearchState(cm).setOverlay(null);
+      state.setOverlay(null);
+      if (state.getScrollbarAnnotate()) {
+        state.getScrollbarAnnotate().clear();
+        state.setScrollbarAnnotate(null);
+      }
     }
     /**
      * Check if pos is in the specified range, INCLUSIVE.
