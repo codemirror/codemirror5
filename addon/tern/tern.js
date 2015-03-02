b4f8836c8f08324283b9cb9cb45714547c3f1f8d
Marijn Haverbeke <marijnh@gmail.com>
2015-03-02 11:13:58 +0100

    [tern addon] Accept closing paren and bracket as the end of an interesting expression

--- addon/tern/tern.js
+++ addon/tern/tern.js
@@ -443,7 +443,7 @@
   function atInterestingExpression(cm) {
     var pos = cm.getCursor("end"), tok = cm.getTokenAt(pos);
     if (tok.start < pos.ch && (tok.type == "comment" || tok.type == "string")) return false;
-    return /\w/.test(cm.getLine(pos.line).slice(Math.max(pos.ch - 1, 0), pos.ch + 1));
+    return /[\w)\]]/.test(cm.getLine(pos.line).slice(Math.max(pos.ch - 1, 0), pos.ch + 1));
   }
 
   // Variable renaming
