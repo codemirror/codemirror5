f5574526eea1ec430560b2883f04982d2cb34738
Marijn Haverbeke <marijnh@gmail.com>
2015-03-18 10:55:35 +0100

    [stylus mode] Fix module import paths
    Issue #3138

--- mode/stylus/stylus.js
+++ mode/stylus/stylus.js
@@ -5,9 +5,9 @@
 
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("lib/codemirror"));
+    mod(require("../../lib/codemirror"));
   else if (typeof define == "function" && define.amd) // AMD
-    define(["lib/codemirror"], mod);
+    define(["../../lib/codemirror"], mod);
   else // Plain browser env
     mod(CodeMirror);
 })(function(CodeMirror) {
