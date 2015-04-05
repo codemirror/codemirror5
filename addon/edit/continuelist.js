62af9c62dd5a14665996a1ea83a3f675822b0ed2
Marijn Haverbeke <marijnh@gmail.com>
2015-04-05 21:47:32 +0200

    [continuelist addon] Don't continue horiziontal rules as lists
    Closes #3178

--- addon/edit/continuelist.js
+++ addon/edit/continuelist.js
@@ -13,29 +13,30 @@
 
   var listRE = /^(\s*)(>[> ]*|[*+-]\s|(\d+)\.)(\s*)/,
       emptyListRE = /^(\s*)(>[> ]*|[*+-]|(\d+)\.)(\s*)$/,
-      unorderedListRE = /[*+-]\s/;
+      unorderedListRE = /[*+-]\s/,
+      ruleRE = /^\s*((-\s*){3}|(\*\s*){3}(_\s*){3})$/;
 
   CodeMirror.commands.newlineAndIndentContinueMarkdownList = function(cm) {
     if (cm.getOption("disableInput")) return CodeMirror.Pass;
     var ranges = cm.listSelections(), replacements = [];
     for (var i = 0; i < ranges.length; i++) {
-      var pos = ranges[i].head, match;
+      var pos = ranges[i].head;
       var eolState = cm.getStateAfter(pos.line);
       var inList = eolState.list !== false;
       var inQuote = eolState.quote !== false;
 
-      if (!ranges[i].empty() || (!inList && !inQuote) || !(match = cm.getLine(pos.line).match(listRE))) {
+      var line = cm.getLine(pos.line), match = listRE.exec(line);
+      if (!ranges[i].empty() || (!inList && !inQuote) || !match || ruleRE.test(line)) {
         cm.execCommand("newlineAndIndent");
         return;
       }
-      if (cm.getLine(pos.line).match(emptyListRE)) {
+      if (emptyListRE.test(line)) {
         cm.replaceRange("", {
           line: pos.line, ch: 0
         }, {
           line: pos.line, ch: pos.ch + 1
         });
         replacements[i] = "\n";
-
       } else {
         var indent = match[1], after = match[4];
         var bullet = unorderedListRE.test(match[2]) || match[2].indexOf(">") >= 0
