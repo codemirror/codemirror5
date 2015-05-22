0c668c67e0493d8a0e3d7b7ebff8ee640e93e668
Madhura Jayaratne <madhura.cj@gmail.com>
2015-05-22 07:32:35 +0530

    Respect alias here as well

--- addon/hint/sql-hint.js
+++ addon/hint/sql-hint.js
@@ -127,13 +127,13 @@
 
     if (columns) {
       addMatches(result, string, columns, function(w) {
+        var tableInsert = table;
+        if (alias == true) tableInsert = aliasTable;
         if (typeof w == "string") {
-          var tableInsert = table;
-          if (alias == true) tableInsert = aliasTable;
           w = tableInsert + "." + w;
         } else {
           w = shallowClone(w);
-          w.text = table + "." + w.text;
+          w.text = tableInsert + "." + w.text;
         }
         return useBacktick ? insertBackticks(w) : w;
       });
