aa5b6b26d7d714ef202228cf049a2ba7cb1cd309
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2015-06-06 16:13:01 +0300

    Update factor.js

--- mode/factor/factor.js
+++ mode/factor/factor.js
@@ -89,13 +89,13 @@
           var stDef = mD[state.state];
           var stMore = state.more;
           //console.log(stMore);
-          if (stMore && stMore.length>0) { 
+          if (stMore && stMore.length>0) {
             //console.log(state.words[0] + " -" + state.more[0] + "- " + state.state);
             stream.match(state.words.shift());
-            return state.more.shift(); 
-          } else { 
+            return state.more.shift();
+          } else {
             state.words = null;
-            state.more = null; 
+            state.more = null;
           }
           for (var idx in stDef) {
             var rule = stDef[idx], rx = new RegExp("^" + rule.regex.source), tk = rule.token, nx = rule.next || state.state;
