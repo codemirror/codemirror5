3ebe7df83714ce43390877945c7a94ca63245b59
Marijn Haverbeke <marijnh@gmail.com>
2015-06-25 13:31:47 +0200

    [tern addon] Allow jump-to-definition in strings

--- addon/tern/tern.js
+++ addon/tern/tern.js
@@ -444,7 +444,7 @@
 
   function atInterestingExpression(cm) {
     var pos = cm.getCursor("end"), tok = cm.getTokenAt(pos);
-    if (tok.start < pos.ch && (tok.type == "comment" || tok.type == "string")) return false;
+    if (tok.start < pos.ch && tok.type == "comment") return false;
     return /[\w)\]]/.test(cm.getLine(pos.line).slice(Math.max(pos.ch - 1, 0), pos.ch + 1));
   }
 
