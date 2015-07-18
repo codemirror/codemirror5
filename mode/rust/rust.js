fc945b69b7ecf0990c755ccb62574b696cc8f139
Rrandom <emanonhere@gmail.com>
2015-07-18 22:35:26 +0800

    add index html

--- mode/rust/rust.js
+++ mode/rust/rust.js
@@ -14,12 +14,22 @@
 	CodeMirror.defineSimpleMode("rust",{
 		start:[
 			{regex: /"(?:[^\\]|\\.)*?"/, token: "string"},
-			{regex: /\b(?:abstract|alignof|as|box|break|continue|const|crate|do|else|enum|extern|for|final|if|impl|in|let|loop|macro|match|mod|move|mut|offsetof|override|priv|proc|pub|pure|ref|return|self|sizeof|static|struct|super|trait|type|typeof|unsafe|unsized|use|virtual|where|while|yield)\b/, token: "keyword"},
+			{regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i,
+			 token: "number"},
+			{regex: /\b(?:abstract|alignof|as|box|break|continue|const|crate|do|else|enum|extern|for|final|if|impl|in|let|loop|macro|match|mod|move|mut|offsetof|override|priv|proc|pub|pure|ref|return|self|sizeof|static|struct|super|trait|type|typeof|unsafe|unsized|use|virtual|where|while|yield)\b/,
+			 token: "kw"},
+			{regex:/\b(fn)\s+([a-zA-Z_][a-zA-Z0-9_]*)/,
+			 token: ["kw", "ident"]
+			},
+			{regex: /[\{\[\(]/, indent: true},
+			{regex: /[\}\]\)]/, dedent: true},
 		],
+
 		comment: [
 			{regex: /.*?\*\//, token: "comment", next: "start"},
 			{regex: /.*/, token: "comment"}
 		],
+
 		meta: {
 			lineComment: "//"
 		}
