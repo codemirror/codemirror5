20ad519f33af49189bc32396ca857767013a03f3
Rrandom <emanonhere@gmail.com>
2015-07-24 17:32:21 +0800

    fix hex, octal and binary number and add to test

---
+++ mode/rust/test.js
@@ -0,0 +1,26 @@
+
+(function() {
+  var mode = CodeMirror.getMode({indentUnit: 4}, "rust");
+  function MT(name) {test.mode(name, mode, Array.prototype.slice.call(arguments, 1));}
+
+  MT('integer_test',
+     '[number 123i32]',
+     '[number 123u32]',
+     '[number 123_u32]',
+     '[number 0xff_u8]',
+     '[number 0o70_i16]',
+     '[number 0b1111_1111_1001_0000_i32]',
+     '[number 0usize]');
+
+  MT('float_test',
+     '[number 123.0f64]',
+     '[number 0.1f64]',
+     '[number 0.1f32]',
+     '[number 12E+99_f64]');
+
+  MT('string-literals-test',
+     "[string-2 'H']",
+     '[string "hello"]',
+     '[string r#"hello"#]');
+
+})();
