3686816436caada9848c4402c5f9e08032e1c06b
Marijn Haverbeke <marijnh@gmail.com>
2015-07-27 13:30:09 +0200

    [lint addon] Revise patch 536bba1

--- addon/lint/lint.js
+++ addon/lint/lint.js
@@ -187,9 +187,8 @@
   CodeMirror.defineOption("lint", false, function(cm, val, old) {
     if (old && old != CodeMirror.Init) {
       clearMarks(cm);
-      if (!state.options.disableLintOnChange) {
+      if (!state.options.lintOnChange !== false)
         cm.off("change", onChange);
-      }
       CodeMirror.off(cm.getWrapperElement(), "mouseover", cm.state.lint.onMouseOver);
       clearTimeout(cm.state.lint.timeout);
       delete cm.state.lint;
@@ -199,9 +198,8 @@
       var gutters = cm.getOption("gutters"), hasLintGutter = false;
       for (var i = 0; i < gutters.length; ++i) if (gutters[i] == GUTTER_ID) hasLintGutter = true;
       var state = cm.state.lint = new LintState(cm, parseOptions(cm, val), hasLintGutter);
-      if (!state.options.disableLintOnChange) {
+      if (!state.options.lintOnChange !== false)
         cm.on("change", onChange);
-      }
       if (state.options.tooltips != false)
         CodeMirror.on(cm.getWrapperElement(), "mouseover", state.onMouseOver);
 
@@ -209,9 +207,7 @@
     }
   });
 
-  CodeMirror.defineExtension("performLint", function(cm) {
-    var state = cm.state.lint;
-    if (!state) return;
-    startLinting(cm);
+  CodeMirror.defineExtension("performLint", function() {
+    if (this.state.lint) startLinting(this);
   });
 });
