bd168a171e0d72c1624ea6fd6ad6b3a881003e2e
Rrandom <emanonhere@gmail.com>
2015-07-30 22:37:02 +0800

    update attributes, comments and string

--- mode/rust/rust.js
+++ mode/rust/rust.js
@@ -15,18 +15,26 @@ CodeMirror.defineSimpleMode("rust",{
   start:[
     {regex: /r?"(?:[^\\]|\\.)*?"/, token: "string"},
     // raw string with #
-    {regex: /r#"(?:[^\\]|\\.)*?"#/, token: "string"},
+    //{regex: /r#"(?:[^\\]|\\.)*?"#/, token: "string"},
     // character
     {regex: /'(?:[^'\\]|\\(?:[nrt0'"]|x[\da-fA-F]{2}|u\{[\da-fA-F]{6}\}))'/, token: "string-2"},
+    // byte
+    {regex: /b'(?:[^']|\\(?:['\\nrt0]|x[\da-fA-F]{2}))'/, token: "string-2"},
+    // byte string
+    {regex: /b"(?:[^"]|\\(?:["\\nrt0]|x[\da-fA-F]{2}|u\{[\da-fA-F]{6}\}))*"/, token: "string-2"},
+
     {regex: /(?:(?:[0-9][0-9_]*)(?:(?:[Ee][+-]?[0-9_]+)|\.[0-9_]+(?:[Ee][+-]?[0-9_]+)?)(?:f32|f64)?)|(?:0(?:b[01_]+|(?:o[0-7_]+)|(?:x[0-9a-fA-F_]+))|(?:[0-9][0-9_]*))(?:u8|u16|u32|u64|i8|i16|i32|i64|isize|usize)?/,
      token: "number"},
-    {regex:/(let\s+(?:mut\s+)?)([a-zA-Z_][a-zA-Z0-9_]*)/, token: ["keyword", "def", null]},
+    {regex: /(let\s+(?:mut\s+)?)([a-zA-Z_][a-zA-Z0-9_]*)/, token: ["keyword", "def"]},
     {regex: /(?:abstract|alignof|as|box|break|continue|const|crate|do|else|enum|extern|for|final|if|impl|in|loop|macro|match|mod|move|offsetof|override|priv|proc|pub|pure|ref|return|self|sizeof|static|struct|super|trait|type|typeof|unsafe|unsized|use|virtual|where|while|yield)\b/, token: "keyword"},
-    {regex: /\b(?:Self|isize|usize|char|bool|u8|u16|u32|u64|f16|f32|f64|i8|i16|i32|i64|str|Option)\b/, token: "atom"},
-    {regex: /\b(?:true|false|Some|None|Ok|Err)\b/,token: "builtin"},
-    {regex:/\b(fn)(\s+)([a-zA-Z_][a-zA-Z0-9_]*)/,
+    {regex: /\b[^:](?:Self|isize|usize|char|bool|u8|u16|u32|u64|f16|f32|f64|i8|i16|i32|i64|str|Option)\b/, token: "atom"},
+    {regex: /\b(?:true|false|Some|None|Ok|Err)\b/, token: "builtin"},
+    {regex: /\b(fn)(\s+)([a-zA-Z_][a-zA-Z0-9_]*)/,
      token: ["keyword", null ,"def"]},
+    {regex: /#!?\[.*\]/, token: "attribute"},
     {regex: /\/\/.*/, token: "comment"},
+    {regex: /\/\*/, token: "comment", next: "comment"},
+    {regex: /[-+\/*=<>!]+/, token: "operator"},
     {regex: /[a-zA-Z_]\w*!/,token: "variable-3"},
     {regex: /[a-zA-Z_]\w*/, token: "variable"},
     {regex: /[\{\[\(]/, indent: true},
