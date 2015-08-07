bc026f1b2e7d188f6742f7a312f65af31dcd0668
Se-Won Kim <kim.sewon@gmail.com>
2015-08-07 11:39:57 +0900

    [tern addon] Use correct primary when selecting variables

--- addon/tern/tern.js
+++ addon/tern/tern.js
@@ -466,12 +466,13 @@
     ts.request(cm, {type: "refs"}, function(error, data) {
       if (error) return showError(ts, cm, error);
       var ranges = [], cur = 0;
+      var curPos = cm.getCursor();
       for (var i = 0; i < data.refs.length; i++) {
         var ref = data.refs[i];
         if (ref.file == name) {
           ranges.push({anchor: ref.start, head: ref.end});
-          if (cmpPos(cur, ref.start) >= 0 && cmpPos(cur, ref.end) <= 0)
-            cur = ranges.length - 1;
+          if (cmpPos(curPos, ref.start) >= 0 && cmpPos(curPos, ref.end) <= 0)
+            cur = i;
         }
       }
       cm.setSelections(ranges, cur);
