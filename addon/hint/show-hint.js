8bb4fd65835025c122f1e9478cd04639dfee1cef
Pauli <wuliupo@163.com>
2015-08-31 17:05:26 +0800

    null check
    if the keywords are not resisted, there will be exception: Cannot read property 'length' of undefined.

--- addon/hint/show-hint.js
+++ addon/hint/show-hint.js
@@ -341,7 +341,7 @@
     if (helpers.length) {
       for (var i = 0; i < helpers.length; i++) {
         var cur = helpers[i](cm, options);
-        if (cur && cur.list.length) return cur;
+        if (cur && cur.list && cur.list.length) return cur;
       }
     } else if (words = cm.getHelper(cm.getCursor(), "hintWords")) {
       if (words) return CodeMirror.hint.fromList(cm, {words: words});
