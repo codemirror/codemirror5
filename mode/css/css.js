661aa16a1c7c26fe319f967ef3141294754a8786
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2015-09-01 16:05:54 +0530

    fix(addons): fix codemirror addons require paths

--- mode/css/css.js
+++ mode/css/css.js
@@ -5,7 +5,7 @@
   if (typeof exports == "object" && typeof module == "object") // CommonJS
     mod(require("../../lib/codemirror"));
   else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
+    define(["codemirror"], mod);
   else // Plain browser env
     mod(CodeMirror);
 })(function(CodeMirror) {
