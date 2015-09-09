1e0923ea4e8a188ee5dd71fd5adc4700b26f8474
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2015-09-09 13:41:00 +0530

    update codemirror deps path to resolve conflict

--- addon/selection/active-line.js
+++ addon/selection/active-line.js
@@ -11,7 +11,7 @@
   if (typeof exports == "object" && typeof module == "object") // CommonJS
     mod(require("../../lib/codemirror"));
   else if (typeof define == "function" && define.amd) // AMD
-    define(["codemirror"], mod);
+    define(["codemirror-lib"], mod);
   else // Plain browser env
     mod(CodeMirror);
 })(function(CodeMirror) {
