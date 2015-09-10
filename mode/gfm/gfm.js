e937b442ead35d031274c28206820237f5202cdd
Wes Cossick <WesCossick@users.noreply.github.com>
2015-09-10 14:27:21 -0500

    Add modeConfig for GitHub Spice

--- mode/gfm/gfm.js
+++ mode/gfm/gfm.js
@@ -14,6 +14,10 @@
 var urlRE = /^((?:coap|doi|javascript|aaa|aaas|about|acap|cap|cid|crid|data|dav|dict|dns|file|ftp|geo|go|gopher|h323|http|https|iax|icap|im|imap|info|ipp|iris|iris\.beep|iris\.xpc|iris\.xpcs|iris\.lwz|ldap|mailto|mid|msrp|msrps|mtqp|mupdate|news|nfs|ni|nih|nntp|opaquelocktoken|pop|pres|rtsp|service|session|shttp|sieve|sip|sips|sms|snmp|soap\.beep|soap\.beeps|tag|tel|telnet|tftp|thismessage|tn3270|tip|tv|urn|vemmi|ws|wss|xcon|xcon-userid|xmlrpc\.beep|xmlrpc\.beeps|xmpp|z39\.50r|z39\.50s|adiumxtra|afp|afs|aim|apt|attachment|aw|beshare|bitcoin|bolo|callto|chrome|chrome-extension|com-eventbrite-attendee|content|cvs|dlna-playsingle|dlna-playcontainer|dtn|dvb|ed2k|facetime|feed|finger|fish|gg|git|gizmoproject|gtalk|hcp|icon|ipn|irc|irc6|ircs|itms|jar|jms|keyparc|lastfm|ldaps|magnet|maps|market|message|mms|ms-help|msnim|mumble|mvn|notes|oid|palm|paparazzi|platform|proxy|psyc|query|res|resource|rmi|rsync|rtmp|secondlife|sftp|sgn|skype|smb|soldat|spotify|ssh|steam|svn|teamspeak|things|udp|unreal|ut2004|ventrilo|view-source|webcal|wtai|wyciwyg|xfire|xri|ymsgr:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]|\([^\s()<>]*\))+(?:\([^\s()<>]*\)|[^\s`*!()\[\]{};:'".,<>?«»“”‘’]))/i
 
 CodeMirror.defineMode("gfm", function(config, modeConfig) {
+  // Should GitHub spice be added (like linking #Num, SHA, etc.)
+  if (modeConfig.gitHubSpice === undefined)
+    modeConfig.gitHubSpice = true;
+  
   var codeDepth = 0;
   function blankLine(state) {
     state.code = false;
@@ -78,7 +82,7 @@ CodeMirror.defineMode("gfm", function(config, modeConfig) {
         state.ateSpace = true;
         return null;
       }
-      if (stream.sol() || state.ateSpace) {
+      if (modeConfig.gitHubSpice && (stream.sol() || state.ateSpace)) {
         state.ateSpace = false;
         if(stream.match(/^(?:[a-zA-Z0-9\-_]+\/)?(?:[a-zA-Z0-9\-_]+@)?(?:[a-f0-9]{7,40}\b)/)) {
           // User/Project@SHA
