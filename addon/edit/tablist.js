9f1535eb6544d05c6f814f1c325b21fc57bd69f5
Wes Cossick <WesCossick@users.noreply.github.com>
2015-09-10 14:51:55 -0500

    Replace tabs with spaces

--- addon/edit/tablist.js
+++ addon/edit/tablist.js
@@ -13,41 +13,41 @@
 
   CodeMirror.commands.tabAndIndentMarkdownList = function(cm) {
     var ranges = cm.listSelections();
-		var pos = ranges[0].head;
-		var eolState = cm.getStateAfter(pos.line);
-		var inList = eolState.list !== false;
+      var pos = ranges[0].head;
+      var eolState = cm.getStateAfter(pos.line);
+      var inList = eolState.list !== false;
 
-		if (inList) {
-			cm.execCommand('indentMore');
-			return;
-		}
+      if (inList) {
+        cm.execCommand('indentMore');
+        return;
+      }
 
-		if(cm.options.indentWithTabs){
-			cm.execCommand('insertTab');
-		}
-		else{
-			var spaces = Array(cm.options.tabSize + 1).join(" ");
-			cm.replaceSelection(spaces);
-		}
+      if(cm.options.indentWithTabs){
+        cm.execCommand('insertTab');
+      }
+      else{
+        var spaces = Array(cm.options.tabSize + 1).join(" ");
+        cm.replaceSelection(spaces);
+      }
   };
 
   CodeMirror.commands.shiftTabAndUnindentMarkdownList = function(cm) {
     var ranges = cm.listSelections();
-		var pos = ranges[0].head;
-		var eolState = cm.getStateAfter(pos.line);
-		var inList = eolState.list !== false;
+    var pos = ranges[0].head;
+    var eolState = cm.getStateAfter(pos.line);
+    var inList = eolState.list !== false;
 
-		if (inList) {
-			cm.execCommand('indentLess');
-			return;
-		}
-		
-		if(cm.options.indentWithTabs){
-			cm.execCommand('insertTab');
-		}
-		else{
-			var spaces = Array(cm.options.tabSize + 1).join(" ");
-			cm.replaceSelection(spaces);
-		}
+    if (inList) {
+      cm.execCommand('indentLess');
+      return;
+    }
+        
+    if(cm.options.indentWithTabs){
+      cm.execCommand('insertTab');
+    }
+    else{
+      var spaces = Array(cm.options.tabSize + 1).join(" ");
+      cm.replaceSelection(spaces);
+    }
   };
 });
