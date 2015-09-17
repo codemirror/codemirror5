ece5c232a43e9260b4c7393de5560ba2b60a4036
Marijn Haverbeke <marijnh@gmail.com>
2015-09-17 14:48:01 +0200

    [gfm mode] Simplify checking of gitHubSpice config parameter

--- mode/gfm/gfm.js
+++ mode/gfm/gfm.js
@@ -14,10 +14,6 @@
 var urlRE = /^((?:(?:aaas?|about|acap|adiumxtra|af[ps]|aim|apt|attachment|aw|beshare|bitcoin|bolo|callto|cap|chrome(?:-extension)?|cid|coap|com-eventbrite-attendee|content|crid|cvs|data|dav|dict|dlna-(?:playcontainer|playsingle)|dns|doi|dtn|dvb|ed2k|facetime|feed|file|finger|fish|ftp|geo|gg|git|gizmoproject|go|gopher|gtalk|h323|hcp|https?|iax|icap|icon|im|imap|info|ipn|ipp|irc[6s]?|iris(?:\.beep|\.lwz|\.xpc|\.xpcs)?|itms|jar|javascript|jms|keyparc|lastfm|ldaps?|magnet|mailto|maps|market|message|mid|mms|ms-help|msnim|msrps?|mtqp|mumble|mupdate|mvn|news|nfs|nih?|nntp|notes|oid|opaquelocktoken|palm|paparazzi|platform|pop|pres|proxy|psyc|query|res(?:ource)?|rmi|rsync|rtmp|rtsp|secondlife|service|session|sftp|sgn|shttp|sieve|sips?|skype|sm[bs]|snmp|soap\.beeps?|soldat|spotify|ssh|steam|svn|tag|teamspeak|tel(?:net)?|tftp|things|thismessage|tip|tn3270|tv|udp|unreal|urn|ut2004|vemmi|ventrilo|view-source|webcal|wss?|wtai|wyciwyg|xcon(?:-userid)?|xfire|xmlrpc\.beeps?|xmpp|xri|ymsgr|z39\.50[rs]?):(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]|\([^\s()<>]*\))+(?:\([^\s()<>]*\)|[^\s`*!()\[\]{};:'".,<>?«»“”‘’]))/i
 
 CodeMirror.defineMode("gfm", function(config, modeConfig) {
-  // Should GitHub spice be added (like linking #Num, SHA, etc.)
-  if (modeConfig.gitHubSpice === undefined)
-    modeConfig.gitHubSpice = true;
-
   var codeDepth = 0;
   function blankLine(state) {
     state.code = false;
@@ -84,7 +80,7 @@ CodeMirror.defineMode("gfm", function(config, modeConfig) {
       }
       if (stream.sol() || state.ateSpace) {
         state.ateSpace = false;
-        if (modeConfig.gitHubSpice) {
+        if (modeConfig.gitHubSpice !== false) {
           if(stream.match(/^(?:[a-zA-Z0-9\-_]+\/)?(?:[a-zA-Z0-9\-_]+@)?(?:[a-f0-9]{7,40}\b)/)) {
             // User/Project@SHA
             // User@SHA
