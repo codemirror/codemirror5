9f149ba3e97a174487f22bac873a422104239703
Marijn Haverbeke <marijnh@gmail.com>
2015-09-21 13:45:23 +0200

    [haxe mode] Don't match start of ... as decimal dot
    Issue #3542

--- mode/haxe/haxe.js
+++ mode/haxe/haxe.js
@@ -66,7 +66,7 @@ CodeMirror.defineMode("haxe", function(config, parserConfig) {
       stream.eatWhile(/[\da-f]/i);
       return ret("number", "number");
     } else if (/\d/.test(ch) || ch == "-" && stream.eat(/\d/)) {
-      stream.match(/^\d*(?:\.\d*)?(?:[eE][+\-]?\d+)?/);
+      stream.match(/^\d*(?:\.\d*(?!\.))?(?:[eE][+\-]?\d+)?/);
       return ret("number", "number");
     } else if (state.reAllowed && (ch == "~" && stream.eat(/\//))) {
       toUnescaped(stream, "/");
