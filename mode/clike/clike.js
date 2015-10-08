41ead67b332fe7591aa455d974165197cde2d5ab
John Engler <jfengler4@gmail.com>
2015-10-07 18:34:34 -0700

    Restore true as a Scala keyword
    Removed in 3d5d9a4af38facd8c2d79abbe74ead61470ca97b as it had been
    mispelled when introduced in eb495233e3813d04c20e495dcf88217e093261fc.

--- mode/clike/clike.js
+++ mode/clike/clike.js
@@ -480,7 +480,7 @@ CodeMirror.defineMode("clike", function(config, parserConfig) {
       /* scala */
       "abstract case catch class def do else extends false final finally for forSome if " +
       "implicit import lazy match new null object override package private protected return " +
-      "sealed super this throw trait try type val var while with yield _ : = => <- <: " +
+      "sealed super this throw trait true try type val var while with yield _ : = => <- <: " +
       "<% >: # @ " +
 
       /* package scala */
