71ac921c4385c7bfcbaa6c83ab105172a0049728
Caitlin Potter <caitpotter88@gmail.com>
2015-10-20 02:48:10 -0400

    [javascript mode] Add a test for new.target

--- mode/javascript/test.js
+++ mode/javascript/test.js
@@ -158,6 +158,14 @@
      "  [keyword return] [variable-2 x];",
      "}");
 
+  MT("new_target",
+     "[keyword function] [def F]([def target]) {",
+     "  [keyword if] ([variable-2 target] [operator &&] [keyword new].[keyword target].[property name]) {",
+     "    [keyword return] [keyword new]",
+     "      .[keyword target];",
+     "  }",
+     "}");
+
   var jsonld_mode = CodeMirror.getMode(
     {indentUnit: 2},
     {name: "javascript", jsonld: true}
