38131c44806f187153dfc3036b8c0437700b0f3d
Marijn Haverbeke <marijnh@gmail.com>
2015-10-30 11:05:54 +0100

    [tests] Fix measuring of editor bottom in case of zero-width scrollbars
    Issue #3517

--- test/scroll_test.js
+++ test/scroll_test.js
@@ -19,14 +19,14 @@
   function barV(cm) { return byClassName(cm.getWrapperElement(), "CodeMirror-vscrollbar")[0]; }
 
   function displayBottom(cm, scrollbar) {
-    if (scrollbar)
+    if (scrollbar && cm.display.scroller.offsetHeight > cm.display.scroller.clientHeight)
       return barH(cm).getBoundingClientRect().top;
     else
       return cm.getWrapperElement().getBoundingClientRect().bottom - 1;
   }
 
   function displayRight(cm, scrollbar) {
-    if (scrollbar)
+    if (scrollbar && cm.display.scroller.offsetWidth > cm.display.scroller.clientWidth)
       return barV(cm).getBoundingClientRect().left;
     else
       return cm.getWrapperElement().getBoundingClientRect().right - 1;
