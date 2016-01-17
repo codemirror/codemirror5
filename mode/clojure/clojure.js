17a0538900ea95af1992337586229736190006d2
Gabriel Horner <gabriel.horner@gmail.com>
2016-01-17 10:30:07 -0500

    [clojure mode] Fix token type for spaces inside string
    Spaces inside string are reporting null type instead of string.
    Fix from
    https://github.com/LightTable/Clojure/commit/bcac085f3556cb296134b75e5dbd13913af6c1f4

--- mode/clojure/clojure.js
+++ mode/clojure/clojure.js
@@ -142,7 +142,7 @@ CodeMirror.defineMode("clojure", function (options) {
             }
 
             // skip spaces
-            if (stream.eatSpace()) {
+            if (state.mode != "string" && stream.eatSpace()) {
                 return null;
             }
             var returnType = null;
