a86638530226e55a06240ce70f7c7e720ed18860
vidmantas zemleris <vidmantas@vinted.com>
2016-01-19 13:36:33 +0200

    Add Int type to scala-mode

--- mode/clike/clike.js
+++ mode/clike/clike.js
@@ -497,7 +497,7 @@
     ),
     types: words(
       "AnyVal App Application Array BufferedIterator BigDecimal BigInt Char Console Either " +
-      "Enumeration Equiv Error Exception Fractional Function IndexedSeq Integral Iterable " +
+      "Enumeration Equiv Error Exception Fractional Function IndexedSeq Int Integral Iterable " +
       "Iterator List Map Numeric Nil NotNull Option Ordered Ordering PartialFunction PartialOrdering " +
       "Product Proxy Range Responder Seq Serializable Set Specializable Stream StringBuilder " +
       "StringContext Symbol Throwable Traversable TraversableOnce Tuple Unit Vector " +
