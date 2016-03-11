f4bbce34724d1798170ed52dd78eaeaaecb139c9
Pierre Gerold <gerold@crans.org>
2016-03-11 13:48:26 +0100

    fix insert/command mode different behavior

--- keymap/vim.js
+++ keymap/vim.js
@@ -1698,9 +1698,10 @@
         var last = cm.lastLine();
         // Vim cancels linewise motions that start on an edge and move beyond
         // that edge. It does not cancel motions that do not start on an edge.
-        if ((line < first && cur.line == first) ||
-            (line > last && cur.line == last)) {
-          return;
+        if (line < first && cur.line == first){
+          return this.moveToStartOfLine(cm, head, motionArgs, vim);
+        }else if (line > last && cur.line == last){
+            return this.moveToEol(cm, head, motionArgs, vim);
         }
         if (motionArgs.toFirstChar){
           endCh=findFirstNonWhiteSpaceCharacter(cm.getLine(line));
