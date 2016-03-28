codemirror/codemirror5

commit 8ba727a0310b2e87d6a2c310580d368b1e3cc791

Fix a number of lint warnings from new-improved-blint

mode/tiddlywiki/tiddlywiki.js
@@ -71,16 +71,6 @@ CodeMirror.defineMode("tiddlywiki", function () {
     return f(stream, state);
   }
 
-  // Used as scratch variables to communicate multiple values without
-  // consing up tons of objects.
-  var type, content;
-
-  function ret(tp, style, cont) {
-    type = tp;
-    content = cont;
-    return style;
-  }
-
   function jsTokenBase(stream, state) {
     var sol = stream.sol(), ch;
 
@@ -95,16 +85,16 @@ CodeMirror.defineMode("tiddlywiki", function () {
         return chain(stream, state, twTokenCode);
       }
       if (stream.match(reBlockQuote)) {
-        return ret('quote', 'quote');
+        return 'quote';
       }
       if (stream.match(reWikiCommentStart) || stream.match(reWikiCommentStop)) {
-        return ret('code', 'comment');
+        return 'comment';
       }
       if (stream.match(reJsCodeStart) || stream.match(reJsCodeStop) || stream.match(reXmlCodeStart) || stream.match(reXmlCodeStop)) {
-        return ret('code', 'comment');
+        return 'comment';
       }
       if (stream.match(reHR)) {
-        return ret('hr', 'hr');
+        return 'hr';
       }
     } // sol
     ch = stream.next();
@@ -112,30 +102,30 @@ CodeMirror.defineMode("tiddlywiki", function () {
     if (sol && /[\/\*!#;:>|]/.test(ch)) {
       if (ch == "!") { // tw header
         stream.skipToEnd();
-        return ret("header", "header");
+        return "header";
       }
       if (ch == "*") { // tw list
         stream.eatWhile('*');
-        return ret("list", "comment");
+        return "comment";
       }
       if (ch == "#") { // tw numbered list
         stream.eatWhile('#');
-        return ret("list", "comment");
+        return "comment";
       }
       if (ch == ";") { // definition list, term
         stream.eatWhile(';');
-        return ret("list", "comment");
+        return "comment";
       }
       if (ch == ":") { // definition list, description
         stream.eatWhile(':');
-        return ret("list", "comment");
+        return "comment";
       }
       if (ch == ">") { // single line quote
         stream.eatWhile(">");
-        return ret("quote", "quote");
+        return "quote";
       }
       if (ch == '|') {
-        return ret('table', 'header');
+        return 'header';
       }
     }
 
@@ -146,29 +136,29 @@ CodeMirror.defineMode("tiddlywiki", function () {
     // rudimentary html:// file:// link matching. TW knows much more ...
     if (/[hf]/i.test(ch)) {
       if (/[ti]/i.test(stream.peek()) && stream.match(/\b(ttps?|tp|ile):\/\/[\-A-Z0-9+&@#\/%?=~_|$!:,.;]*[A-Z0-9+&@#\/%=~_|$]/i)) {
-        return ret("link", "link");
+        return "link";
       }
     }
     // just a little string indicator, don't want to have the whole string covered
     if (ch == '"') {
-      return ret('string', 'string');
+      return 'string';
     }
     if (ch == '~') {    // _no_ CamelCase indicator should be bold
-      return ret('text', 'brace');
+      return 'brace';
     }
     if (/[\[\]]/.test(ch)) { // check for [[..]]
       if (stream.peek() == ch) {
         stream.next();
-        return ret('brace', 'brace');
+        return 'brace';
       }
     }
     if (ch == "@") {    // check for space link. TODO fix @@...@@ highlighting
       stream.eatWhile(isSpaceName);
-      return ret("link", "link");
+      return "link";
     }
     if (/\d/.test(ch)) {        // numbers
       stream.eatWhile(/\d/);
-      return ret("number", "number");
+      return "number";
     }
     if (ch == "/") { // tw invisible comment
       if (stream.eat("%")) {
@@ -191,7 +181,7 @@ CodeMirror.defineMode("tiddlywiki", function () {
           return chain(stream, state, twTokenStrike);
         // mdash
         if (stream.peek() == ' ')
-          return ret('text', 'brace');
+          return 'brace';
       }
     }
     if (ch == "'") { // tw bold
@@ -205,7 +195,7 @@ CodeMirror.defineMode("tiddlywiki", function () {
       }
     }
     else {
-      return ret(ch);
+      return null;
     }
 
     // core macro handling
@@ -213,8 +203,7 @@ CodeMirror.defineMode("tiddlywiki", function () {
     var word = stream.current(),
     known = textwords.propertyIsEnumerable(word) && textwords[word];
 
-    return known ? ret(known.type, known.style, word) : ret("text", null, word);
-
+    return known ? known.style : null;
   } // jsTokenBase()
 
   // tw invisible comment
@@ -228,7 +217,7 @@ CodeMirror.defineMode("tiddlywiki", function () {
       }
       maybeEnd = (ch == "%");
     }
-    return ret("comment", "comment");
+    return "comment";
   }
 
   // tw strong / bold
@@ -242,29 +231,29 @@ CodeMirror.defineMode("tiddlywiki", function () {
       }
       maybeEnd = (ch == "'");
     }
-    return ret("text", "strong");
+    return "strong";
   }
 
   // tw code
   function twTokenCode(stream, state) {
-    var ch, sb = state.block;
+    var sb = state.block;
 
     if (sb && stream.current()) {
-      return ret("code", "comment");
+      return "comment";
     }
 
     if (!sb && stream.match(reUntilCodeStop)) {
       state.tokenize = jsTokenBase;
-      return ret("code", "comment");
+      return "comment";
     }
 
     if (sb && stream.sol() && stream.match(reCodeBlockStop)) {
       state.tokenize = jsTokenBase;
-      return ret("code", "comment");
+      return "comment";
     }
 
-    ch = stream.next();
-    return (sb) ? ret("code", "comment") : ret("code", "comment");
+    stream.next();
+    return "comment";
   }
 
   // tw em / italic
@@ -278,7 +267,7 @@ CodeMirror.defineMode("tiddlywiki", function () {
       }
       maybeEnd = (ch == "/");
     }
-    return ret("text", "em");
+    return "em";
   }
 
   // tw underlined text
@@ -292,7 +281,7 @@ CodeMirror.defineMode("tiddlywiki", function () {
       }
       maybeEnd = (ch == "_");
     }
-    return ret("text", "underlined");
+    return "underlined";
   }
 
   // tw strike through text looks ugly
@@ -307,7 +296,7 @@ CodeMirror.defineMode("tiddlywiki", function () {
       }
       maybeEnd = (ch == "-");
     }
-    return ret("text", "strikethrough");
+    return "strikethrough";
   }
 
   // macro
@@ -315,19 +304,19 @@ CodeMirror.defineMode("tiddlywiki", function () {
     var ch, word, known;
 
     if (stream.current() == '<<') {
-      return ret('brace', 'macro');
+      return 'macro';
     }
 
     ch = stream.next();
     if (!ch) {
       state.tokenize = jsTokenBase;
-      return ret(ch);
+      return null;
     }
     if (ch == ">") {
       if (stream.peek() == '>') {
         stream.next();
         state.tokenize = jsTokenBase;
-        return ret("brace", "macro");
+        return "macro";
       }
     }
 
@@ -336,10 +325,10 @@ CodeMirror.defineMode("tiddlywiki", function () {
     known = keywords.propertyIsEnumerable(word) && keywords[word];
 
     if (known) {
-      return ret(known.type, known.style, word);
+      return known.style, word;
     }
     else {
-      return ret("macro", null, word);
+      return null, word;
     }
   }
 

addon/display/rulers.js
@@ -38,7 +38,7 @@
     for (var i = 0; i < val.length; i++) {
       var elt = document.createElement("div");
       elt.className = "CodeMirror-ruler";
-      var col, cls = null, conf = val[i];
+      var col, conf = val[i];
       if (typeof conf == "number") {
         col = conf;
       } else {
@@ -47,7 +47,6 @@
         if (conf.color) elt.style.borderColor = conf.color;
         if (conf.lineStyle) elt.style.borderLeftStyle = conf.lineStyle;
         if (conf.width) elt.style.borderLeftWidth = conf.width;
-        cls = val[i].className;
       }
       elt.style.left = (left + col * cw) + "px";
       elt.style.top = "-50px";

addon/search/searchcursor.js
@@ -177,9 +177,9 @@
   });
 
   CodeMirror.defineExtension("selectMatches", function(query, caseFold) {
-    var ranges = [], next;
+    var ranges = [];
     var cur = this.getSearchCursor(query, this.getCursor("from"), caseFold);
-    while (next = cur.findNext()) {
+    while (cur.findNext()) {
       if (CodeMirror.cmpPos(cur.to(), this.getCursor("to")) > 0) break;
       ranges.push({anchor: cur.from(), head: cur.to()});
     }

addon/tern/worker.js
@@ -39,6 +39,6 @@ function startServer(defs, plugins, scripts) {
   });
 }
 
-var console = {
+this.console = {
   log: function(v) { postMessage({type: "debug", message: v}); }
 };

keymap/vim.js
@@ -4581,10 +4581,9 @@
         searchCursor.replace(newText);
       }
       function next() {
-        var found;
         // The below only loops to skip over multiple occurrences on the same
         // line when 'global' is not true.
-        while(found = searchCursor.findNext() &&
+        while(searchCursor.findNext() &&
               isInRange(searchCursor.from(), lineStart, lineEnd)) {
           if (!global && lastPos && searchCursor.from().line == lastPos.line) {
             continue;

lib/codemirror.js
@@ -728,12 +728,9 @@
   }
 
   function postUpdateDisplay(cm, update) {
-    var force = update.force, viewport = update.viewport;
+    var viewport = update.viewport;
     for (var first = true;; first = false) {
-      if (first && cm.options.lineWrapping && update.oldDisplayWidth != displayWidth(cm)) {
-        force = true;
-      } else {
-        force = false;
+      if (!first || !cm.options.lineWrapping || update.oldDisplayWidth == displayWidth(cm)) {
         // Clip forced viewport to actual scrollable area.
         if (viewport && viewport.top != null)
           viewport = {top: Math.min(cm.doc.height + paddingVert(cm.display) - displayHeight(cm), viewport.top)};
@@ -1844,7 +1841,7 @@
       var partPos = getBidiPartAt(order, pos.ch);
       side = partPos % 2 ? "right" : "left";
     }
-    var result = nodeAndOffsetInLineMap(info.map, pos.ch, "left");
+    var result = nodeAndOffsetInLineMap(info.map, pos.ch, side);
     result.offset = result.collapse == "right" ? result.end : result.start;
     return result;
   }

mode/apl/apl.js
@@ -102,7 +102,7 @@ CodeMirror.defineMode("apl", function() {
       };
     },
     token: function(stream, state) {
-      var ch, funcName, word;
+      var ch, funcName;
       if (stream.eatSpace()) {
         return null;
       }
@@ -163,7 +163,6 @@ CodeMirror.defineMode("apl", function() {
         return "function jot-dot";
       }
       stream.eatWhile(/[\w\$_]/);
-      word = stream.current();
       state.prev = true;
       return "keyword";
     }

mode/asterisk/asterisk.js
@@ -65,8 +65,7 @@ CodeMirror.defineMode("asterisk", function() {
 
   function basicToken(stream,state){
     var cur = '';
-    var ch  = '';
-    ch = stream.next();
+    var ch = stream.next();
     // comment
     if(ch == ";") {
       stream.skipToEnd();
@@ -136,7 +135,6 @@ CodeMirror.defineMode("asterisk", function() {
     token: function(stream, state) {
 
       var cur = '';
-      var ch  = '';
       if(stream.eatSpace()) return null;
       // extension started
       if(state.extenStart){
@@ -170,7 +168,7 @@ CodeMirror.defineMode("asterisk", function() {
       } else if(state.extenPriority) {
         state.extenPriority = false;
         state.extenApplication = true;
-        ch = stream.next(); // get comma
+        stream.next(); // get comma
         if(state.extenSame) return null;
         stream.eatWhile(/[^,]/);
         return "number";

commit f99aebf2c7e75a05f97dc20d785d3f9aff9c9c6b

[tiddlywiki mode] Fix bogus return statements
8ba727a0310b2e87d6a2c310580d368b1e3cc791 caused two return
statements to return the wrong variable. In addition, they cause
warnings when compiled with Closure Compiler. Fixed them to
return what they should.

mode/tiddlywiki/tiddlywiki.js
@@ -324,12 +324,7 @@ CodeMirror.defineMode("tiddlywiki", function () {
     word = stream.current();
     known = keywords.propertyIsEnumerable(word) && keywords[word];
 
-    if (known) {
-      return known.style, word;
-    }
-    else {
-      return null, word;
-    }
+    return known ? known.style : null;
   }
 
   // Interface