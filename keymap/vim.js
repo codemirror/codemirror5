87a12b6887d63c0a22880a990b5647e2d583675d
Josh Soref <jsoref@users.noreply.github.com>
2016-03-30 22:30:57 +0000

    spelling: instantiation

--- keymap/vim.js
+++ keymap/vim.js
@@ -26,7 +26,7 @@
  *  2. Variable declarations and short basic helpers
  *  3. Instance (External API) implementation
  *  4. Internal state tracking objects (input state, counter) implementation
- *     and instanstiation
+ *     and instantiation
  *  5. Key handler (the main command dispatcher) implementation
  *  6. Motion, operator, and action implementations
  *  7. Helper functions for the key handler, motions, operators, and actions
