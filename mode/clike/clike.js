59c4ebd9e6e3bec3adeabd2724e2048143872856
Marijn Haverbeke <marijn@haverbeke.nl>
2016-05-20 10:40:02 +0200

    [clike mode] Allow underscores in Java numbers
    Closes #3900

--- mode/clike/clike.js
+++ mode/clike/clike.js
@@ -433,6 +433,7 @@ CodeMirror.defineMode("clike", function(config, parserConfig) {
     typeFirstDefinitions: true,
     atoms: words("true false null"),
     endStatement: /^[;:]$/,
+    number: /^(?:0x[a-f\d_]+|0b[01_]+|(?:[\d_]+\.?\d*|\.\d+)(?:e[-+]?[\d_]+)?)(u|ll?|l|f)?/i,
     hooks: {
       "@": function(stream) {
         stream.eatWhile(/[\w\$_]/);
