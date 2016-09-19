4d0d2ffd0c41f931dee42dfd6e2ecbb4a8ef6485
Adrian Heine <mail@adrianheine.de>
2016-09-19 21:13:23 +0200

    Add lint for src/

--- test/lint.js
+++ test/lint.js
@@ -8,4 +8,12 @@ var blint = require("blint");
   });
 });
 
+["src"].forEach(function(dir) {
+  blint.checkDir(dir, {
+    browser: true,
+    blob: "// CodeMirror, copyright (c) by Marijn Haverbeke and others\n// Distributed under an MIT license: http:\/\/codemirror.net\/LICENSE\n\n",
+    ecmaVersion: 6
+  });
+});
+
 module.exports = {ok: blint.success()};
