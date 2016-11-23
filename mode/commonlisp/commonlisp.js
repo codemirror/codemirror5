8bfabc472acf00eaee0d4a099f3b90d8b5dd47a8
Marijn Haverbeke <marijn@haverbeke.nl>
2016-11-23 09:42:52 +0100

    [commonlisp mode] Recognize character literal syntax
    Closes #4401

--- mode/commonlisp/commonlisp.js
+++ mode/commonlisp/commonlisp.js
@@ -48,6 +48,7 @@ CodeMirror.defineMode("commonlisp", function (config) {
       else if (/\d/.test(ch) && stream.match(/^\d*#/)) return null;
       else if (ch == "|") return (state.tokenize = inComment)(stream, state);
       else if (ch == ":") { readSym(stream); return "meta"; }
+      else if (ch == "\\") { stream.next(); readSym(stream); return "string-2" }
       else return "error";
     } else {
       var name = readSym(stream);
