744deb9a467b4d6aef4426a2655a3676ccd7f657
Oskar Segersvärd <oseg@google.com>
2016-11-25 14:42:07 +0100

    Extend soy mode with template/variable definitions, checking, types, additional keywords and indentation fixes. Tests.

---
+++ mode/soy/test.js
@@ -0,0 +1,75 @@
+// CodeMirror, copyright (c) by Marijn Haverbeke and others
+// Distributed under an MIT license: http://codemirror.net/LICENSE
+
+(function() {
+  var mode = CodeMirror.getMode({indentUnit: 2}, "soy");
+  function MT(name) {test.mode(name, mode, Array.prototype.slice.call(arguments, 1));}
+
+  MT('let-test',
+     '[keyword {template] [def .name][keyword }]',
+     '  [keyword {let] [def $name]: [string "world"][keyword /}]',
+     '  [tag&bracket <][tag h1][tag&bracket >]',
+     '    Hello, [keyword {][variable-2 $name][keyword }]',
+     '  [tag&bracket </][tag h1][tag&bracket >]',
+     '[keyword {/template}]',
+     '');
+
+  MT('param-type-test',
+     '[keyword {@param] [def a]: ' +
+         '[variable-3 list]<[[[variable-3 a]: [variable-3 int], ' +
+         '[variable-3 b]: [variable-3 map]<[variable-3 string], ' +
+         '[variable-3 bool]>]]>][keyword }]');
+
+  MT('undefined-var',
+     '[keyword {][variable-2&error $var]');
+
+  MT('param-scope-test',
+     '[keyword {template] [def .a][keyword }]',
+     '  [keyword {@param] [def x]: [variable-3 string][keyword }]',
+     '  [keyword {][variable-2 $x][keyword }]',
+     '[keyword {/template}]',
+     '',
+     '[keyword {template] [def .b][keyword }]',
+     '  [keyword {][variable-2&error $x][keyword }]',
+     '[keyword {/template}]',
+     '');
+
+  MT('if-variable-test',
+     '[keyword {if] [variable-2&error $showThing][keyword }]',
+     '  Yo!',
+     '[keyword {/if}]',
+     '');
+
+  MT('defined-if-variable-test',
+     '[keyword {template] [def .foo][keyword }]',
+     '  [keyword {@param?] [def showThing]: [variable-3 bool][keyword }]',
+     '  [keyword {if] [variable-2 $showThing][keyword }]',
+     '    Yo!',
+     '  [keyword {/if}]',
+     '[keyword {/template}]',
+     '');
+
+  MT('template-calls-test',
+     '[keyword {template] [def .foo][keyword }]',
+     '  Yo!',
+     '[keyword {/template}]',
+     '[keyword {call] [variable-2 .foo][keyword /}]',
+     '[keyword {call] [variable foo][keyword /}]',
+     '[keyword {call] [variable .bar][keyword /}]',
+     '[keyword {call] [variable bar][keyword /}]',
+     '');
+
+  MT('foreach-scope-test',
+     '[keyword {foreach] [def $foo] [keyword in] [variable-2&error $foos][keyword }]',
+     '  [keyword {][variable-2 $foo][keyword }]',
+     '[keyword {/foreach}]',
+     '[keyword {][variable-2&error $foo][keyword }]');
+
+  MT('foreach-ifempty-indent-test',
+     '[keyword {foreach] [def $foo] [keyword in] [variable-2&error $foos][keyword }]',
+     '  something',
+     '[keyword {ifempty}]',
+     '  nothing',
+     '[keyword {/foreach}]',
+     '');
+})();
