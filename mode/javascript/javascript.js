ae45e2bdad8bb4a00c7f1cd7f6127a6963d9df8f
Marijn Haverbeke <marijn@haverbeke.nl>
2016-12-20 14:04:43 +0100

    [javascript mode] Also allow a regexp after 'default'
    (For 'export default' syntax)
    
    Issue #4452

--- mode/javascript/javascript.js
+++ mode/javascript/javascript.js
@@ -12,7 +12,7 @@
 "use strict";
 
 function expressionAllowed(stream, state, backUp) {
-  return /^(?:operator|sof|keyword c|case|new|export|[\[{}\(,;:]|=>)$/.test(state.lastType) ||
+  return /^(?:operator|sof|keyword c|case|new|export|default|[\[{}\(,;:]|=>)$/.test(state.lastType) ||
     (state.lastType == "quasi" && /\{\s*$/.test(stream.string.slice(0, stream.pos - (backUp || 0))))
 }
 
