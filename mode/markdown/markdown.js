6ea8a906d94994f34faacdde282988a8bb589f94
Marijn Haverbeke <marijn@haverbeke.nl>
2017-02-02 11:53:27 +0100

    [markdown mode] Fix styling of closing code block fence
    Issue #4528

--- mode/markdown/markdown.js
+++ mode/markdown/markdown.js
@@ -239,11 +239,13 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
   }
 
   function local(stream, state) {
-    if (state.fencedChars && stream.skipTo(state.fencedChars)) {
-      stream.match(state.fencedChars)
+    if (state.fencedChars && stream.match(state.fencedChars)) {
+      if (modeCfg.highlightFormatting) state.formatting = "code-block";
       state.localMode = state.localState = null;
       state.f = state.block = leavingLocal;
-      return "comment";
+      return getType(state)
+    } else if (state.fencedChars && stream.skipTo(state.fencedChars)) {
+      return "commment"
     } else if (state.localMode) {
       return state.localMode.token(stream, state.localState);
     } else {
