db979409eaa5c3b15306f8bd0101fc18d3acbeaf
Erik Welander <alurcard2@gmail.com>
2017-02-10 08:42:19 -0800

    Don't render line annotations past EOF

--- addon/scroll/annotatescrollbar.js
+++ addon/scroll/annotatescrollbar.js
@@ -1,5 +1,25 @@
-// CodeMirror, copyright (c) by Marijn Haverbeke and others
-// Distributed under an MIT license: http://codemirror.net/LICENSE
+/**
+ * @license
+ * Copyright (C) 2014 by Marijn Haverbeke <marijnh@gmail.com> and others
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
@@ -74,9 +94,8 @@
     var curLine = null, curLineObj = null;
     var lastLine = cm.lineCount() - 1;
     function getY(pos, top) {
-      var line = Math.min(lastLine, pos.line);
-      if (curLine != line) {
-        curLine = line;
+      if (curLine != pos.line) {
+        curLine = pos.line;
         curLineObj = cm.getLineHandle(curLine);
       }
       if ((curLineObj.widgets && curLineObj.widgets.length) ||
@@ -88,6 +107,9 @@
 
     if (cm.display.barWidth) for (var i = 0, nextTop; i < anns.length; i++) {
       var ann = anns[i];
+      if (ann.to > lastLine) {
+        continue;
+      }
       var top = nextTop || getY(ann.from, true) * hScale;
       var bottom = getY(ann.to, false) * hScale;
       while (i < anns.length - 1) {
