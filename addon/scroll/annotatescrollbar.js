64ba43f503fc804ff78fc3870ac006551c2b4b34
Erik Welander <alurcard2@gmail.com>
2017-02-10 08:43:23 -0800

    Update annotatescrollbar.js
    Really fix the license text

--- addon/scroll/annotatescrollbar.js
+++ addon/scroll/annotatescrollbar.js
@@ -1,4 +1,4 @@
-// CodeMirror, copyright (c) by Marijn Haverbeke and others		 +/**
+// CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
 (function(mod) {
