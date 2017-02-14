459873b5c1807527314706c03580ec06515e8d1e
Marijn Haverbeke <marijn@haverbeke.nl>
2017-02-14 10:47:42 +0100

    [merge addon] Fix bug in determining the extent of chunks near the end of the doc
    Issue #4572

--- addon/merge/merge.js
+++ addon/merge/merge.js
@@ -627,10 +627,10 @@
   function endOfLineClean(diff, i) {
     if (i == diff.length - 1) return true;
     var next = diff[i + 1][1];
-    if (next.length == 1 || next.charCodeAt(0) != 10) return false;
+    if ((next.length == 1 && i < diff.length - 2) || next.charCodeAt(0) != 10) return false;
     if (i == diff.length - 2) return true;
     next = diff[i + 2][1];
-    return next.length > 1 && next.charCodeAt(0) == 10;
+    return (next.length > 1 || i == diff.length - 3) && next.charCodeAt(0) == 10;
   }
 
   function startOfLineClean(diff, i) {
