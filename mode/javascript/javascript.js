53612ac34c5c5e4bdb4c7e46b498c01fdee20767
Marijn Haverbeke <marijn@haverbeke.nl>
2017-02-16 08:57:39 +0100

    [javascript mode] Simplify 88b37cf580
    Issue #4497

--- mode/javascript/javascript.js
+++ mode/javascript/javascript.js
@@ -634,11 +634,8 @@ CodeMirror.defineMode("javascript", function(config, parserConfig) {
     if (type == "variable") {register(value); return cont(classNameAfter);}
   }
   function classNameAfter(type, value) {
-    if (value == "extends" || value == "implements") {
-      return isTS ?
-        cont(commasep(typeexpr, "{"), pushlex("}"), classBody, poplex) :
-        cont(expression, classNameAfter);
-    }
+    if (value == "extends" || value == "implements" || (isTS && type == ","))
+      return cont(isTS ? typeexpr : expression, classNameAfter);
     if (type == "{") return cont(pushlex("}"), classBody, poplex);
   }
   function classBody(type, value) {
