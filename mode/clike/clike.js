d1d6ee2ddeaafc5d5f540b22c8d4fa58ce4bf92c
satamas <se.atamas@gmail.com>
2017-02-27 18:51:26 +0300

    [clike mode] Add suspend to Kotlin keywords

--- mode/clike/clike.js
+++ mode/clike/clike.js
@@ -575,7 +575,7 @@ CodeMirror.defineMode("clike", function(config, parserConfig) {
       "file import where by get set abstract enum open inner override private public internal " +
       "protected catch finally out final vararg reified dynamic companion constructor init " +
       "sealed field property receiver param sparam lateinit data inline noinline tailrec " +
-      "external annotation crossinline const operator infix"
+      "external annotation crossinline const operator infix suspend"
     ),
     types: words(
       /* package java.lang */
