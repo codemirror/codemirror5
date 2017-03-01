1d8ba8a3679fc765d7f6aaa45799b7211b5aac4c
Marijn Haverbeke <marijn@haverbeke.nl>
2017-03-01 10:43:38 +0100

    Remove es6-isms

--- test/sql-hint-test.js
+++ test/sql-hint-test.js
@@ -182,14 +182,14 @@
     if (a === b) return true
     if (!(a && typeof a == "object") ||
         !(b && typeof b == "object")) return false
-    let array = Array.isArray(a)
+    var array = Array.isArray(a)
     if (Array.isArray(b) != array) return false
     if (array) {
       if (a.length != b.length) return false
-      for (let i = 0; i < a.length; i++) if (!deepCompare(a[i], b[i])) return false
+      for (var i = 0; i < a.length; i++) if (!deepCompare(a[i], b[i])) return false
     } else {
-      for (let p in a) if (!(p in b) || !deepCompare(a[p], b[p])) return false
-      for (let p in b) if (!(p in a)) return false
+      for (var p in a) if (!(p in b) || !deepCompare(a[p], b[p])) return false
+      for (var p in b) if (!(p in a)) return false
     }
     return true
   }
