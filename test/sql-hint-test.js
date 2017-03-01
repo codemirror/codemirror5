b359a0210ede0755dc010f465faf2d15d113f89e
Marijn Haverbeke <marijn@haverbeke.nl>
2017-03-01 10:40:14 +0100

    [sql-hint tests] Fix result comparision function
    Issue #4618

--- test/sql-hint-test.js
+++ test/sql-hint-test.js
@@ -144,7 +144,7 @@
     value: "SELECT myt",
     cursor: Pos(0, 10),
     tables: displayTextTables,
-    list: displayTextTables,
+    list: [{text: "mytable", displayText: "mytable | The main table",}],
     from: Pos(0, 7),
     to: Pos(0, 10)
   });
@@ -179,11 +179,18 @@
   })
 
   function deepCompare(a, b) {
-    if (!a || typeof a != "object")
-      return a === b;
-    if (!b || typeof b != "object")
-      return false;
-    for (var prop in a) if (!deepCompare(a[prop], b[prop])) return false;
-    return true;
+    if (a === b) return true
+    if (!(a && typeof a == "object") ||
+        !(b && typeof b == "object")) return false
+    let array = Array.isArray(a)
+    if (Array.isArray(b) != array) return false
+    if (array) {
+      if (a.length != b.length) return false
+      for (let i = 0; i < a.length; i++) if (!deepCompare(a[i], b[i])) return false
+    } else {
+      for (let p in a) if (!(p in b) || !deepCompare(a[p], b[p])) return false
+      for (let p in b) if (!(p in a)) return false
+    }
+    return true
   }
 })();
