bfbdd885989a868fae94a35786f5f5ff53961b4a
Marijn Haverbeke <marijn@haverbeke.nl>
2017-03-07 14:31:59 +0100

    [merge addon] Don't try to align lines in deleted chunks
    Issue #4627

--- addon/merge/merge.js
+++ addon/merge/merge.js
@@ -372,14 +372,22 @@
   // lines that need to be aligned with each other.
   function mergeAlignable(result, origAlignable, chunks, setIndex) {
     var rI = 0, origI = 0, chunkI = 0, diff = 0
-    for (;; rI++) {
+    outer: for (;; rI++) {
       var nextR = result[rI], nextO = origAlignable[origI]
       if (!nextR && nextO == null) break
 
       var rLine = nextR ? nextR[0] : 1e9, oLine = nextO == null ? 1e9 : nextO
       while (chunkI < chunks.length) {
         var chunk = chunks[chunkI]
-        if (chunk.editTo > rLine) break
+        if (chunk.origFrom <= oLine && chunk.origTo > oLine) {
+          origI++
+          rI--
+          continue outer;
+        }
+        if (chunk.editTo > rLine) {
+          if (chunk.editFrom <= rLine) continue outer;
+          break
+        }
         diff += (chunk.origTo - chunk.origFrom) - (chunk.editTo - chunk.editFrom)
         chunkI++
       }
