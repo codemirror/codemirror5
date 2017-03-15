9301db90173f04701d3da8be83b15b45df3b748b
Axel Lewenhaupt <axel@x2d.org>
2017-03-15 21:47:07 +0100

    The indentation was previously added to the first token of the inner mode.

--- mode/soy/soy.js
+++ mode/soy/soy.js
@@ -31,17 +31,18 @@
     }
 
     function tokenUntil(stream, state, untilRegExp) {
+      if (stream.sol()) {
+        for (var indent = 0; indent < state.indent; indent++) {
+          if (!stream.eat(/\s/)) break;
+        }
+        if (indent) return null;
+      }
       var oldString = stream.string;
       var match = untilRegExp.exec(oldString.substr(stream.pos));
       if (match) {
         // We don't use backUp because it backs up just the position, not the state.
         // This uses an undocumented API.
         stream.string = oldString.substr(0, stream.pos + match.index);
-      }
-      if (stream.sol()) {
-        for (var i = 0; i < state.indent; i++) {
-          if (!stream.eat(/\s/)) break;
-        }
       }
       var result = stream.hideFirstChars(state.indent, function() {
         var localState = last(state.localStates);
