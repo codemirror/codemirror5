415bdff0fce114f73896ea9624da4232ee8d0907
Pablo Zubieta <pabloferz@yahoo.com.mx>
2017-04-11 00:49:41 -0500

    Fix #4668, properly handle string literals

--- mode/julia/julia.js
+++ mode/julia/julia.js
@@ -45,7 +45,7 @@ CodeMirror.defineMode("julia", function(config, parserConf) {
 
   var macro = /^@[_A-Za-z][\w]*/;
   var symbol = /^:[_A-Za-z\u00A1-\uFFFF][\w\u00A1-\uFFFF]*!*/;
-  var stringPrefixes = /^(`|"{3}|([_A-Za-z\u00A1-\uFFFF]*"))/;
+  var stringPrefixes = /^(`|([_A-Za-z\u00A1-\uFFFF]*"("")?))/;
 
   function inArray(state) {
     return inGenerator(state, '[')
@@ -338,7 +338,11 @@ CodeMirror.defineMode("julia", function(config, parserConf) {
   }
 
   function tokenStringFactory(delimiter) {
-    delimiter = (delimiter === '`' || delimiter === '"""') ? delimiter : '"';
+    if (delimiter.substr(-3) === '"""') {
+      delimiter = '"""';
+    } else if (delimiter.substr(-1) === '"') {
+      delimiter = '"';
+    }
     function tokenString(stream, state) {
       if (stream.eat('\\')) {
         stream.next();
