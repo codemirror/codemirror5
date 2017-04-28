21ea9a2a0e609278096e5920457b91952312398b
Marijn Haverbeke <marijn@haverbeke.nl>
2017-04-28 09:52:51 +0200

    [vim bindings] Fix double-deref in defineRegister

--- keymap/vim.js
+++ keymap/vim.js
@@ -958,7 +958,7 @@
      * for a reference implementation.
      */
     function defineRegister(name, register) {
-      var registers = vimGlobalState.registerController.registers[name];
+      var registers = vimGlobalState.registerController.registers;
       if (!name || name.length != 1) {
         throw Error('Register name must be 1 character');
       }
