ec2b2f5b96b411d1c09133f2734db82e7b1651c2
Max Schaefer <xiemaisi@gmail.com>
2017-05-22 09:20:37 +0800

    Fix misleading indentation in `tiki.js`.

--- mode/tiki/tiki.js
+++ mode/tiki/tiki.js
@@ -298,13 +298,13 @@ return {
     if (context && context.noIndent) return 0;
     if (context && /^{\//.test(textAfter))
         context = context.prev;
-        while (context && !context.startOfLine)
-          context = context.prev;
-        if (context) return context.indent + indentUnit;
-        else return 0;
-       },
-    electricChars: "/"
-  };
+    while (context && !context.startOfLine)
+        context = context.prev;
+    if (context) return context.indent + indentUnit;
+    else return 0;
+  },
+  electricChars: "/"
+};
 });
 
 CodeMirror.defineMIME("text/tiki", "tiki");
