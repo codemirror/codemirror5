d2dbe1f26ee622c98b5dba7f6c2ca634b30c1181
KyleMcNutt <kylem@unity3d.com>
2017-05-24 23:16:36 +0200

    Adding hg and svn as highlited commands to shell

--- mode/shell/shell.js
+++ mode/shell/shell.js
@@ -30,9 +30,9 @@ CodeMirror.defineMode('shell', function() {
 
   // Commands
   define('builtin', 'ab awk bash beep cat cc cd chown chmod chroot clear cp ' +
-    'curl cut diff echo find gawk gcc get git grep kill killall ln ls make ' +
+    'curl cut diff echo find gawk gcc get git grep hg kill killall ln ls make ' +
     'mkdir openssl mv nc node npm ping ps restart rm rmdir sed service sh ' +
-    'shopt shred source sort sleep ssh start stop su sudo tee telnet top ' +
+    'shopt shred source sort sleep ssh start stop su sudo svn tee telnet top ' +
     'touch vi vim wall wc wget who write yes zsh');
 
   function tokenBase(stream, state) {
