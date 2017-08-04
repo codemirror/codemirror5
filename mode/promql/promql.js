b9a89947c50cee07576f47bb1aa89ecdc924f304
Dawid Polak <dawid@mentax.pl>
2017-08-05 00:04:16 +0200

    PromQL key words

--- mode/promql/promql.js
+++ mode/promql/promql.js
@@ -15,27 +15,32 @@ CodeMirror.defineMode("promql", function(config) {
   var indentUnit = config.indentUnit;
 
   var keywords = {
-    "break":true, "case":true, "chan":true, "const":true, "continue":true,
-    "default":true, "defer":true, "else":true, "fallthrough":true, "for":true,
-    "func":true, "go":true, "goto":true, "if":true, "import":true,
-    "interface":true, "map":true, "package":true, "range":true, "return":true,
-    "select":true, "struct":true, "switch":true, "type":true, "var":true,
-    "bool":true, "byte":true, "complex64":true, "complex128":true,
-    "float32":true, "float64":true, "int8":true, "int16":true, "int32":true,
-    "int64":true, "string":true, "uint8":true, "uint16":true, "uint32":true,
-    "uint64":true, "int":true, "uint":true, "uintptr":true, "error": true,
-    "rune":true
+    "sum":true, "min":true, "max":true, "avg":true, "stddev":true,
+    "stdvar":true, "count":true, "count_values":true, "bottomk":true, "topk":true,
+    "quantile":true,  
+ 
+    "abs":true, "absent":true, "ceil":true, "changes":true, 
+    "clamp_max":true, "clamp_min":true, "count_scalar":true, 
+    "day_of_month":true, "day_of_week":true, "days_in_month":true, 
+    "delta":true, "deriv":true, "drop_common_labels":true, 
+    "exp":true, "floor":true, "histogram_quantile":true, 
+    "holt_winters":true, "hour":true, "idelta":true, 
+    "increase":true, "irate":true, "label_replace":true,
+    "ln":true, "log2":true, "log10":true, "minute":true, 
+    "month":true, "predict_linear":true, "rate":true, "resets":true, 
+    "round":true, "scalar":true, "sort":true, "sort_desc":true, 
+    "sqrt":true, "time":true, "vector":true, "year":true
+     //<aggregation>_over_time => true
   };
 
   var atoms = {
-    "true":true, "false":true, "iota":true, "nil":true, "append":true,
-    "cap":true, "close":true, "complex":true, "copy":true, "delete":true, "imag":true,
-    "len":true, "make":true, "new":true, "panic":true, "print":true,
-    "println":true, "real":true, "recover":true
+    "true":true, "false":true,
+    "and":true, "or":true, "unless":true,
+    "ignoring":true, "on":true, "offset":true,
+    "by": true
   };
 
   var isOperatorChar = /[+\-*&^%:=<>!|\/]/;
-
   var curPunc;
 
   function tokenBase(stream, state) {
@@ -50,10 +55,11 @@ CodeMirror.defineMode("promql", function(config) {
       } else if (ch == "0") {
         stream.match(/^[xX][0-9a-fA-F]+/) || stream.match(/^0[0-7]+/);
       } else {
-        stream.match(/^[0-9]*\.?[0-9]*([eE][\-+]?[0-9]+)?/);
+        stream.match(/^[0-9]*\.?[0-9]*([eE][\-+]?[0-9]+)?([s|m|h|d|w|y])?/);
       }
       return "number";
-    }
+    } 
+
     if (/[\[\]{}\(\),;\:\.]/.test(ch)) {
       curPunc = ch;
       return null;
@@ -74,8 +80,7 @@ CodeMirror.defineMode("promql", function(config) {
     }
     stream.eatWhile(/[\w\$_\xa1-\uffff]/);
     var cur = stream.current();
-    if (keywords.propertyIsEnumerable(cur)) {
-      if (cur == "case" || cur == "default") curPunc = "case";
+    if (keywords.propertyIsEnumerable(cur)) {      
       return "keyword";
     }
     if (atoms.propertyIsEnumerable(cur)) return "atom";
@@ -143,7 +148,6 @@ CodeMirror.defineMode("promql", function(config) {
         if (ctx.align == null) ctx.align = false;
         state.indented = stream.indentation();
         state.startOfLine = true;
-        if (ctx.type == "case") ctx.type = "}";
       }
       if (stream.eatSpace()) return null;
       curPunc = null;
@@ -154,7 +158,6 @@ CodeMirror.defineMode("promql", function(config) {
       if (curPunc == "{") pushContext(state, stream.column(), "}");
       else if (curPunc == "[") pushContext(state, stream.column(), "]");
       else if (curPunc == "(") pushContext(state, stream.column(), ")");
-      else if (curPunc == "case") ctx.type = "case";
       else if (curPunc == "}" && ctx.type == "}") popContext(state);
       else if (curPunc == ctx.type) popContext(state);
       state.startOfLine = false;
@@ -164,17 +167,13 @@ CodeMirror.defineMode("promql", function(config) {
     indent: function(state, textAfter) {
       if (state.tokenize != tokenBase && state.tokenize != null) return CodeMirror.Pass;
       var ctx = state.context, firstChar = textAfter && textAfter.charAt(0);
-      if (ctx.type == "case" && /^(?:case|default)\b/.test(textAfter)) {
-        state.context.type = "}";
-        return ctx.indented;
-      }
       var closing = firstChar == ctx.type;
       if (ctx.align) return ctx.column + (closing ? 0 : 1);
       else return ctx.indented + (closing ? 0 : indentUnit);
     },
+ 
 
-    electricChars: "{}):",
-    closeBrackets: "()[]{}''\"\"``",
+    closeBrackets: {pairs: "()[]{}\"\"''"},
     fold: "brace",
     blockCommentStart: "/*",
     blockCommentEnd: "*/",
