c752cb1ab0b17f22565981809145f2233f63501b
callodacity <callodacity@gmail.com>
2017-09-09 08:37:12 +1000

    fixed bug in mark-selection

--- addon/selection/mark-selection.js
+++ addon/selection/mark-selection.js
@@ -86,7 +86,7 @@
     if (!array.length) return coverRange(cm, from, to);
 
     var coverStart = array[0].find(), coverEnd = array[array.length - 1].find();
-    if (!coverStart || !coverEnd || to.line - from.line < CHUNK_SIZE ||
+    if (!coverStart || !coverEnd || to.line - from.line <= CHUNK_SIZE ||
         cmp(from, coverEnd.to) >= 0 || cmp(to, coverStart.from) <= 0)
       return reset(cm);
 
