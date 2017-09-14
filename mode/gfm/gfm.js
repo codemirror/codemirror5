1d3605b9fcf0c8d96a0ccc3811fe529856a9888c
Parth Laxmikant Kolekar <ParthKolekar@users.noreply.github.com>
2017-09-13 22:20:08 -0500

    Update gfm.js
    Remove SHA hash from being treated specially.
    
    Now this will require 
    
    // User/Project@SHA
    // User@SHA
    
    The existing regex matches arbitrary code that is a series of `[a-f0-9]{7,40}` not matter how they appear in a sentence.

--- mode/gfm/gfm.js
+++ mode/gfm/gfm.js
@@ -81,10 +81,9 @@ CodeMirror.defineMode("gfm", function(config, modeConfig) {
       if (stream.sol() || state.ateSpace) {
         state.ateSpace = false;
         if (modeConfig.gitHubSpice !== false) {
-          if(stream.match(/^(?:[a-zA-Z0-9\-_]+\/)?(?:[a-zA-Z0-9\-_]+@)?(?:[a-f0-9]{7,40}\b)/)) {
+          if(stream.match(/^(?:[a-zA-Z0-9\-_]+\/)?(?:[a-zA-Z0-9\-_]+@)(?:[a-f0-9]{7,40}\b)/)) {
             // User/Project@SHA
             // User@SHA
-            // SHA
             state.combineTokens = true;
             return "link";
           } else if (stream.match(/^(?:[a-zA-Z0-9\-_]+\/)?(?:[a-zA-Z0-9\-_]+)?#[0-9]+\b/)) {
