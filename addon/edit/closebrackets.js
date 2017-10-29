edbc598959b1284f49f55f9f29b9fe6beeb8e920
Marijn Haverbeke <marijn@haverbeke.nl>
2017-10-29 11:10:18 +0100

    [closebrackets addon] Improve start-of-string heuristic
    To also make sure the last token isn't a string.
    
    Issue codemirror/google-modes#74

--- addon/edit/closebrackets.js
+++ addon/edit/closebrackets.js
@@ -203,6 +203,7 @@
 
   function stringStartsAfter(cm, pos) {
     var token = cm.getTokenAt(Pos(pos.line, pos.ch + 1))
-    return /\bstring/.test(token.type) && token.start == pos.ch
+    return /\bstring/.test(token.type) && token.start == pos.ch &&
+      (pos.ch == 0 || !/\bstring/.test(cm.getTokenTypeAt(pos)))
   }
 });
