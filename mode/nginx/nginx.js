9c9646f61511526b22b45eef75604852f97d885c
Hélio <insign@gmail.com>
2017-12-28 19:15:05 -0300

    fix my fix

--- mode/nginx/nginx.js
+++ mode/nginx/nginx.js
@@ -173,6 +173,7 @@ CodeMirror.defineMode("nginx", function(config) {
   };
 });
 
-CodeMirror.defineMIME("text/nginx", "text/x-nginx-conf", "nginx");
+CodeMirror.defineMIME("text/nginx", "nginx");
+CodeMirror.defineMIME("text/x-nginx-conf", "nginx");
 
 });
