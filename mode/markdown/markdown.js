16e7be864c280ad6f6a3db67c09df6283ae229d2
Shane Liesegang <shane@techie.net>
2018-01-04 09:35:42 -0500

    Fixing accidental deletion.

--- mode/markdown/markdown.js
+++ mode/markdown/markdown.js
@@ -862,6 +862,8 @@ CodeMirror.defineMode("markdown", function(cmCfg, modeCfg) {
   return mode;
 }, "xml");
 
+CodeMirror.defineMIME("text/markdown", "markdown");
+
 CodeMirror.defineMIME("text/x-markdown", "markdown");
 
 });
