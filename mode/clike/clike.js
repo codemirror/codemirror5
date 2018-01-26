ef2341373c7a6b88779d0e01188a295154897577
Marijn Haverbeke <marijn@haverbeke.nl>
2018-01-26 13:34:00 +0100

    [clike mode] Disallow trailing dots in numbers in Kotlin

--- mode/clike/clike.js
+++ mode/clike/clike.js
@@ -617,7 +617,7 @@ CodeMirror.defineMode("clike", function(config, parserConfig) {
     intendSwitch: false,
     indentStatements: false,
     multiLineStrings: true,
-    number: /^(?:0x[a-f\d_]+|0b[01_]+|(?:[\d_]+\.?\d*|\.\d+)(?:e[-+]?[\d_]+)?)(u|ll?|l|f)?/i,
+    number: /^(?:0x[a-f\d_]+|0b[01_]+|(?:[\d_]+(\.\d+)?|\.\d+)(?:e[-+]?[\d_]+)?)(u|ll?|l|f)?/i,
     blockKeywords: words("catch class do else finally for if where try while enum"),
     defKeywords: words("class val var object interface fun"),
     atoms: words("true false null this"),
