5723debf1f68deabc2caacdef6d28587e1f52426
Alexis DOUALLE <adoualle@datawords.com>
2018-06-11 14:12:32 +0200

    clearing mark now resets line order

--- src/model/mark_text.js
+++ src/model/mark_text.js
@@ -58,6 +58,7 @@ export class TextMarker {
         if (span.to != null) max = lineNo(line)
         if (span.from != null) min = lineNo(line)
       }
+      if (cm) { cm.state.newIsolate = true; }
       line.markedSpans = removeMarkedSpan(line.markedSpans, span)
       if (span.from == null && this.collapsed && !lineIsHidden(this.doc, line) && cm)
         updateLineHeight(line, textHeight(cm.display))
