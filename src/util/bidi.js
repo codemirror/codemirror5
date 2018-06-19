a30947d1e011a344e89b85d0a473f9a00b4057ef
Alexis DOUALLE <adoualle@datawords.com>
2018-06-19 16:19:47 +0200

    Reverted bidi neutral algorithm

--- src/util/bidi.js
+++ src/util/bidi.js
@@ -183,8 +183,7 @@ let bidiOrdering = (function() {
           for (end$1 = i$6 + 1; end$1 < isolateLen && isNeutral.test(types[end$1]); ++end$1) {}
           var before = (i$6 - textAndIsolates[i$6b].from ? types[i$6-1] : outerType) == "L"
           var after = (end$1 < isolateLen ? types[end$1] : outerType) == "L"
-          if (isolateDir) { replace$1 = isolateDir == "rtl" ? "R" : "L"}
-          else { replace$1 = before == after ? (before ? "L" : "R") : outerType }
+          replace$1 = before == after ? (before ? "L" : "R") : outerType 
           for (var j$1 = i$6; j$1 < end$1; ++j$1) { types[j$1] = replace$1 }
           i$6 = end$1 - 1
         }
