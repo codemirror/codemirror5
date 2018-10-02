80eeba03634d70e81e1baec5d0942769490e97ea
Brett Zamir <brettz9@yahoo.com>
2018-10-02 11:05:25 +0800

    - Refactoring: Update to latest Rollup API - npm: Update node-static, Rollup deps; add `package-lock.json` to gitignore and use `author` field in `package.json`

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
 import buble from 'rollup-plugin-buble';
 
 export default {
-  banner: `// CodeMirror, copyright (c) by Marijn Haverbeke and others
+  input: "src/codemirror.js",
+  output: {
+    banner: `// CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
 
 // This is CodeMirror (https://codemirror.net), a code editor
@@ -9,10 +11,10 @@ export default {
 //
 // You can find some technical background for some of the code below
 // at http://marijnhaverbeke.nl/blog/#cm-internals .
-`,
-  entry: "src/codemirror.js",
-  format: "umd",
-  dest: "lib/codemirror.js",
-  moduleName: "CodeMirror",
+  `,
+    format: "umd",
+    file: "lib/codemirror.js",
+    name: "CodeMirror"
+  },
   plugins: [ buble({namedFunctionExpressions: false}) ]
 };
