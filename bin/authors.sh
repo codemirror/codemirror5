3c833a033bb357ed87739033a4f1991824d3e40f
Dave MacLachlan <dmaclach@gmail.com>
2018-10-18 09:57:55 -0700

    Change Google Inc. to Google LLC (and added myself)

--- bin/authors.sh
+++ bin/authors.sh
@@ -2,5 +2,5 @@
 tail --lines=+3 AUTHORS > AUTHORS.tmp
 git log --format='%aN' | grep -v "Piët Delport" >> AUTHORS.tmp
 echo -e "List of CodeMirror contributors. Updated before every release.\n" > AUTHORS
-sort -u AUTHORS.tmp >> AUTHORS
+sort -u AUTHORS.tmp | sed 's/Google Inc\./Google LLC/' >> AUTHORS
 rm -f AUTHORS.tmp
