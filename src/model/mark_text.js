0d3b841a7d82fda5d6c67b8e85cf79287ec73bae
Prendota <Prendota@mail.ru>
2018-11-19 13:35:40 +0300

    [mark-selection] add `aria-label` attribute

--- src/model/mark_text.js
+++ src/model/mark_text.js
@@ -211,7 +211,7 @@ export function markText(doc, from, to, options, type) {
     if (updateMaxLine) cm.curOp.updateMaxLine = true
     if (marker.collapsed)
       regChange(cm, from.line, to.line + 1)
-    else if (marker.className || marker.title || marker.startStyle || marker.endStyle || marker.css)
+    else if (marker.className || marker.startStyle || marker.endStyle || marker.css || marker.attributes )
       for (let i = from.line; i <= to.line; i++) regLineChange(cm, i, "text")
     if (marker.atomic) reCheckSelection(cm.doc)
     signalLater(cm, "markerAdded", cm, marker)
