746260274bbe651b04f39d629db1ee2119d6029f
reactma <github@reactma.com>
2019-02-20 14:40:59 +0800

    Added json-frontmatter and toml-frontmatter modes

---
+++ mode/toml-frontmatter/toml-frontmatter.js
@@ -0,0 +1,68 @@
+// CodeMirror, copyright (c) by Marijn Haverbeke and others
+// Distributed under an MIT license: https://codemirror.net/LICENSE
+
+(function (mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"), require("../toml/toml"))
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror", "../toml/toml"], mod)
+  else // Plain browser env
+    mod(CodeMirror)
+})(function (CodeMirror) {
+
+  var START = 0, FRONTMATTER = 1, BODY = 2
+
+  // a mixed mode for Markdown text with an optional TOML front matter
+  CodeMirror.defineMode("toml-frontmatter", function (config, parserConfig) {
+    var tomlMode = CodeMirror.getMode(config, "toml")
+    var innerMode = CodeMirror.getMode(config, parserConfig && parserConfig.base || "gfm")
+
+    function curMode(state) {
+      return state.state == BODY ? innerMode : tomlMode
+    }
+
+    return {
+      startState: function () {
+        return {
+          state: START,
+          inner: CodeMirror.startState(tomlMode)
+        }
+      },
+      copyState: function (state) {
+        return {
+          state: state.state,
+          inner: CodeMirror.copyState(curMode(state), state.inner)
+        }
+      },
+      token: function (stream, state) {
+        if (state.state == START) {
+          if (stream.match(/---/, false)) {
+            state.state = FRONTMATTER
+            return tomlMode.token(stream, state.inner)
+          } else {
+            state.state = BODY
+            state.inner = CodeMirror.startState(innerMode)
+            return innerMode.token(stream, state.inner)
+          }
+        } else if (state.state == FRONTMATTER) {
+          var end = stream.sol() && stream.match(/---/, false)
+          var style = tomlMode.token(stream, state.inner)
+          if (end) {
+            state.state = BODY
+            state.inner = CodeMirror.startState(innerMode)
+          }
+          return style
+        } else {
+          return innerMode.token(stream, state.inner)
+        }
+      },
+      innerMode: function (state) {
+        return {mode: curMode(state), state: state.inner}
+      },
+      blankLine: function (state) {
+        var mode = curMode(state)
+        if (mode.blankLine) return mode.blankLine(state.inner)
+      }
+    }
+  })
+});
