313b53aaa73238d453ff9c7cbb876652f9f43fee
William Desportes <williamdes@wdes.fr>
2019-03-19 21:57:43 +0100

    [sql-hint addon] Fix defaultTable matches
    Introduced by: 6041c2280e29e0b62302ca91d657767b23c5a983
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- addon/hint/sql-hint.js
+++ addon/hint/sql-hint.js
@@ -275,24 +275,29 @@
     if (search.charAt(0) == "." || search.charAt(0) == identifierQuote) {
       start = nameCompletion(cur, token, result, editor);
     } else {
-      addMatches(result, search, defaultTable, function(w) {return {text:w, className: "CodeMirror-hint-table CodeMirror-hint-default-table"};});
-      addMatches(
-          result,
-          search,
-          tables,
-          function(w) {
-              if (typeof w === 'object') {
-                  w.className =  "CodeMirror-hint-table";
-              } else {
-                  w = {text: w, className: "CodeMirror-hint-table"};
-              }
-
-              return w;
-          }
-      );
-      if (!disableKeywords)
-        addMatches(result, search, keywords, function(w) {return {text: w.toUpperCase(), className: "CodeMirror-hint-keyword"};});
-    }
+      var objectOrClass = function(w, className) {
+        if (typeof w === "object") {
+          w.className = className;
+        } else {
+          w = { text: w, className: className };
+        }
+        return w;
+      };
+    addMatches(result, search, defaultTable, function(w) {
+        return objectOrClass(w, "CodeMirror-hint-table CodeMirror-hint-default-table");
+    });
+    addMatches(
+        result,
+        search,
+        tables, function(w) {
+          return objectOrClass(w, "CodeMirror-hint-table");
+        }
+    );
+    if (!disableKeywords)
+      addMatches(result, search, keywords, function(w) {
+          return objectOrClass(w.toUpperCase(), "CodeMirror-hint-keyword");
+      });
+  }
 
     return {list: result, from: Pos(cur.line, start), to: Pos(cur.line, end)};
   });
