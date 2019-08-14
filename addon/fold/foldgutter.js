c88380a26f6f44c418fd39e3c65a08ab7f3a6ac1
raymondf <raymond.fu@semsci.com>
2019-08-14 16:38:33 +0930

    significant speed optimisation when large amount of lines are folded

--- addon/fold/foldgutter.js
+++ addon/fold/foldgutter.js
@@ -51,8 +51,13 @@
 
   function isFolded(cm, line) {
     var marks = cm.findMarks(Pos(line, 0), Pos(line + 1, 0));
-    for (var i = 0; i < marks.length; ++i)
-      if (marks[i].__isFold && marks[i].find().from.line == line) return marks[i];
+    for (var i = 0; i < marks.length; ++i) {
+      if (marks[i].__isFold) {
+        var fromPos = marks[i].find(-1);
+        if (fromPos && fromPos.line === line)
+          return marks[i];
+      }
+    }
   }
 
   function marker(spec) {
