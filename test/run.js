5a20ce88edf8aa3da2c89584037e7f782f555448
ossdev <ossdev@puresoftware.com>
2019-11-04 15:39:58 +0530

    Codemirror: Replaced PhantomJS Dependency with Puppeteer
    Signed-off-by: ossdev <ossdev@puresoftware.com>

--- test/run.js
+++ test/run.js
@@ -15,17 +15,32 @@ var server = require('http').createServer(function (req, res) {
   }).resume();
 }).addListener('error', function (err) {
   throw err;
-}).listen(3000, function () {
-  var childProcess = require('child_process');
-  var phantomjs = require("phantomjs-prebuilt");
-  var childArgs = [
-    require("path").join(__dirname, 'phantom_driver.js')
-  ];
-  childProcess.execFile(phantomjs.path, childArgs, function (err, stdout, stderr) {
-    server.close();
-    console.log(stdout);
-    if (err) console.error(err);
-    if (stderr) console.error(stderr);
-    process.exit(err || stderr || !ok ? 1 : 0);
+}).listen(3000,(async () => {
+  const puppeteer = require('puppeteer');
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('http://localhost:3000/test/index.html')
+  page.on('dialog', async dialog => {
+    console.log(dialog.message())
+    await dialog.dismiss()
+  })
+  var ret1 = false;
+  while(1){
+    ret1 = await page.evaluate(() => {
+      var output = document.getElementById('status');
+      if (!output) {
+        return false;
+      }
+	return (/^(\d+ failures?|all passed)/i).test(output.innerText);
+    })
+  if(ret1 === true) { break}
+  }
+  var failed = await page.evaluate(function () { return window.failed; });
+  var output = await page.evaluate(function () {
+    return document.getElementById('output').innerText + "\n" +
+    document.getElementById('status').innerText;
   });
-});
+  console.log(output);
+  process.exit(failed > 0 ? 1 : 0);
+  await browser.close()
+})());
