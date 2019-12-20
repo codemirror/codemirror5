0a2b8761640cf672d4907c918f672b663fdddf5b
Marijn Haverbeke <marijn@haverbeke.nl>
2019-12-20 10:04:21 +0100

    [sublime bindings] Fix sub-word motion at start of word

--- keymap/sublime.js
+++ keymap/sublime.js
@@ -22,18 +22,19 @@
     if (dir < 0 && start.ch == 0) return doc.clipPos(Pos(start.line - 1));
     var line = doc.getLine(start.line);
     if (dir > 0 && start.ch >= line.length) return doc.clipPos(Pos(start.line + 1, 0));
-    var state = "start", type;
-    for (var pos = start.ch, e = dir < 0 ? 0 : line.length, i = 0; pos != e; pos += dir, i++) {
+    var state = "start", type, startPos = start.ch;
+    for (var pos = startPos, e = dir < 0 ? 0 : line.length, i = 0; pos != e; pos += dir, i++) {
       var next = line.charAt(dir < 0 ? pos - 1 : pos);
       var cat = next != "_" && CodeMirror.isWordChar(next) ? "w" : "o";
       if (cat == "w" && next.toUpperCase() == next) cat = "W";
       if (state == "start") {
         if (cat != "o") { state = "in"; type = cat; }
+        else startPos = pos + dir
       } else if (state == "in") {
         if (type != cat) {
           if (type == "w" && cat == "W" && dir < 0) pos--;
           if (type == "W" && cat == "w" && dir > 0) { // From uppercase to lowercase
-            if (pos == start.ch + 1) { type = "w"; continue; }
+            if (pos == startPos + 1) { type = "w"; continue; }
             else pos--;
           }
           break;
