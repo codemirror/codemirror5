1fd2f70d475af85fe78e77ecea8c668a2a6d95a0
Erik Welander <ewelander@google.com>
2020-03-17 16:31:09 -0700

    Drop empty space

--- keymap/vim.js
+++ keymap/vim.js
@@ -4186,7 +4186,7 @@
     }
     function makePrompt(prefix, desc) {
       var raw = '<span style="font-family: monospace; white-space: pre">' +
-          (prefix || "") + '<input type="text" autocorrect="off" ' + 
+          (prefix || "") + '<input type="text" autocorrect="off" ' +
           'autocapitalize="off" spellcheck="false"></span>';
       if (desc)
         raw += ' <span style="color: #888">' + desc + '</span>';
