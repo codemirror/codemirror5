519c520989c7a2cfc1e4e721bedee24e867ef6c2
Erik Welander <ewelander@google.com>
2020-03-18 00:07:04 -0700

    [vim bindings] Disable autocorrect for prompt

--- keymap/vim.js
+++ keymap/vim.js
@@ -4186,7 +4186,8 @@
     }
     function makePrompt(prefix, desc) {
       var raw = '<span style="font-family: monospace; white-space: pre">' +
-          (prefix || "") + '<input type="text"></span>';
+          (prefix || "") + '<input type="text" autocorrect="off" ' +
+          'autocapitalize="off" spellcheck="false"></span>';
       if (desc)
         raw += ' <span style="color: #888">' + desc + '</span>';
       return raw;
