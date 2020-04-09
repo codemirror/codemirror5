b4912a273441cb664d3e9f42495164d27649dfa6
Fons van der Plas <fonsvdplas@gmail.com>
2020-04-09 22:40:37 +0200

    [show-hint] Scroll cursor into view after picking.
    **Fixes a minor visual bug:**
    The picked hint might push the cursor out of the screen - this scrolls the cursor back into view. (Would normally happen after the next keypress.)

--- addon/hint/show-hint.js
+++ addon/hint/show-hint.js
@@ -89,6 +89,7 @@
       if (completion.hint) completion.hint(this.cm, data, completion);
       else this.cm.replaceRange(getText(completion), completion.from || data.from,
                                 completion.to || data.to, "complete");
+      this.cm.scrollIntoView();
       CodeMirror.signal(data, "pick", completion);
       this.close();
     },
