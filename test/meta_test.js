55e17daf895ef8e2379226344ec0f7c63aa6d908
Boris Kubiak <kubiakboris@gmail.com>
2020-05-12 16:04:16 +0200

    Add tests

--- test/meta_test.js
+++ test/meta_test.js
@@ -2,8 +2,22 @@ test('meta_id_unique', function () {
   const map = {};
   for (const m of CodeMirror.modeInfo) {
     if (map[m.id]) {
-      throw new Failure("Modes \"" + map[m.id] + "\" and \"" + m.name + "\" share the same ID: \"" + m.id + "\"");
+      throw new Failure("Modes \"" + map[m.id] + "\" and \"" + m.name + "\" share the same ID in meta.js: \"" + m.id + "\"");
     }
     map[m.id] = m.name;
   }
-})
+});
+
+test('meta_id_mandatory', function () {
+  for (const m of CodeMirror.modeInfo) {
+    if (!m.id) throw new Failure("Mode \"" + m.name + "\" must have an id in meta.js");
+  }
+});
+
+test('meta_id_format', function () {
+  const formatRegexp = /^[a-z0-9]{1,4}$/
+  for (const m of CodeMirror.modeInfo) {
+    if (!formatRegexp.test(m.id)) throw new Failure("Mode \"" + m.name + "\" has an invalid id ("+ m.id +
+      "). It should be composed of 1 to 4 alphanumeric characters");
+  }
+});
