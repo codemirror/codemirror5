b6ac63e3c361834d1470ac386e98d13f40fc07f6
Ajin Abraham <ajin25@gmail.com>
2020-05-15 00:03:40 +0530

    Proper escaping of \s
    See: https://lgtm.com/rules/1510624619946/

--- src/line/highlight.js
+++ src/line/highlight.js
@@ -198,7 +198,7 @@ function extractLineClasses(type, output) {
     let prop = lineClass[1] ? "bgClass" : "textClass"
     if (output[prop] == null)
       output[prop] = lineClass[2]
-    else if (!(new RegExp("(?:^|\s)" + lineClass[2] + "(?:$|\s)")).test(output[prop]))
+    else if (!(new RegExp("(?:^|\\s)" + lineClass[2] + "(?:$|\\s)")).test(output[prop]))
       output[prop] += " " + lineClass[2]
   }
   return type
