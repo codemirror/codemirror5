8307b9190dd8f022e0b7f3827f705797e0a79e9f
orionlee <orionlee@yahoo.com>
2020-08-01 13:56:03 -0700

    [css] helper to find missing CSS property value keywords

--- bin/list-missing-css-reserved-words.js
+++ bin/list-missing-css-reserved-words.js
@@ -3,6 +3,9 @@
 // List missing reserved words in CSS mode by comparing them against the list on MDN
 // output the missing one in a html file
 //  bin/list-missing-css-reserved-words.js > bin/missing-css.html
+//
+// List missing property value of the property names specified.
+//  bin/list-missing-css-reserved-words.js --missing-css-property-values <optional property names, comma separated>
 
 
 // MDN related methods
@@ -18,7 +21,7 @@ function mayPrefixed(compatSupport) {
 
 function isOnStandardTrack(compatSupport) {
   // prefixed in some browser;
-  return compatSupport.status['standard_track'];
+  return compatSupport && compatSupport.status['standard_track'];
 
 }
 
@@ -28,7 +31,7 @@ function isOnStandardTrack(compatSupport) {
 function getCssPropNamesOnStandardTrack() {
   const propStandardTrack = [];
   for (const [propName, propDef] of Object.entries(bcd.css.properties)) {
-      if (propDef.__compat && isOnStandardTrack(propDef.__compat)) {
+      if (isOnStandardTrack(propDef.__compat)) {
           propStandardTrack.push(propName);
       }
   }
@@ -37,6 +40,18 @@ function getCssPropNamesOnStandardTrack() {
 
 function getMdnLink(cssPropName) {
   return (bcd.css.properties[cssPropName].__compat || {})['mdn_url'];
+}
+
+function getCssPropertyValuesOnStandardTrack(cssPropName) {
+  const res = [];
+  for (const [key, val] of Object.entries((bcd.css.properties[cssPropName] || {}))) {
+    if (key === '__compat') { continue; }
+    // else the key refers to a a property value
+    if (isOnStandardTrack(val.__compat)) {
+      res.push(key);
+    }
+  }
+  return res;
 }
 
 // CodeMirror ones and the diffs
@@ -50,15 +65,37 @@ EXCLUDED_PROPS = [
   'custom-property', // it means -- variable
 ];
 
-function getMissingCssPropNameList() {
-  const cmPropNameList = Object.keys(cmCssSpecs.propertyKeywords)
+// both standard and non-standard in CSS modes
+function getCMPropNameList() {
+  return Object.keys(cmCssSpecs.propertyKeywords)
     .concat(Object.keys(cmCssSpecs.nonStandardPropertyKeywords));
+}
+
+function getMissingCssPropNameList() {
+  const cmPropNameList = getCMPropNameList();
 
   const missingPropNames = getCssPropNamesOnStandardTrack()
     .filter(p => !cmPropNameList.includes(p))
     .filter(p => !EXCLUDED_PROPS.includes(p));
 
   return missingPropNames;
+}
+const EXCLUDE_PROP_VALUES = [
+  'three_value_syntax', // from mask-position, it's not really a property value
+];
+
+function getMissingCssPropValueListOfProps(propNameList) {
+  const cmValueList = Object.keys(cmCssSpecs.valueKeywords);
+  const res = {};
+  propNameList = propNameList ||  getCMPropNameList();
+  propNameList.forEach(propName => {
+    const missingValues = getCssPropertyValuesOnStandardTrack(propName)
+      .filter(v => !cmValueList.includes(v) && !EXCLUDE_PROP_VALUES.includes(v));
+    if (missingValues.length > 0) {
+      res[propName] = missingValues;
+    }
+  })
+  return res;
 }
 
 /**
@@ -99,5 +136,11 @@ ${propListHtml}
 
 // Main for command line
 if (require.main === module) {
+  const args = process.argv.slice(2);
+  if (args[0] == '--missing-css-property-values') {
+    const propNameList = args[1] ? args[1].split(',') : null;
+    console.log(JSON.stringify(getMissingCssPropValueListOfProps(propNameList), null, 2));
+  } else {
     console.log(listMissingInHtml());
+  }
 }
