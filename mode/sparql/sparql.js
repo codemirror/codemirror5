60603f859895fc56c39a0cc23dc76a0421e18fc9
Mark Boyes <mark.boyes@oxfordsemantic.tech>
2020-10-15 10:55:50 +0100

    Do not treat the opening '<' of an expanded IRI atom as an operator
    The existing code would not highlight the IRI atom "<foo#bar>" in the following line as an atom.
    FILTER( ?x = "42"^^<foo#bar> )
    for example everything after the # would be highlighted as a comment. This is because the sequence "^^<" while all "operator characters", are not all SPARQL operators in this case: the "<" introduces the IRI atom. I special-case the "^^".

--- mode/sparql/sparql.js
+++ mode/sparql/sparql.js
@@ -60,6 +60,12 @@ CodeMirror.defineMode("sparql", function(config) {
       stream.skipToEnd();
       return "comment";
     }
+    else if (ch === "^") {
+      ch = stream.next();
+      if (ch === "^") stream.eat("^");
+      else stream.eatWhile(operatorChars);
+      return "operator";
+    }
     else if (operatorChars.test(ch)) {
       stream.eatWhile(operatorChars);
       return "operator";
