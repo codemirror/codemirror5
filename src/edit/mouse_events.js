8e7f6728bf1d36963fafdf997b12858f25d7711a
Marijn Haverbeke <marijn@haverbeke.nl>
2020-10-26 09:08:36 +0100

    Delay blur events during dragging and clicking
    Issue #6427

--- src/edit/mouse_events.js
+++ src/edit/mouse_events.js
@@ -1,4 +1,4 @@
-import { delayBlurEvent, ensureFocus } from "../display/focus.js"
+import { delayBlurEvent, ensureFocus, onBlur } from "../display/focus.js"
 import { operation } from "../display/operations.js"
 import { visibleLines } from "../display/update_lines.js"
 import { clipPos, cmp, maxPos, minPos, Pos } from "../line/pos.js"
@@ -149,6 +149,7 @@ function leftButtonStartDrag(cm, event, pos, behavior) {
   let dragEnd = operation(cm, e => {
     if (webkit) display.scroller.draggable = false
     cm.state.draggingText = false
+    if (cm.state.delayingBlurEvent) delayBlurEvent(cm)
     off(display.wrapper.ownerDocument, "mouseup", dragEnd)
     off(display.wrapper.ownerDocument, "mousemove", mouseMove)
     off(display.scroller, "dragstart", dragStart)
@@ -172,15 +173,15 @@ function leftButtonStartDrag(cm, event, pos, behavior) {
   if (webkit) display.scroller.draggable = true
   cm.state.draggingText = dragEnd
   dragEnd.copy = !behavior.moveOnDrag
-  // IE's approach to draggable
-  if (display.scroller.dragDrop) display.scroller.dragDrop()
   on(display.wrapper.ownerDocument, "mouseup", dragEnd)
   on(display.wrapper.ownerDocument, "mousemove", mouseMove)
   on(display.scroller, "dragstart", dragStart)
   on(display.scroller, "drop", dragEnd)
 
-  delayBlurEvent(cm)
+  cm.state.delayingBlurEvent = true
   setTimeout(() => display.input.focus(), 20)
+  // IE's approach to draggable
+  if (display.scroller.dragDrop) display.scroller.dragDrop()
 }
 
 function rangeForUnit(cm, pos, unit) {
@@ -193,6 +194,7 @@ function rangeForUnit(cm, pos, unit) {
 
 // Normal selection, as opposed to text dragging.
 function leftButtonSelect(cm, event, start, behavior) {
+  if (ie) delayBlurEvent(cm)
   let display = cm.display, doc = cm.doc
   e_preventDefault(event)
 
