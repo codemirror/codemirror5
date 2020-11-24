06fe02df76d884326d99c5667d3366a6a2ceaaf0
Sebastian Slepowronski <slepowronski@sumologic.com>
2020-11-24 15:56:46 +0100

    Fixed lint error

--- addon/hint/show-hint.js
+++ addon/hint/show-hint.js
@@ -1,6 +1,8 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/LICENSE
 
+// declare global: DOMRect
+
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
     mod(require("../../lib/codemirror"));
