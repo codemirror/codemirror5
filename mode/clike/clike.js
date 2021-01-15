126a266a74b0afe6600fac23230432b68441d2bd
Thiemo Kreuz <thiemowmde@users.noreply.github.com>
2021-01-15 15:21:10 +0100

    [various modes] Add missing ^ anchors to StringStream.match() as optimization
    The method fails anyway (i.e. it acts as if there was no match)
    when the match is not at the start of the string. Adding the
    anchor does not change any behavior, but increases performance.

--- mode/clike/clike.js
+++ mode/clike/clike.js
@@ -350,8 +350,8 @@ CodeMirror.defineMode("clike", function(config, parserConfig) {
   function cpp11StringHook(stream, state) {
     stream.backUp(1);
     // Raw strings.
-    if (stream.match(/(R|u8R|uR|UR|LR)/)) {
-      var match = stream.match(/"([^\s\\()]{0,16})\(/);
+    if (stream.match(/^(?:R|u8R|uR|UR|LR)/)) {
+      var match = stream.match(/^"([^\s\\()]{0,16})\(/);
       if (!match) {
         return false;
       }
@@ -360,8 +360,8 @@ CodeMirror.defineMode("clike", function(config, parserConfig) {
       return tokenRawString(stream, state);
     }
     // Unicode strings/chars.
-    if (stream.match(/(u8|u|U|L)/)) {
-      if (stream.match(/["']/, /* eat */ false)) {
+    if (stream.match(/^(?:u8|u|U|L)/)) {
+      if (stream.match(/^["']/, /* eat */ false)) {
         return "string";
       }
       return false;
