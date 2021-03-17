4c81051bd7ddda857ad90a0237887dcf0701c781
Marijn Haverbeke <marijn@haverbeke.nl>
2021-03-17 08:08:15 +0100

    Initialize contenteditable input as editable
    So that autofocus works.
    
    Closes #6624

--- src/input/ContentEditableInput.js
+++ src/input/ContentEditableInput.js
@@ -29,6 +29,7 @@ export default class ContentEditableInput {
   init(display) {
     let input = this, cm = input.cm
     let div = input.div = display.lineDiv
+    div.contentEditable = true
     disableBrowserMagic(div, cm.options.spellcheck, cm.options.autocorrect, cm.options.autocapitalize)
 
     function belongsToInput(e) {
