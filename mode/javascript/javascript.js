6fbe464029ca4f8dc7d63b14de3ff0b16135b059
Marijn Haverbeke <marijn@haverbeke.nl>
2021-05-25 14:18:43 +0200

    [javascript mode] Be more robust about missing expressions
    Issue https://github.com/codemirror/CodeMirror/issues/6693

--- mode/javascript/javascript.js
+++ mode/javascript/javascript.js
@@ -445,6 +445,7 @@ CodeMirror.defineMode("javascript", function(config, parserConfig) {
     if (type == "{") return contCommasep(objprop, "}", null, maybeop);
     if (type == "quasi") return pass(quasi, maybeop);
     if (type == "new") return cont(maybeTarget(noComma));
+    if (type.match(/[;\}\)\]]/)) return pass();
     return cont();
   }
   function maybeexpression(type) {
