b69f9d9bd58af60bc4aa19e1e74e533c9b6db80f
Marijn Haverbeke <marijn@haverbeke.nl>
2021-06-14 09:29:17 +0200

    [lint addon] And an arrow function

--- addon/lint/lint.js
+++ addon/lint/lint.js
@@ -84,7 +84,7 @@
   }
 
   function clearErrorLines(cm) {
-    cm.eachLine(line => {
+    cm.eachLine(function(line) {
       var has = line.wrapClass && /\bCodeMirror-lint-line-\w+\b/.exec(line.wrapClass);
       if (has) cm.removeLineClass(line, "wrap", has[0]);
     })
