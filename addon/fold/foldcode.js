25e70f0e06f68b55f463f024a04259e98092b30f
Billiam <billiamthesecond@gmail.com>
2021-07-09 12:57:57 -0500

    Exit method early of mode is 'fold' instead of iterating

--- addon/fold/foldcode.js
+++ addon/fold/foldcode.js
@@ -24,9 +24,11 @@
     function getRange(allowFolded) {
       var range = finder(cm, pos);
       if (!range || range.to.line - range.from.line < minSize) return null;
+      if (force === "fold") return range;
+
       var marks = cm.findMarksAt(range.from);
       for (var i = 0; i < marks.length; ++i) {
-        if (marks[i].__isFold && force !== "fold") {
+        if (marks[i].__isFold) {
           if (!allowFolded) return null;
           range.cleared = true;
           marks[i].clear();
