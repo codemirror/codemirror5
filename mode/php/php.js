5eb3016d7b8fd692d5c99c322d3a55d19fdd914e
Ondřej Mirtes <ondrej@mirtes.cz>
2021-11-08 11:07:35 +0100

    PHP mode - highlight enum keyword

--- mode/php/php.js
+++ mode/php/php.js
@@ -80,7 +80,7 @@
   }
 
   var phpKeywords = "abstract and array as break case catch class clone const continue declare default " +
-    "do else elseif enddeclare endfor endforeach endif endswitch endwhile extends final " +
+    "do else elseif enddeclare endfor endforeach endif endswitch endwhile enum extends final " +
     "for foreach function global goto if implements interface instanceof namespace " +
     "new or private protected public static switch throw trait try use var while xor " +
     "die echo empty exit eval include include_once isset list require require_once return " +
@@ -95,7 +95,7 @@
     helperType: "php",
     keywords: keywords(phpKeywords),
     blockKeywords: keywords("catch do else elseif for foreach if switch try while finally"),
-    defKeywords: keywords("class function interface namespace trait"),
+    defKeywords: keywords("class enum function interface namespace trait"),
     atoms: keywords(phpAtoms),
     builtin: keywords(phpBuiltin),
     multiLineStrings: true,
