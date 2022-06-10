904e51b811ee3e822bf058423d3e7b64c73d2909
Jakub Jankiewicz <jcubic@onet.pl>
2022-06-10 16:52:12 +0200

    fix PegJS mode

--- mode/pegjs/pegjs.js
+++ mode/pegjs/pegjs.js
@@ -1,5 +1,5 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
-// Distributed under an MIT license: https://codemirror.net/5/LICENSE
+// Distributed under an MIT license: https://codemirror.net/LICENSE
 
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
@@ -18,95 +18,104 @@ CodeMirror.defineMode("pegjs", function (config) {
     return stream.match(/^[a-zA-Z_][a-zA-Z0-9_]*/);
   }
 
+  function peg(stream, state) {
+    if (stream)
+    //check for state changes
+    if (!state.inString && !state.inComment && (["'", '"'].includes(stream.peek()))) {
+      state.stringType = stream.peek();
+      stream.next(); // Skip quote
+      state.inString = true; // Update state
+    }
+    if (!state.inString && !state.inComment && stream.match('/*')) {
+      state.inComment = true;
+    }
+
+    //return state
+    if (state.inString) {
+      while (state.inString && !stream.eol()) {
+        if (stream.peek() === state.stringType) {
+          stream.next(); // Skip quote
+          state.inString = false; // Clear flag
+        } else if (stream.peek() === '\\') {
+          stream.next();
+          stream.next();
+        } else {
+          stream.match(/^.[^\\\"\']*/);
+        }
+      }
+      return state.lhs ? "property string" : "string"; // Token style
+    } else if (state.inComment) {
+      while (state.inComment && !stream.eol()) {
+        if (stream.match('*/')) {
+          state.inComment = false; // Clear flag
+        } else {
+          stream.match(/^.[^\*]*/);
+        }
+      }
+      return "comment";
+    } else if (state.inCharacterClass) {
+      while (state.inCharacterClass && !stream.eol()) {
+        if (!(stream.match(/^[^\]\\]+/) || stream.match(/^\\./))) {
+          state.inCharacterClass = false;
+        }
+      }
+    } else if (stream.peek() === '[') {
+      stream.next();
+      state.inCharacterClass = true;
+      return 'bracket';
+    } else if (stream.match('//')) {
+      stream.skipToEnd();
+      return "comment";
+    } else if (stream.peek() === '{') {
+      state.token = function(stream, state) {
+        var char = stream.peek();
+        if (char === '{') {
+          state.braced++;
+        } else if (char == '}') {
+          if (state.braced > 0) {
+            state.braced--;
+          } else {
+            stream.eat("}");
+            state.token = peg;
+            state.localState = state.localMode = null;
+            return null;
+          }
+        }
+        return state.localMode.token(stream, state.localState);
+      };
+      state.braced = 0;
+      state.localMode = jsMode;
+      state.localState = CodeMirror.startState(jsMode);
+    } else if (identifier(stream)) {
+      if (stream.peek() === ':') {
+        return 'variable';
+      }
+      return 'variable-2';
+    } else if (['[', '\x5d', '(', ')'].indexOf(stream.peek()) != -1) {
+      stream.next();
+      return 'bracket';
+    } else if (!stream.eatSpace()) {
+      stream.next();
+    }
+    return null;
+  }
+
   return {
     startState: function () {
       return {
+        token: peg,
         inString: false,
         stringType: null,
         inComment: false,
         inCharacterClass: false,
         braced: 0,
         lhs: true,
+        localMode: null,
         localState: null
       };
     },
     token: function (stream, state) {
-      if (stream)
-
-      //check for state changes
-      if (!state.inString && !state.inComment && ((stream.peek() == '"') || (stream.peek() == "'"))) {
-        state.stringType = stream.peek();
-        stream.next(); // Skip quote
-        state.inString = true; // Update state
-      }
-      if (!state.inString && !state.inComment && stream.match('/*')) {
-        state.inComment = true;
-      }
-
-      //return state
-      if (state.inString) {
-        while (state.inString && !stream.eol()) {
-          if (stream.peek() === state.stringType) {
-            stream.next(); // Skip quote
-            state.inString = false; // Clear flag
-          } else if (stream.peek() === '\\') {
-            stream.next();
-            stream.next();
-          } else {
-            stream.match(/^.[^\\\"\']*/);
-          }
-        }
-        return state.lhs ? "property string" : "string"; // Token style
-      } else if (state.inComment) {
-        while (state.inComment && !stream.eol()) {
-          if (stream.match('*/')) {
-            state.inComment = false; // Clear flag
-          } else {
-            stream.match(/^.[^\*]*/);
-          }
-        }
-        return "comment";
-      } else if (state.inCharacterClass) {
-          while (state.inCharacterClass && !stream.eol()) {
-            if (!(stream.match(/^[^\]\\]+/) || stream.match(/^\\./))) {
-              state.inCharacterClass = false;
-            }
-          }
-      } else if (stream.peek() === '[') {
-        stream.next();
-        state.inCharacterClass = true;
-        return 'bracket';
-      } else if (stream.match('//')) {
-        stream.skipToEnd();
-        return "comment";
-      } else if (state.braced || stream.peek() === '{') {
-        if (state.localState === null) {
-          state.localState = CodeMirror.startState(jsMode);
-        }
-        var token = jsMode.token(stream, state.localState);
-        var text = stream.current();
-        if (!token) {
-          for (var i = 0; i < text.length; i++) {
-            if (text[i] === '{') {
-              state.braced++;
-            } else if (text[i] === '}') {
-              state.braced--;
-            }
-          };
-        }
-        return token;
-      } else if (identifier(stream)) {
-        if (stream.peek() === ':') {
-          return 'variable';
-        }
-        return 'variable-2';
-      } else if (['[', ']', '(', ')'].indexOf(stream.peek()) != -1) {
-        stream.next();
-        return 'bracket';
-      } else if (!stream.eatSpace()) {
-        stream.next();
-      }
-      return null;
+      return state.token(stream, state);
     }
   };
 }, "javascript");
