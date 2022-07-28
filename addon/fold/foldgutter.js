7ac872b0dbdd75a5c88a8c143c14021f70451041
Marijn Haverbeke <marijn@haverbeke.nl>
2022-07-28 10:27:42 +0200

    [foldgutter addon] Recompute fold markers on mode change
    Closes https://github.com/codemirror/codemirror5/issues/6968

--- addon/fold/foldgutter.js
+++ addon/fold/foldgutter.js
@@ -21,6 +21,7 @@
       cm.off("fold", onFold);
       cm.off("unfold", onFold);
       cm.off("swapDoc", onChange);
+      cm.off("optionChange", optionChange);
     }
     if (val) {
       cm.state.foldGutter = new State(parseOptions(val));
@@ -31,6 +32,7 @@
       cm.on("fold", onFold);
       cm.on("unfold", onFold);
       cm.on("swapDoc", onChange);
+      cm.on("optionChange", optionChange);
     }
   });
 
@@ -118,6 +120,10 @@
     var folded = isFolded(cm, line);
     if (folded) folded.clear();
     else cm.foldCode(Pos(line, 0), opts);
+  }
+
+  function optionChange(cm, option) {
+    if (option == "mode") onChange(cm)
   }
 
   function onChange(cm) {
