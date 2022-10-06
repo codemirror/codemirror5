7438475c0a55636232f8f441c6e87c77d69e6f4e
Mark Boyes <mark.boyes@oxfordsemantic.tech>
2022-10-06 11:35:23 +0100

    Simplify use of match, remove Unicocde RegExp

--- mode/sparql/sparql.js
+++ mode/sparql/sparql.js
@@ -33,14 +33,9 @@ CodeMirror.defineMode("sparql", function(config) {
                              "true", "false", "with",
                              "data", "copy", "to", "move", "add", "create", "drop", "clear", "load", "into"]);
   var operatorChars = /[*+\-<>=&|\^\/!\?]/;
-  var PN_CHARS_BASE =
-    "[A-Za-z\\u{00C0}-\\u{00D6}\\u{00D8}-\\u{00F6}\\u{00F8}-\\u{02FF}" +
-    "\\u{0370}-\\u{037D}\\u{037F}-\\u{1FFF}\\u{200C}-\\u{200D}\\u{2070}-\\u{218F}" +
-    "\\u{2C00}-\\u{2FEF}\\u{3001}-\\u{D7FF}\\u{F900}-\\u{FDCF}\\u{FDF0}-\\u{FFFD}\\u{10000}-\\u{EFFFF}]";
-  var PN_CHARS_U = PN_CHARS_BASE + "|_";
-  var PN_CHARS = PN_CHARS_U + "|[\\-0-9\\u{00B7}\\u{0300}-\\u{036F}\\u{203F}-\\u{2040}]";
-  var PREFIX_START = new RegExp(PN_CHARS_BASE, "u");
-  var PREFIX_REMAINDER = new RegExp("((" + PN_CHARS + "|\\.)*(" + PN_CHARS + "))?:", "u");
+  var PN_CHARS = "[A-Za-z_\\-0-9]";
+  var PREFIX_START = new RegExp("[A-Za-z]");
+  var PREFIX_REMAINDER = new RegExp("((" + PN_CHARS + "|\\.)*(" + PN_CHARS + "))?:");
 
   function tokenBase(stream, state) {
     var ch = stream.next();
@@ -79,8 +74,7 @@ CodeMirror.defineMode("sparql", function(config) {
       stream.eatWhile(/[a-z\d\-]/i);
       return "meta";
     }
-    else if (PREFIX_START.test(ch) && stream.match(PREFIX_REMAINDER, false)) {
-        stream.match(PREFIX_REMAINDER)
+    else if (PREFIX_START.test(ch) && stream.match(PREFIX_REMAINDER)) {
         eatPnLocal(stream);
         return "atom";
     }
