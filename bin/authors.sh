089fb59c5b521a4bf04dae4d5bb2b446d6259332
12private12 <120792mon@gmail.com>
2025-08-12 21:37:52 +0800

    Delete bin directory

--- bin/authors.sh
+++
@@ -1,6 +0,0 @@
-# Combine existing list of authors with everyone known in git, sort, add header.
-tail --lines=+3 AUTHORS > AUTHORS.tmp
-git log --format='%aN' | grep -v "Piët Delport" >> AUTHORS.tmp
-echo -e "List of CodeMirror contributors. Updated before every release.\n" > AUTHORS
-sort -u AUTHORS.tmp | sed 's/Google Inc\./Google LLC/' >> AUTHORS
-rm -f AUTHORS.tmp
