bf3eed1d33e884290d74f5f33ba580a060b1073b
Rodrigo Laiola Guimarães <rlaiola@users.noreply.github.com>
2025-12-23 13:26:34 -0300

    Consolidate tests

--- mode/sql/test.js
+++ mode/sql/test.js
@@ -1,6 +1,8 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: https://codemirror.net/5/LICENSE
 
+/* global CodeMirror, test, testCM, is, eq */
+
 (function() {
   var mode = CodeMirror.getMode({indentUnit: 2}, "text/x-mysql");
   function MT(name) { test.mode(name, mode, Array.prototype.slice.call(arguments, 1)); }
@@ -60,4 +62,104 @@
   // ODBC escape syntax intentionally not supported
   MT("no_curly_braces",
      "[keyword SELECT] {d [string '2023-01-01']}");
+
+  // Tests for SQL mode - focusing on text/x-mysql
+
+  testCM("sql_mysql_keyword_invisible", function(cm) {
+    var tokens = cm.getLineTokens(0);
+    var invisible = tokens.find(function(t) {
+      return t.string.toUpperCase() === "INVISIBLE";
+    });
+    is(invisible, "INVISIBLE token should exist");
+    eq(invisible.type, "keyword");
+  }, {
+    mode: "text/x-mysql",
+    value: "CREATE INDEX idx ON t(c) INVISIBLE"
+  });
+
+  testCM("sql_mysql_role_token_exists", function(cm) {
+    var tokens = cm.getLineTokens(0);
+    var role = tokens.find(function(t) {
+      return t.string.toUpperCase() === "ROLE";
+    });
+    is(role, "ROLE token should exist");
+  }, {
+    mode: "text/x-mysql",
+    value: "CREATE ROLE reporting_role"
+  });
+
+  testCM("sql_mysql_keyword_json_table", function(cm) {
+    var tokens = cm.getLineTokens(0);
+    var jt = tokens.find(function(t) {
+      return t.string.toUpperCase() === "JSON_TABLE";
+    });
+    is(jt, "JSON_TABLE token should exist");
+    eq(jt.type, "keyword");
+  }, {
+    mode: "text/x-mysql",
+    value: "SELECT * FROM JSON_TABLE(doc, '$.a[*]' COLUMNS(x INT PATH '$')) jt"
+  });
+
+  testCM("sql_mysql_window_keywords", function(cm) {
+    var tokens = cm.getLineTokens(0);
+    var kws = tokens
+      .filter(function(t) { return t.type === "keyword"; })
+      .map(function(t) { return t.string.toUpperCase(); });
+
+    is(kws.indexOf("OVER") !== -1);
+    is(kws.indexOf("PARTITION") !== -1);
+    is(kws.indexOf("ROW_NUMBER") !== -1);
+  }, {
+    mode: "text/x-mysql",
+    value: "SELECT ROW_NUMBER() OVER (PARTITION BY dept) FROM emp"
+  });
+
+  testCM("sql_mysql_with_recursive_keywords", function(cm) {
+    var tokens = cm.getLineTokens(0);
+    var kws = tokens
+      .filter(function(t) { return t.type === "keyword"; })
+      .map(function(t) { return t.string.toUpperCase(); });
+
+    is(kws.indexOf("WITH") !== -1);
+    is(kws.indexOf("RECURSIVE") !== -1);
+  }, {
+    mode: "text/x-mysql",
+    value: "WITH RECURSIVE t AS (SELECT 1) SELECT * FROM t"
+  });
+
+  testCM("sql_mysql_date_type_token", function(cm) {
+    var tokens = cm.getLineTokens(0);
+    var date = tokens.find(function(t) {
+      return t.string.toUpperCase() === "DATE";
+    });
+    is(date, "DATE token should exist");
+  }, {
+    mode: "text/x-mysql",
+    value: "CREATE TABLE t (d DATE)"
+  });
+
+  testCM("sql_mysql_schema_tokens", function(cm) {
+    var tokens = cm.getLineTokens(0);
+    var schemas = tokens.find(function(t) {
+      return t.string.toUpperCase() === "SCHEMAS";
+    });
+    is(schemas, "SCHEMAS token should exist");
+    is(schemas.type === "keyword" || schemas.type === "builtin");
+  }, {
+    mode: "text/x-mysql",
+    value: "SHOW SCHEMAS"
+  });
+
+  testCM("sql_mysql_invisible_identifier_untouched", function(cm) {
+    var tokens = cm.getLineTokens(0);
+    var ident = tokens.find(function(t) {
+      return t.string === "idx";
+    });
+    is(ident);
+    is(ident.type !== "keyword");
+  }, {
+    mode: "text/x-mysql",
+    value: "CREATE INDEX idx ON t(c) INVISIBLE"
+  });
+
 })();
